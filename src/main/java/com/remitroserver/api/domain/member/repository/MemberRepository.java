68743f4aec967bc95e2e27dbb0558755ca2e78bf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 03:09:48 +0900

    feat: 회원 이메일로 사용자 조회 로직 구현

--- src/main/java/com/remitroserver/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/remitroserver/api/domain/member/repository/MemberRepository.java
@@ -1,10 +1,14 @@
 package com.remitroserver.api.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.remitroserver.api.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findMemberByEmail(String email);
 
 	boolean existsMemberByEmail(String email);
 
