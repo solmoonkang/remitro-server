cb7b8b47dfca09c8987947455794aa61737ea8d4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-20 19:34:57 +0900

    feat: AES, 중복 및 요청 데이터 형식 불일치 에러 메시지 정의

--- src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
+++ src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
@@ -8,7 +8,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorMessage {
 
-	UNKNOWN_SERVER_ERROR("[❎ ERROR] 서버에서 알 수 없는 에러가 발생했습니다.");
+	UNKNOWN_SERVER_ERROR("[❎ ERROR] 서버에서 알 수 없는 에러가 발생했습니다."),
+
+	AES_INITIALIZE_ERROR("[❎ ERROR] AES 초기화 중 예외가 발생했습니다."),
+	AES_ENCRYPT_ERROR("[❎ ERROR] AES 암호화 중 오류가 발생했습니다."),
+	AES_DECRYPT_ERROR("[❎ ERROR] AES 복호화 중 오류가 발생했습니다."),
+
+	DUPLICATED_EMAIL("[❎ ERROR] 이미 존재하는 사용자 이메일입니다."),
+	DUPLICATED_NICKNAME("[❎ ERROR] 이미 존재하는 사용자 닉네임입니다."),
+	DUPLICATED_REGISTRATION_NUMBER("[❎ ERROR] 이미 가입된 사용자입니다."),
+	PASSWORD_MISMATCH("[❎ ERROR] 입력하신 비밀번호가 서로 일치하지 않습니다.");
 
 	private final String message;
 }
