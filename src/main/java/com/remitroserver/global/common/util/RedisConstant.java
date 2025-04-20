e440c44ac75610fae9d74aa7c1828d4546cccbf2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 03:08:54 +0900

    feat: 리프레시 토큰 Redis 키 값 상수 정의

---
+++ src/main/java/com/remitroserver/global/common/util/RedisConstant.java
@@ -0,0 +1,10 @@
+package com.remitroserver.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RedisConstant {
+
+	public static final String REFRESH_TOKEN_KEY_PREFIX = "REFRESH_TOKEN:";
+}
