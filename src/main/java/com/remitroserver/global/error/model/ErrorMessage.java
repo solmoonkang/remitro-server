38058c8270f3f3c51f28202341809797ba880247
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 03:08:18 +0900

    feat: 존재하지 않는 회원이 있을 경우에 대한 예외 메시지 정의

--- src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
+++ src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
@@ -14,13 +14,14 @@ public enum ErrorMessage {
 	AES_ENCRYPT_ERROR("[❎ ERROR] AES 암호화 중 오류가 발생했습니다."),
 	AES_DECRYPT_ERROR("[❎ ERROR] AES 복호화 중 오류가 발생했습니다."),
 
-	UNAUTHORIZED_REQUEST("[❎ ERROR] 인증되지 않은 사용자 요청입니다."),
-	INVALID_AUTHORIZATION_HEADER("[❎ ERROR] 유효하지 않은 AUTHORIZATION 헤더입니다."),
+	UNAUTHORIZED_REQUEST_ERROR("[❎ ERROR] 인증되지 않은 사용자 요청입니다."),
+	INVALID_AUTHORIZATION_HEADER_ERROR("[❎ ERROR] 유효하지 않은 AUTHORIZATION 헤더입니다."),
 
-	DUPLICATED_EMAIL("[❎ ERROR] 이미 존재하는 사용자 이메일입니다."),
-	DUPLICATED_NICKNAME("[❎ ERROR] 이미 존재하는 사용자 닉네임입니다."),
-	DUPLICATED_REGISTRATION_NUMBER("[❎ ERROR] 이미 가입된 사용자입니다."),
-	PASSWORD_MISMATCH("[❎ ERROR] 입력하신 비밀번호가 서로 일치하지 않습니다.");
+	DUPLICATED_EMAIL_ERROR("[❎ ERROR] 이미 존재하는 사용자 이메일입니다."),
+	DUPLICATED_NICKNAME_ERROR("[❎ ERROR] 이미 존재하는 사용자 닉네임입니다."),
+	DUPLICATED_REGISTRATION_NUMBER_ERROR("[❎ ERROR] 이미 가입된 사용자입니다."),
+	PASSWORD_MISMATCH_ERROR("[❎ ERROR] 입력하신 비밀번호가 서로 일치하지 않습니다."),
+	MEMBER_NOT_FOUND_ERROR("[❎ ERROR] 요청하신 회원을 찾을 수 없습니다.");
 
 	private final String message;
 }
