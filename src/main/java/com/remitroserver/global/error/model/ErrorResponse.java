983b9cb33031c2667f60fb2e837db34ae473c28d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-20 19:35:18 +0900

    feat: swagger 문서화 작업

--- src/main/java/com/remitroserver/global/error/model/ErrorResponse.java
+++ src/main/java/com/remitroserver/global/error/model/ErrorResponse.java
@@ -4,12 +4,16 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "공통 에러 응답 포맷")
 public record ErrorResponse(
+	@Schema(description = "에러 메시지", example = "유효하지 않은 요청입니다.")
 	String message,
 
+	@Schema(description = "검증 실패 필드 및 메시지", example = "{\"email\": \"이메일 형식이 아닙니다.\"}")
 	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	Map<String, String> validation
 ) {
