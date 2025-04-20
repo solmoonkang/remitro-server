e16bce16984194b4078b8d9556ab6e272aceeb34
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-20 16:48:02 +0900

    feat: JPA 리스닝 어노테이션 추가

--- src/main/java/com/remitroserver/RemitroServerApplication.java
+++ src/main/java/com/remitroserver/RemitroServerApplication.java
@@ -2,12 +2,13 @@ package com.remitroserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RemitroServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RemitroServerApplication.class, args);
 	}
-
 }
