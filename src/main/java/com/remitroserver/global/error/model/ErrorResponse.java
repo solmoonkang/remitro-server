106118ee36b0cb1b85bf7bc9da2fe66049f80fae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-20 16:48:59 +0900

    feat: 예외 응답 메시지 포맷 구현

---
+++ src/main/java/com/remitroserver/global/error/model/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.remitroserver.global.error.model;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+
+@Builder
+public record ErrorResponse(
+	String message,
+
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+	Map<String, String> validation
+) {
+}
