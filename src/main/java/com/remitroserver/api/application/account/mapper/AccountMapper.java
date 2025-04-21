ce7c4ec26cbfee4f74f002dca08fbfb9abc65f3c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 16:11:30 +0900

    feat: 계좌 상세 조회 응답 DTO 핼퍼 로직 구현

--- src/main/java/com/remitroserver/api/application/account/mapper/AccountMapper.java
+++ src/main/java/com/remitroserver/api/application/account/mapper/AccountMapper.java
@@ -3,6 +3,7 @@ package com.remitroserver.api.application.account.mapper;
 import static com.remitroserver.global.error.model.ErrorMessage.*;
 
 import com.remitroserver.api.domain.account.entity.Account;
+import com.remitroserver.api.dto.account.response.AccountDetailResponse;
 import com.remitroserver.api.dto.account.response.AccountSummaryResponse;
 
 public class AccountMapper {
@@ -14,9 +15,21 @@ public class AccountMapper {
 	public static AccountSummaryResponse toSummaryResponse(Account account) {
 		return AccountSummaryResponse.builder()
 			.accountNumber(account.getAccountNumber())
+			.accountToken(account.getAccountToken())
 			.balance(account.getBalance().getValue())
 			.status(account.getStatus())
 			.createdAt(account.getCreatedAt())
+			.build();
+	}
+
+	public static AccountDetailResponse toDetailResponse(Account account) {
+		return AccountDetailResponse.builder()
+			.accountNumber(account.getAccountNumber())
+			.accountType(account.getAccountType())
+			.balance(account.getBalance().getValue())
+			.status(account.getStatus())
+			.createdAt(account.getCreatedAt())
+			.ownerNickname(account.getMember().getNickname())
 			.build();
 	}
 }
