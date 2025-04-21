8cd7c42182a191cc372cf29acb3828cc4f8291f1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 13:05:53 +0900

    feat: 금액 유효성 및 연산 로직 구현

---
+++ src/main/java/com/remitroserver/api/domain/account/model/Money.java
@@ -0,0 +1,52 @@
+package com.remitroserver.api.domain.account.model;
+
+import static com.remitroserver.global.error.model.ErrorMessage.*;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import com.remitroserver.global.error.exception.BadRequestException;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Money {
+
+	@Column(name = "amount", nullable = false)
+	private Long value;
+
+	public Money(Long value) {
+		this.value = Optional.ofNullable(value)
+			.filter(money -> money >= 0)
+			.orElseThrow(() -> new BadRequestException(INVALID_MONEY_VALUE_ERROR));
+	}
+
+	public Money add(Money target) {
+		Objects.requireNonNull(target, "추가할 금액이 NULL일 수 없습니다.");
+		return new Money(this.value + target.value);
+	}
+
+	public Money subtract(Money target) {
+		Objects.requireNonNull(target, "차감할 금액이 NULL일 수 없습니다.");
+
+		return Optional.of(target)
+			.filter(money -> this.value >= money.value)
+			.map(money -> new Money(this.value - money.value))
+			.orElseThrow(() -> new BadRequestException(INSUFFICIENT_BALANCE_ERROR));
+	}
+
+	public boolean isGreaterThan(Money target) {
+		Objects.requireNonNull(target, "비교 대상 금액이 NULL일 수 없습니다.");
+		return this.value > target.value;
+	}
+
+	public static Money zero() {
+		return new Money(0L);
+	}
+}
