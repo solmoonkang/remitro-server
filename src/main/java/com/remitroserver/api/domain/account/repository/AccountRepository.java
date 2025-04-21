60c9fd1bf73dfa1553b6e0dd520a1f407d40344f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 15:23:46 +0900

    feat: 모든 계좌 정보를 최신순으로 반환하는 로직 구현

--- src/main/java/com/remitroserver/api/domain/account/repository/AccountRepository.java
+++ src/main/java/com/remitroserver/api/domain/account/repository/AccountRepository.java
@@ -1,6 +1,10 @@
 package com.remitroserver.api.domain.account.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.remitroserver.api.domain.account.entity.Account;
 import com.remitroserver.api.domain.account.model.AccountType;
@@ -11,4 +15,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 	boolean existsByAccountNumber(String accountNumber);
 
 	int countByMemberAndAccountType(Member member, AccountType accountType);
+
+	@Query("SELECT a FROM Account a WHERE a.member = :member ORDER BY a.createdAt DESC")
+	List<Account> findAccountsByMemberOrderByCreatedAt(@Param("member") Member member);
 }
