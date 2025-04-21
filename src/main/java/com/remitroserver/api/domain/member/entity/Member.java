628076205cb0f46e573a636af3d5097f56d3ea46
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 10:18:48 +0900

    refactor: 테이블명 수정

--- src/main/java/com/remitroserver/api/domain/member/entity/Member.java
+++ src/main/java/com/remitroserver/api/domain/member/entity/Member.java
@@ -16,26 +16,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "TBL_MEMBERS")
+@Table(name = "members")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "member_id")
+	@Column(name = "member_id", nullable = false)
 	private Long id;
 
 	@Column(name = "email", unique = true, nullable = false)
 	private String email;
 
-	@Column(name = "password", unique = true, nullable = false)
+	@Column(name = "password", nullable = false)
 	private String password;
 
 	@Column(name = "registration_number", unique = true, nullable = false)
 	private String registrationNumber;
 
-	@Column(name = "nickname", unique = true, nullable = false)
+	@Column(name = "nickname", length = 50, unique = true, nullable = false)
 	private String nickname;
 
 	private Member(String email, String password, String registrationNumber, String nickname) {
