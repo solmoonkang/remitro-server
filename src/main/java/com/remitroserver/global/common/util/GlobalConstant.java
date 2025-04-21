93a73d152b21bc1ac20af27727f2e2b29f6bffcd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 10:17:38 +0900

    feat: 하이픈 상수 정의

---
+++ src/main/java/com/remitroserver/global/common/util/GlobalConstant.java
@@ -0,0 +1,10 @@
+package com.remitroserver.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GlobalConstant {
+
+	public static final String HYPHEN = "-";
+}
