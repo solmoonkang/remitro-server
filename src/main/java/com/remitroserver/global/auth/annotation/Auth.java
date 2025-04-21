50c9f2025ad6c9b7a8778f738969e8b16ac2cf2b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 10:17:25 +0900

    feat: 커스텀 어노테이션 구현

---
+++ src/main/java/com/remitroserver/global/auth/annotation/Auth.java
@@ -0,0 +1,16 @@
+package com.remitroserver.global.auth.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : #this")
+public @interface Auth {
+}
