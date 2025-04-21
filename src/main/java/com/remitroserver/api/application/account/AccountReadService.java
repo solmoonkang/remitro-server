010243f5b9c4be9f5565924b6877048a2e892101
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 13:07:29 +0900

    feat: 계좌 검증 로직 구현

---
+++ src/main/java/com/remitroserver/api/application/account/AccountReadService.java
@@ -0,0 +1,45 @@
+package com.remitroserver.api.application.account;
+
+import static com.remitroserver.global.error.model.ErrorMessage.*;
+
+import org.springframework.stereotype.Service;
+
+import com.remitroserver.api.domain.account.model.AccountType;
+import com.remitroserver.api.domain.account.repository.AccountRepository;
+import com.remitroserver.api.domain.member.entity.Member;
+import com.remitroserver.global.common.util.AccountNumberGenerator;
+import com.remitroserver.global.error.exception.BadRequestException;
+import com.remitroserver.global.error.exception.ConflictException;
+import com.remitroserver.global.error.model.ErrorMessage;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountReadService {
+
+	private static final int MAX_ACCOUNT_NUMBER_GENERATION_RETRY = 5;
+
+	private final AccountNumberGenerator accountNumberGenerator;
+	private final AccountRepository accountRepository;
+
+	public void validateAccountLimitExceeded(Member member, AccountType accountType) {
+		if (accountRepository.countByMemberAndAccountType(member, accountType) >= accountType.getMaxAccount()) {
+			throw new BadRequestException(ACCOUNT_TYPE_LIMIT_EXCEEDED_ERROR);
+		}
+	}
+
+	public String generateUniqueAccountNumber(AccountType accountType) {
+		int attemptCount = 0;
+
+		while (attemptCount++ < MAX_ACCOUNT_NUMBER_GENERATION_RETRY) {
+			final String generatedNumber = accountNumberGenerator.generateAccountNumber(accountType);
+
+			if (!accountRepository.existsByAccountNumber(generatedNumber)) {
+				return generatedNumber;
+			}
+		}
+
+		throw new ConflictException(ErrorMessage.ACCOUNT_NUMBER_GENERATION_FAILED_ERROR);
+	}
+}
