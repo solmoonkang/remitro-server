ef5c2cd77090f9c4c4813ffd61f97f28d4ceb779
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-21 13:08:55 +0900

    feat: 계좌 금액 관련 에러 메시지 정의

--- src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
+++ src/main/java/com/remitroserver/global/error/model/ErrorMessage.java
@@ -18,9 +18,14 @@ public enum ErrorMessage {
 	INVALID_AUTHORIZATION_HEADER_ERROR("[❎ ERROR] 유효하지 않은 AUTHORIZATION 헤더입니다."),
 	UNAUTHORIZED_REFRESH_TOKEN_ERROR("[❎ ERROR] 유효하지 않은 리프레시 토큰입니다."),
 
+	INVALID_MONEY_VALUE_ERROR("[❎ ERROR] 금액은 0원 이상이어야 합니다."),
+	INSUFFICIENT_BALANCE_ERROR("[❎ ERROR] 잔액이 부족합니다."),
+
 	DUPLICATED_EMAIL_ERROR("[❎ ERROR] 이미 존재하는 사용자 이메일입니다."),
 	DUPLICATED_NICKNAME_ERROR("[❎ ERROR] 이미 존재하는 사용자 닉네임입니다."),
 	DUPLICATED_REGISTRATION_NUMBER_ERROR("[❎ ERROR] 이미 가입된 사용자입니다."),
+	ACCOUNT_NUMBER_GENERATION_FAILED_ERROR("[❎ ERROR] 이미 존재하는 계좌 번호입니다."),
+	ACCOUNT_TYPE_LIMIT_EXCEEDED_ERROR("[❎ ERROR] 해당 계좌 유형은 최대 생성 개수를 초과했습니다."),
 	PASSWORD_MISMATCH_ERROR("[❎ ERROR] 입력하신 비밀번호가 서로 일치하지 않습니다."),
 	INVALID_REGISTRATION_NUMBER_FORMAT_ERROR("[❎ ERROR] 올바르지 않은 주민등록번호 형식입니다."),
 	MEMBER_NOT_FOUND_ERROR("[❎ ERROR] 요청하신 회원을 찾을 수 없습니다.");
