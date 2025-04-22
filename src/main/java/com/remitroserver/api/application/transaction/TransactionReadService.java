5d30e624dc528c544a644874d2856a73ddc83a20
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-22 16:30:14 +0900

    feat: 요청된 거래 내역 조회 로직 구현

--- src/main/java/com/remitroserver/api/application/transaction/TransactionReadService.java
+++ src/main/java/com/remitroserver/api/application/transaction/TransactionReadService.java
@@ -2,10 +2,16 @@ package com.remitroserver.api.application.transaction;
 
 import static com.remitroserver.global.error.model.ErrorMessage.*;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.remitroserver.api.domain.member.entity.Member;
+import com.remitroserver.api.domain.transaction.entity.Transaction;
+import com.remitroserver.api.domain.transaction.model.TransactionStatus;
 import com.remitroserver.api.domain.transaction.repository.TransactionRepository;
 import com.remitroserver.global.error.exception.ConflictException;
+import com.remitroserver.global.error.exception.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +20,12 @@ import lombok.RequiredArgsConstructor;
 public class TransactionReadService {
 
 	private final TransactionRepository transactionRepository;
+
+	public Transaction getRequestedTransactionByTokenAndOwner(UUID transactionToken, Member member) {
+		return transactionRepository
+			.findByTransactionTokenAndFromAccountMemberAndStatus(transactionToken, member, TransactionStatus.REQUESTED)
+			.orElseThrow(() -> new NotFoundException(TRANSACTION_NOT_FOUND_ERROR));
+	}
 
 	public void validateIdempotencyKeyExists(String idempotencyKey) {
 		if (transactionRepository.existsByIdempotencyKey(idempotencyKey)) {
