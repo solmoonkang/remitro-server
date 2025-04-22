6fc06403f771522c94fa587a5d652f94678ad0f9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-22 16:29:47 +0900

    feat: 거래토큰, 수신자, 그리고 상태에 따른 거래 조회 로직 구현

--- src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionRepository.java
@@ -1,10 +1,18 @@
 package com.remitroserver.api.domain.transaction.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.remitroserver.api.domain.member.entity.Member;
 import com.remitroserver.api.domain.transaction.entity.Transaction;
+import com.remitroserver.api.domain.transaction.model.TransactionStatus;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 	boolean existsByIdempotencyKey(String idempotencyKey);
+
+	Optional<Transaction> findByTransactionTokenAndFromAccountMemberAndStatus(
+		UUID transactionToken, Member member, TransactionStatus status);
 }
