83425f5243f7985cc16d30905138495e87063184
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-23 14:03:39 +0900

    feat: 트랜잭션 토큰과 인증된 회원을 통한 거래 내역 조회 로직 구현

--- src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionRepository.java
@@ -19,6 +19,8 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
 	boolean existsByIdempotencyKey(String idempotencyKey);
 
+	Optional<Transaction> findByTransactionTokenAndMember(UUID transactionToken, Member member);
+
 	Optional<Transaction> findByTransactionTokenAndFromAccountMemberAndStatus(
 		UUID transactionToken,
 		Member member,
