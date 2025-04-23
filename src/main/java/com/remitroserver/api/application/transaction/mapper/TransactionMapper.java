6c56a09e41f081b713c8a68860da0ddacbe2bf92
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-23 14:04:29 +0900

    feat: 거래 상세 조회 응답 DTO, 거래 상태 로그 응답 DTO 핼퍼 로직 구현

--- src/main/java/com/remitroserver/api/application/transaction/mapper/TransactionMapper.java
+++ src/main/java/com/remitroserver/api/application/transaction/mapper/TransactionMapper.java
@@ -1,6 +1,11 @@
 package com.remitroserver.api.application.transaction.mapper;
 
+import java.util.List;
+
 import com.remitroserver.api.domain.transaction.entity.Transaction;
+import com.remitroserver.api.domain.transaction.entity.TransactionStatusLog;
+import com.remitroserver.api.dto.transaction.response.TransactionDetailResponse;
+import com.remitroserver.api.dto.transaction.response.TransactionStatusLogResponse;
 import com.remitroserver.api.dto.transaction.response.TransactionSummaryResponse;
 
 public class TransactionMapper {
@@ -13,5 +18,34 @@ public class TransactionMapper {
 			.amount(transaction.getAmount().getValue())
 			.createdAt(transaction.getCreatedAt())
 			.build();
+	}
+
+	public static TransactionDetailResponse toDetailResponse(
+		Transaction transaction,
+		List<TransactionStatusLog> transactionStatusLogs) {
+
+		return TransactionDetailResponse.builder()
+			.transactionToken(transaction.getTransactionToken())
+			.fromAccountNumber(transaction.getFromAccount().getAccountNumber())
+			.fromNickname(transaction.getFromAccount().getMember().getNickname())
+			.toAccountNumber(transaction.getToAccount().getAccountNumber())
+			.toNickname(transaction.getToAccount().getMember().getNickname())
+			.amount(transaction.getAmount().getValue())
+			.createdAt(transaction.getCreatedAt())
+			.statusLogs(toLogResponse(transactionStatusLogs))
+			.build();
+	}
+
+	public static TransactionStatusLogResponse toLogResponse(TransactionStatusLog transactionStatusLog) {
+		return TransactionStatusLogResponse.builder()
+			.status(transactionStatusLog.getStatus())
+			.changedAt(transactionStatusLog.getCreatedAt())
+			.build();
+	}
+
+	private static List<TransactionStatusLogResponse> toLogResponse(List<TransactionStatusLog> transactionStatusLogs) {
+		return transactionStatusLogs.stream()
+			.map(TransactionMapper::toLogResponse)
+			.toList();
 	}
 }
