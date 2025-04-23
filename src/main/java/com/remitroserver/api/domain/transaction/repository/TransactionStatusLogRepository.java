2f76bab8b64147cf59244981cee642b38694072e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-23 14:03:18 +0900

    feat: 거래 내역을 통해 거래 상태 로그를 최신순 정렬하는 조회 로직 구현

--- src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionStatusLogRepository.java
+++ src/main/java/com/remitroserver/api/domain/transaction/repository/TransactionStatusLogRepository.java
@@ -1,8 +1,13 @@
 package com.remitroserver.api.domain.transaction.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.remitroserver.api.domain.transaction.entity.Transaction;
 import com.remitroserver.api.domain.transaction.entity.TransactionStatusLog;
 
 public interface TransactionStatusLogRepository extends JpaRepository<TransactionStatusLog, Long> {
+
+	List<TransactionStatusLog> findAllByTransactionOrderByCreatedAtAsc(Transaction transaction);
 }
