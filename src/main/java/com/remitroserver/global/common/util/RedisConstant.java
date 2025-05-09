8d419cf40c602b1adc8600be39ab1f1634203075
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-05-09 18:39:11 +0900

    feat: Redis 관련 상수 정의

--- src/main/java/com/remitroserver/global/common/util/RedisConstant.java
+++ src/main/java/com/remitroserver/global/common/util/RedisConstant.java
@@ -1,5 +1,7 @@
 package com.remitroserver.global.common.util;
 
+import java.time.Duration;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -7,4 +9,10 @@ import lombok.NoArgsConstructor;
 public class RedisConstant {
 
 	public static final String REFRESH_TOKEN_KEY_PREFIX = "REFRESH_TOKEN:";
+	public static final String ACCOUNT_FAIL_KEY_PREFIX = "ACCOUNT_FAIL:";
+	public static final String ACCOUNT_LOCK_KEY_PREFIX = "ACCOUNT_LOCK:";
+
+	public static final Duration ACCOUNT_LOCK_EXPIRATION = Duration.ofMinutes(5);
+
+	public static final long SECONDS_PER_MINUTE = 60L;
 }
