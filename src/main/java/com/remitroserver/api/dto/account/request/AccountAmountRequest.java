859df5272da03c51b3c37504189f4760e647d511
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-05-09 18:38:14 +0900

    feat: 계좌 비밀번호 필드 추가

--- src/main/java/com/remitroserver/api/dto/account/request/AccountAmountRequest.java
+++ src/main/java/com/remitroserver/api/dto/account/request/AccountAmountRequest.java
@@ -1,6 +1,7 @@
 package com.remitroserver.api.dto.account.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
@@ -11,6 +12,10 @@ public record AccountAmountRequest(
 	@Schema(description = "입금 또는 출금할 금액", example = "10,000", minimum = "1", type = "integer")
 	@NotNull(message = "입금 또는 출금 금액은 필수 항목입니다.")
 	@Positive(message = "입금 또는 출금 금액은 1원 이상이어야 합니다.")
-	Long amount
+	Long amount,
+
+	@Schema(description = "사용자 계좌 비밀번호", example = "encodedAccountPassword")
+	@NotBlank(message = "계좌 비밀번호를 입력해주세요.")
+	String accountPassword
 ) {
 }
