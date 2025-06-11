67b30347231e19e3ff127854e84bc46eb60566bc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-11 19:35:15 +0900

    refactor: 입금 출금 생성 로직 이동

--- src/main/java/com/remitroserver/api/application/transaction/TransactionService.java
+++ src/main/java/com/remitroserver/api/application/transaction/TransactionService.java
@@ -17,15 +17,18 @@ import com.remitroserver.api.domain.account.entity.Account;
 import com.remitroserver.api.domain.account.model.Money;
 import com.remitroserver.api.domain.auth.model.AuthMember;
 import com.remitroserver.api.domain.member.entity.Member;
-import com.remitroserver.api.domain.transaction.entity.Transaction;
 import com.remitroserver.api.domain.transaction.entity.StatusLog;
+import com.remitroserver.api.domain.transaction.entity.Transaction;
 import com.remitroserver.api.domain.transaction.repository.StatusLogRepository;
+import com.remitroserver.api.dto.account.request.AccountAmountRequest;
 import com.remitroserver.api.dto.transaction.request.TransactionSearchRequest;
 import com.remitroserver.api.dto.transaction.request.TransferRequest;
 import com.remitroserver.api.dto.transaction.response.TransactionDetailResponse;
 import com.remitroserver.api.dto.transaction.response.TransactionSummaryResponse;
 import com.remitroserver.global.error.exception.BadRequestException;
+import com.remitroserver.global.lock.aop.DistributedLock;
 import com.remitroserver.global.lock.core.DistributedLockExecutor;
+import com.remitroserver.global.lock.core.RetryExecutor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -40,6 +43,24 @@ public class TransactionService {
 	private final TransactionReadService transactionReadService;
 	private final TransactionWriteService transactionWriteService;
 	private final DistributedLockExecutor distributedLockExecutor;
+	private final RetryExecutor retryExecutor;
+
+	@DistributedLock(key = "#accountToken")
+	public void requestDeposit(UUID accountToken, AuthMember authMember, AccountAmountRequest accountAmountRequest) {
+		final Member member = memberReadService.getMemberByEmail(authMember.email());
+
+		retryExecutor.execute(() ->
+			transactionWriteService.processDeposit(accountToken, member, accountAmountRequest.amount()));
+	}
+
+	@DistributedLock(key = "#accountToken")
+	public void requestWithdraw(UUID accountToken, AuthMember authMember, AccountAmountRequest accountAmountRequest) {
+		final Member member = memberReadService.getMemberByEmail(authMember.email());
+
+		retryExecutor.execute(() ->
+			transactionWriteService.processWithdraw(
+				accountToken, member, accountAmountRequest.amount(), accountAmountRequest.accountPassword()));
+	}
 
 	public void requestTransfer(AuthMember authMember, String idempotencyKey, TransferRequest transferRequest) {
 		transactionReadService.validateIdempotencyKeyExists(idempotencyKey);
