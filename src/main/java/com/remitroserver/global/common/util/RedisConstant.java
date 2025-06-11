e8714a53a9a594c985310aa80e193a1ec86bf816
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-06-11 20:55:30 +0900

    feat: 멱등성 Redis 키 값 정의

--- src/main/java/com/remitroserver/global/common/util/RedisConstant.java
+++ src/main/java/com/remitroserver/global/common/util/RedisConstant.java
@@ -11,8 +11,11 @@ public class RedisConstant {
 	public static final String REFRESH_TOKEN_KEY_PREFIX = "REFRESH_TOKEN:";
 	public static final String ACCOUNT_FAIL_KEY_PREFIX = "ACCOUNT_FAIL:";
 	public static final String ACCOUNT_LOCK_KEY_PREFIX = "ACCOUNT_LOCK:";
+	public static final String IDEMPOTENCY_KEY_PREFIX = "IDEMPOTENCY_KEY:";
+	public static final String IDEMPOTENCY_TRANSACTION_TOKEN_KEY_PREFIX = "IDEMPOTENCY_TRANSACTION_TOKEN_KEY:";
 
 	public static final Duration ACCOUNT_LOCK_EXPIRATION = Duration.ofMinutes(5);
+	public static final Duration IDEMPOTENCY_EXPIRATION = Duration.ofMinutes(10);
 
 	public static final long SECONDS_PER_MINUTE = 60L;
 }
