763bd78f2f6d56b0dbcb2760a1a1e0b9b941e05e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 02:40:59 +0900

    feat: 이메일로 회원 객체 조회 로직 구현

--- remitro-member/src/main/java/com/remitro/member/domain/repository/MemberRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.remitro.member.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.remitro.member.domain.model.Member;
@@ -9,4 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
 
 	boolean existsByNickname(String nickname);
+
+	Optional<Member> findByEmail(String email);
 }
