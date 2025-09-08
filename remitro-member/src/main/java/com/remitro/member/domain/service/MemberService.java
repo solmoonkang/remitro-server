ce25ee58cd541eb409c30a9d7b3ff4b7a24fbca4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 02:42:51 +0900

    feat: 회원 정보 수정 서비스 구현

--- remitro-member/src/main/java/com/remitro/member/domain/service/MemberService.java
+++ remitro-member/src/main/java/com/remitro/member/domain/service/MemberService.java
@@ -3,8 +3,11 @@ package com.remitro.member.domain.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.remitro.common.auth.model.AuthMember;
 import com.remitro.member.application.dto.request.SignUpRequest;
+import com.remitro.member.application.dto.request.UpdateMemberRequest;
 import com.remitro.member.application.validator.MemberValidator;
+import com.remitro.member.domain.model.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +18,7 @@ public class MemberService {
 
 	private final MemberValidator memberValidator;
 	private final MemberWriteService memberWriteService;
+	private final MemberReadService memberReadService;
 
 	@Transactional
 	public void signUpMember(SignUpRequest signUpRequest) {
@@ -22,5 +26,12 @@ public class MemberService {
 		memberValidator.validateNicknameNotDuplicated(signUpRequest.nickname());
 		memberValidator.validatePasswordMatche(signUpRequest.password(), signUpRequest.checkPassword());
 		memberWriteService.createMember(signUpRequest);
+	}
+
+	@Transactional
+	public void updateMember(AuthMember authMember, UpdateMemberRequest updateMemberRequest) {
+		final Member member = memberReadService.findMemberById(authMember.id());
+		memberValidator.validateNicknameNotDuplicated(updateMemberRequest.nickname());
+		memberWriteService.updateMember(member, updateMemberRequest);
 	}
 }
