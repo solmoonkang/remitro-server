96688cdcbc74c638024b106f8187cf6d0501207e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 00:31:53 +0900

    refactor: 예외 상황 명시 및 검증 로직명 수정

--- remitro-member/src/main/java/com/remitro/member/domain/service/MemberService.java
+++ remitro-member/src/main/java/com/remitro/member/domain/service/MemberService.java
@@ -4,6 +4,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.remitro.common.error.exception.BadRequestException;
+import com.remitro.common.error.exception.ConflictException;
+import com.remitro.common.error.model.ErrorMessage;
 import com.remitro.member.application.dto.request.SignUpRequest;
 import com.remitro.member.domain.model.Member;
 import com.remitro.member.domain.repository.MemberRepository;
@@ -15,14 +18,14 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class MemberService {
 
-	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final MemberRepository memberRepository;
 
 	@Transactional
 	public void signUpMember(SignUpRequest signUpRequest) {
-		validateDuplicateEmail(signUpRequest.email());
-		validateDuplicateNickname(signUpRequest.nickname());
-		validatePasswordMatches(signUpRequest.password(), signUpRequest.checkPassword());
+		validateEmailNotDuplicated(signUpRequest.email());
+		validateNicknameNotDuplicated(signUpRequest.nickname());
+		validatePasswordMatche(signUpRequest.password(), signUpRequest.checkPassword());
 
 		final String encodedPassword = passwordEncoder.encode(signUpRequest.password());
 		final Member member = Member.createMember(signUpRequest.email(), encodedPassword, signUpRequest.nickname());
@@ -30,21 +33,21 @@ public class MemberService {
 		memberRepository.save(member);
 	}
 
-	private void validateDuplicateEmail(String email) {
+	private void validateEmailNotDuplicated(String email) {
 		if (memberRepository.existsByEmail(email)) {
-			throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+			throw new ConflictException(ErrorMessage.EMAIL_DUPLICATED);
 		}
 	}
 
-	private void validateDuplicateNickname(String nickname) {
+	private void validateNicknameNotDuplicated(String nickname) {
 		if (memberRepository.existsByNickname(nickname)) {
-			throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+			throw new ConflictException(ErrorMessage.NICKNAME_DUPLICATED);
 		}
 	}
 
-	private void validatePasswordMatches(String password, String checkPassword) {
+	private void validatePasswordMatche(String password, String checkPassword) {
 		if (!password.equals(checkPassword)) {
-			throw new IllegalArgumentException("비밀번호와 확인 비밀번호가 서로 일치하지 않습니다.");
+			throw new BadRequestException(ErrorMessage.INVALID_PASSWORD);
 		}
 	}
 }
