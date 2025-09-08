d3fad6e52d67320082e50f258d44ae3b6e9cdaea
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-08 20:08:31 +0900

    feat: 사용자 회원가입 엔드포인트 설정

---
+++ remitro-member/src/main/java/com/remitro/member/application/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.remitro.member.application.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.remitro.member.application.dto.request.SignUpRequest;
+import com.remitro.member.domain.service.MemberService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/signUp")
+	public ResponseEntity<?> signUpMember(@Valid @RequestBody SignUpRequest signUpRequest) {
+		memberService.signUpMember(signUpRequest);
+		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 회원가입을 성공적으로 완료했습니다.");
+	}
+}
