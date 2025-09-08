3466326bb3db368fe1167611791e0c894c859d01
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 00:31:28 +0900

    feat: 예외 응답 포맷 정의

---
+++ remitro-common/src/main/java/com/remitro/common/error/model/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.remitro.common.error.model;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public record ErrorResponse(
+	String message,
+	@JsonInclude(JsonInclude.Include.NON_EMPTY) Map<String, String> validation
+) {
+}
