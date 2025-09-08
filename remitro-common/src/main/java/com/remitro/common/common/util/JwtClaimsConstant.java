c080dc97f5db376805271d04c996263ed26f0cb1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 01:15:26 +0900

    feat: JWT 클레임 관련 상수 정의

---
+++ remitro-common/src/main/java/com/remitro/common/common/util/JwtClaimsConstant.java
@@ -0,0 +1,12 @@
+package com.remitro.common.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JwtClaimsConstant {
+
+	public static final String MEMBER_ID = "id";
+	public static final String MEMBER_EMAIL = "email";
+	public static final String MEMBER_NICKNAME = "nickname";
+}
