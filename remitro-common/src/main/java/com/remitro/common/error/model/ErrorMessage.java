90f85db236cd82c5434f48c9a26a11380b1e38a0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 00:31:16 +0900

    feat: 예외 메시지 상수 정의

---
+++ remitro-common/src/main/java/com/remitro/common/error/model/ErrorMessage.java
@@ -0,0 +1,23 @@
+package com.remitro.common.error.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorMessage {
+
+	INVALID_PASSWORD("입력하신 비밀번호는 틀린 비밀번호입니다."),
+
+	EMAIL_DUPLICATED("이미 사용중인 이메일입니다."),
+	NICKNAME_DUPLICATED("이미 사용중인 닉네임입니다."),
+
+	UNKNOWN_SERVER_ERROR("서버에서 알 수 없는 에러가 발생했습니다.");
+
+	private final String message;
+
+	public String formattedMessage(Object... args) {
+		return String.format(this.message, args);
+	}
+}
