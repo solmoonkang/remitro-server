f9ab7c942d666407d514c6f501a66c185656162e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 01:14:48 +0900

    feat: 공통 유틸 remitro.common 모듈 사용 명시

--- remitro-member/src/main/java/com/remitro/member/MemberServiceApplication.java
+++ remitro-member/src/main/java/com/remitro/member/MemberServiceApplication.java
@@ -2,8 +2,10 @@ package com.remitro.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.remitro.common")
 public class MemberServiceApplication {
 
 	public static void main(String[] args) {
