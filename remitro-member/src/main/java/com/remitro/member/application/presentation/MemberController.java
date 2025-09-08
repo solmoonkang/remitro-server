a82014989e2569623dbbc04340b693c0cb15993a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 02:42:58 +0900

    feat: 회원 정보 수정 엔드포인트 설정

--- remitro-member/src/main/java/com/remitro/member/application/presentation/MemberController.java
+++ remitro-member/src/main/java/com/remitro/member/application/presentation/MemberController.java
@@ -2,11 +2,15 @@ package com.remitro.member.application.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.remitro.common.auth.annotation.Auth;
+import com.remitro.common.auth.model.AuthMember;
 import com.remitro.member.application.dto.request.SignUpRequest;
+import com.remitro.member.application.dto.request.UpdateMemberRequest;
 import com.remitro.member.domain.service.MemberService;
 
 import jakarta.validation.Valid;
@@ -19,9 +23,18 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@PostMapping("/signUp")
+	@PostMapping("/signup")
 	public ResponseEntity<?> signUpMember(@Valid @RequestBody SignUpRequest signUpRequest) {
 		memberService.signUpMember(signUpRequest);
 		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 회원가입을 성공적으로 완료했습니다.");
+	}
+
+	@PutMapping
+	public ResponseEntity<?> updateMember(
+		@Auth AuthMember authMember,
+		@Valid @RequestBody UpdateMemberRequest updateMemberRequest) {
+
+		memberService.updateMember(authMember, updateMemberRequest);
+		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 정보 수정을 성공적으로 완료했습니다.");
 	}
 }
