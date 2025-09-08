85279884e77aa4c74ce30c9c08a77aa94624f949
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 02:41:59 +0900

    feat: 회원 객체 조회 로직 구현

---
+++ remitro-member/src/main/java/com/remitro/member/domain/service/MemberReadService.java
@@ -0,0 +1,27 @@
+package com.remitro.member.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.remitro.common.error.exception.NotFoundException;
+import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.member.domain.model.Member;
+import com.remitro.member.domain.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberReadService {
+
+	private final MemberRepository memberRepository;
+
+	public Member findMemberById(Long id) {
+		return memberRepository.findById(id)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
+	}
+
+	public Member findMemberByEmail(String email) {
+		return memberRepository.findByEmail(email)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
+	}
+}
