36ca8cc08ec50c0803a5233076b10c4e86d29dae
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-09 02:42:17 +0900

    feat: 회원가입, 로그인 요청 필터 통과 설정

--- remitro-common/src/main/java/com/remitro/common/config/SecurityConfig.java
+++ remitro-common/src/main/java/com/remitro/common/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.remitro.common.config;
 
+import static org.springframework.http.HttpMethod.*;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -22,12 +26,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+	private static final String[] DEV_TOOLS_ENDPOINTS = {
+		"/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"
+	};
+	private static final String[] AUTHENTICATION_REQUEST_ENDPOINTS = {
+		"/api/signup", "/api/login"
+	};
+
 	private final JwtProvider jwtProvider;
 	private final HandlerExceptionResolver handlerExceptionResolver;
 
 	@Bean
 	public static PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+			.requestMatchers(DEV_TOOLS_ENDPOINTS)
+			.requestMatchers(POST, AUTHENTICATION_REQUEST_ENDPOINTS);
 	}
 
 	@Bean
