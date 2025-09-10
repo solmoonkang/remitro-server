e43a6a1c2bf6aae06a96fdfebaff61ee6687adc8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-10 16:46:03 +0900

    feat: 계좌 생성 및 조회 관련 서비스 구현

---
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountService.java
@@ -0,0 +1,75 @@
+package com.remitro.account.domain.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.account.application.dto.request.AccountDepositRequest;
+import com.remitro.account.application.dto.request.AccountPasswordRequest;
+import com.remitro.account.application.dto.request.AccountWithdrawRequest;
+import com.remitro.account.application.dto.request.CreateAccountRequest;
+import com.remitro.account.application.dto.request.UpdateStatusRequest;
+import com.remitro.account.application.dto.response.AccountDetailResponse;
+import com.remitro.account.application.mapper.AccountMapper;
+import com.remitro.account.application.validator.AccountValidator;
+import com.remitro.account.domain.model.Account;
+import com.remitro.common.auth.model.AuthMember;
+import com.remitro.member.domain.model.Member;
+import com.remitro.member.domain.service.MemberReadService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AccountService {
+
+	private final AccountValidator accountValidator;
+	private final MemberReadService memberReadService;
+	private final AccountWriteService accountWriteService;
+	private final AccountReadService accountReadService;
+
+	@Transactional
+	public void createAccount(AuthMember authMember, CreateAccountRequest createAccountRequest) {
+		final Member member = memberReadService.findMemberById(authMember.id());
+		accountWriteService.saveAccount(member, createAccountRequest);
+	}
+
+	public AccountDetailResponse findAccountDetail(AuthMember authMember, Long accountId,
+		AccountPasswordRequest accountPasswordRequest) {
+
+		final Account account = accountReadService.findAccountById(accountId);
+		accountValidator.validateAccountAccess(account.getMember().getId(), authMember.id());
+		accountValidator.validateAccountPasswordMatch(accountPasswordRequest.password(), account.getPassword());
+		return AccountMapper.toAccountDetailResponse(account);
+	}
+
+	public List<AccountDetailResponse> findAllAccounts(AuthMember authMember) {
+		final List<Account> accounts = accountReadService.findAllAccountsByMemberId(authMember.id());
+		return AccountMapper.toAccountListResponse(accounts);
+	}
+
+	@Transactional
+	public void depositToAccount(Long accountId, AccountDepositRequest accountDepositRequest) {
+		accountValidator.validateAmountPositive(accountDepositRequest.amount());
+		final Account account = accountReadService.findAccountById(accountId);
+		account.deposit(accountDepositRequest.amount());
+	}
+
+	@Transactional
+	public void withdrawToAccount(Long accountId, AccountWithdrawRequest accountWithdrawRequest) {
+		accountValidator.validateAmountPositive(accountWithdrawRequest.amount());
+		final Account account = accountReadService.findAccountById(accountId);
+		accountValidator.validateAccountPasswordMatch(accountWithdrawRequest.password(), account.getPassword());
+		account.withdraw(accountWithdrawRequest.amount());
+	}
+
+	@Transactional
+	public void updateAccountStatus(AuthMember authMember, Long accountId, UpdateStatusRequest updateStatusRequest) {
+		final Account account = accountReadService.findAccountById(accountId);
+		accountValidator.validateAccountAccess(account.getMember().getId(), authMember.id());
+		accountValidator.validateAccountPasswordMatch(updateStatusRequest.password(), account.getPassword());
+		accountWriteService.updateAccountStatus(account, updateStatusRequest);
+	}
+}
