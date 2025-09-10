b6a4f78764d58e11d08871f9052eb7eb361cf39e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-10 16:44:36 +0900

    feat: 계좌 상태 변경 요청 DTO 구현

---
+++ remitro-account/src/main/java/com/remitro/account/application/dto/request/UpdateStatusRequest.java
@@ -0,0 +1,14 @@
+package com.remitro.account.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record UpdateStatusRequest(
+	@NotBlank(message = "변경할 계좌 상태를 입력해주세요.")
+	String accountStatus,
+
+	@NotBlank(message = "계좌 비밀번호를 입력해주세요.")
+	String password
+) {
+}
