76a66215e59fcaf505f0e48f73c90cf7ad9ab339
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-10 16:43:11 +0900

    feat: JPA Auditing 어노테이션 추가

--- remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
+++ remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
@@ -2,8 +2,12 @@ package com.remitro.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan(basePackages = "com.remitro.member")
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
