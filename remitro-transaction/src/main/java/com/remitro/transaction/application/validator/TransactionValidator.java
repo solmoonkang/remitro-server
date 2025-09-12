3261bf54f80e29a844b7ea534788bf140ac4f7ef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-12 17:25:01 +0900

    feat: 거래내역 접근 권한 확인 검증 로직 구현

---
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/validator/TransactionValidator.java
@@ -0,0 +1,17 @@
+package com.remitro.transaction.application.validator;
+
+import org.springframework.stereotype.Component;
+
+import com.remitro.common.error.exception.ForbiddenException;
+import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.transaction.domain.model.Transaction;
+
+@Component
+public class TransactionValidator {
+
+	public void validateTransactionAccess(Long loginMemberId, Transaction transaction) {
+		if (!transaction.isSentBy(loginMemberId) && !transaction.isReceivedBy(loginMemberId)) {
+			throw new ForbiddenException(ErrorMessage.TRANSACTION_ACCESS_FORBIDDEN);
+		}
+	}
+}
