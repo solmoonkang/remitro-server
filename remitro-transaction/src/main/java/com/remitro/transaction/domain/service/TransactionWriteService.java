ede6eb8e60d31a15dd7b79dc474e2f29d4d14d40
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-12 17:25:20 +0900

    feat: 거래내역 송금, 입금, 출금 쓰기 작업 구현

---
+++ remitro-transaction/src/main/java/com/remitro/transaction/domain/service/TransactionWriteService.java
@@ -0,0 +1,28 @@
+package com.remitro.transaction.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.remitro.transaction.domain.model.Transaction;
+import com.remitro.transaction.domain.repository.TransactionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionWriteService {
+
+	private final TransactionRepository transactionRepository;
+
+	public void saveTransferTransaction(Transaction senderTransaction, Transaction receiverTransaction) {
+		transactionRepository.save(senderTransaction);
+		transactionRepository.save(receiverTransaction);
+	}
+
+	public void saveDepositTransaction(Transaction depositTransaction) {
+		transactionRepository.save(depositTransaction);
+	}
+
+	public void saveWithdrawalTransaction(Transaction drawalTransaction) {
+		transactionRepository.save(drawalTransaction);
+	}
+}
