736d895f32a18244c5414fdab86111e80439b586
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-16 16:04:20 +0900

    feat: 트랜잭션 관련 상수 정의

---
+++ remitro-common/src/main/java/com/remitro/common/common/util/TransactionConstant.java
@@ -0,0 +1,12 @@
+package com.remitro.common.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TransactionConstant {
+
+	public static final String TRANSFER = "TRANSFER";
+	public static final String DEPOSIT = "DEPOSIT";
+	public static final String WITHDRAWAL = "WITHDRAWAL";
+}
