994c0e24370c128fc029b99409ef695ffb8c6a9f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-16 16:02:49 +0900

    feat: Kafka 관련 상수 정의

---
+++ remitro-common/src/main/java/com/remitro/common/common/util/KafkaConstant.java
@@ -0,0 +1,11 @@
+package com.remitro.common.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class KafkaConstant {
+
+	public static final String TOPIC_TRANSACTION_EVENTS = "remitro.events.transactions";
+	public static final String GROUP_TRANSACTION_CONSUMERS = "remitro.transaction.group";
+}
