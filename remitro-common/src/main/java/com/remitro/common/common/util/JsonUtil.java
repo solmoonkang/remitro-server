4cde0293bfd2a699111afc528a73dfefe21078b8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-16 16:01:32 +0900

    feat: JSON 메시지 직렬화 및 역직렬화 유틸 클래스 정의

---
+++ remitro-common/src/main/java/com/remitro/common/common/util/JsonUtil.java
@@ -0,0 +1,29 @@
+package com.remitro.common.common.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JsonUtil {
+
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	public static String toJSON(Object message) {
+		try {
+			return objectMapper.writeValueAsString(message);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static <T> T fromJSON(String message, Class<T> clazz) {
+		try {
+			return objectMapper.readValue(message, clazz);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
