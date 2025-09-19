f7749f02c9d8228cdaf21a0fb982e91c8fb78742
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-19 15:08:51 +0900

    feat: 멱등성 키 중복 여부 검증 로직 구현

--- remitro-transaction/src/main/java/com/remitro/transaction/application/validator/TransactionValidator.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/validator/TransactionValidator.java
@@ -2,16 +2,29 @@ package com.remitro.transaction.application.validator;
 
 import org.springframework.stereotype.Component;
 
+import com.remitro.common.error.exception.ConflictException;
 import com.remitro.common.error.exception.ForbiddenException;
 import com.remitro.common.error.model.ErrorMessage;
 import com.remitro.transaction.domain.model.Transaction;
+import com.remitro.transaction.domain.service.TransactionReadService;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class TransactionValidator {
+
+	private final TransactionReadService transactionReadService;
 
 	public void validateTransactionAccess(Long loginMemberId, Transaction transaction) {
 		if (!transaction.isSentBy(loginMemberId) && !transaction.isReceivedBy(loginMemberId)) {
 			throw new ForbiddenException(ErrorMessage.TRANSACTION_ACCESS_FORBIDDEN);
+		}
+	}
+
+	public void validateIdempotencyKeyNotDuplicated(String idempotencyKey) {
+		if (transactionReadService.existsByIdempotencyKey(idempotencyKey)) {
+			throw new ConflictException(ErrorMessage.IDEMPOTENCY_KEY_DUPLICATED);
 		}
 	}
 }
