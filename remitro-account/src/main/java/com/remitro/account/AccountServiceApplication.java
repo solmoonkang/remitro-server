1a405143b82143c8f1186fba2c3fee5dd985b94b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-29 21:27:58 +0900

    refactor: Redisson 기본 자동 구성 비활성화

--- remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
+++ remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
@@ -1,13 +1,18 @@
 package com.remitro.account;
 
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = {RedissonAutoConfiguration.class}
+)
 @EnableJpaAuditing
-@ComponentScan(basePackages = {"com.remitro.member", "com.remitro.common"})
+@ComponentScan(
+	basePackages = {"com.remitro.account", "com.remitro.member", "com.remitro.common"}
+)
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
