07f61550bb2e3b72f0281d41a17ce44f4184aeef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-09-29 21:27:05 +0900

    refactor: 비관적 락 제거

--- remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
@@ -4,23 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.remitro.account.domain.model.Account;
 
-import jakarta.persistence.LockModeType;
-
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT a FROM Account a WHERE a.id = :accountId")
 	Optional<Account> findByAccountId(@Param("accountId") Long accountId);
 
 	List<Account> findByMemberId(Long memberId);
 
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<Account> findByAccountNumber(String accountNumber);
 
 	@Query("SELECT a.id FROM Account a WHERE a.accountNumber = :accountNumber")
