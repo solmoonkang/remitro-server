790715e9673ffe20951b780bcd7fd4a78771bfd6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-10-07 17:59:34 +0900

    refactor: 입금, 출금, 송금 로직 분리

--- remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
@@ -4,13 +4,24 @@ import java.util.Random;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.remitro.account.application.dto.request.AccountDepositRequest;
+import com.remitro.account.application.dto.request.AccountWithdrawRequest;
 import com.remitro.account.application.dto.request.CreateAccountRequest;
+import com.remitro.account.application.dto.request.CreatePublishedEventRequest;
+import com.remitro.account.application.dto.request.TransferFormRequest;
 import com.remitro.account.application.dto.request.UpdateStatusRequest;
+import com.remitro.account.application.mapper.EventMapper;
 import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.enums.AccountStatus;
 import com.remitro.account.domain.model.enums.AccountType;
 import com.remitro.account.domain.repository.AccountRepository;
+import com.remitro.common.common.entity.enums.EventType;
+import com.remitro.common.common.event.DepositEventMessage;
+import com.remitro.common.common.event.TransferEventMessage;
+import com.remitro.common.common.event.WithdrawEventMessage;
 import com.remitro.member.domain.model.Member;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +34,7 @@ public class AccountWriteService {
 
 	private final PasswordEncoder passwordEncoder;
 	private final AccountRepository accountRepository;
+	private final PublishedEventService publishedEventService;
 
 	public void saveAccount(Member member, CreateAccountRequest createAccountRequest) {
 		final AccountType accountType = AccountType.valueOf(createAccountRequest.accountType().toUpperCase());
@@ -39,6 +51,31 @@ public class AccountWriteService {
 		account.updateAccountStatus(newAccountStatus);
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void processDeposit(Account receiver, AccountDepositRequest accountDepositRequest) {
+		receiver.deposit(accountDepositRequest.amount());
+		final DepositEventMessage depositEventMessage = EventMapper.toDepositEventMessage(
+			receiver.getAccountNumber(), accountDepositRequest.amount());
+		recordPublishedEventForAccount(receiver, EventType.DEPOSIT_EVENT, depositEventMessage);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void processWithdraw(Account sender, AccountWithdrawRequest accountWithdrawRequest) {
+		sender.withdraw(accountWithdrawRequest.amount());
+		final WithdrawEventMessage withdrawEventMessage = EventMapper.toWithdrawEventMessage(
+			sender.getAccountNumber(), accountWithdrawRequest.amount());
+		recordPublishedEventForAccount(sender, EventType.WITHDRAWAL_EVENT, withdrawEventMessage);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void processTransfer(Account sender, Account receiver, TransferFormRequest transferFormRequest) {
+		sender.withdraw(transferFormRequest.amount());
+		receiver.deposit(transferFormRequest.amount());
+		final TransferEventMessage transferEventMessage = EventMapper.toTransferEventMessage(
+			sender, receiver, transferFormRequest);
+		recordPublishedEventForAccount(sender, EventType.TRANSFER_EVENT, transferEventMessage);
+	}
+
 	private String generateRandomAccountNumber() {
 		Random random = new Random();
 		StringBuilder randomNumberBuilder = new StringBuilder();
@@ -48,5 +85,11 @@ public class AccountWriteService {
 		}
 
 		return randomNumberBuilder.toString();
+	}
+
+	private <T> void recordPublishedEventForAccount(Account account, EventType eventType, T eventMessage) {
+		final CreatePublishedEventRequest createPublishedEventRequest = EventMapper.toCreatePublishedEventRequest(
+			account, eventType, eventMessage);
+		publishedEventService.recordPublishedEvent(createPublishedEventRequest);
 	}
 }
