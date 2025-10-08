9fe5d98498f70ceeb50b3e49bf833546f07f3336
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-10-08 13:32:44 +0900

    refactor: 입출금, 송금 시 분산 락 적용

--- remitro-account/src/main/java/com/remitro/account/domain/service/AccountService.java
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountService.java
@@ -9,19 +9,14 @@ import com.remitro.account.application.dto.request.AccountDepositRequest;
 import com.remitro.account.application.dto.request.AccountPasswordRequest;
 import com.remitro.account.application.dto.request.AccountWithdrawRequest;
 import com.remitro.account.application.dto.request.CreateAccountRequest;
-import com.remitro.account.application.dto.request.CreatePublishedEventRequest;
 import com.remitro.account.application.dto.request.TransferFormRequest;
 import com.remitro.account.application.dto.request.UpdateStatusRequest;
 import com.remitro.account.application.dto.response.AccountDetailResponse;
 import com.remitro.account.application.mapper.AccountMapper;
-import com.remitro.account.application.mapper.EventMapper;
 import com.remitro.account.application.validator.AccountValidator;
 import com.remitro.account.domain.model.Account;
+import com.remitro.account.infrastructure.redis.DistributedLockManager;
 import com.remitro.common.auth.model.AuthMember;
-import com.remitro.common.common.entity.enums.EventType;
-import com.remitro.common.common.event.DepositEventMessage;
-import com.remitro.common.common.event.TransferEventMessage;
-import com.remitro.common.common.event.WithdrawEventMessage;
 import com.remitro.member.domain.model.Member;
 import com.remitro.member.domain.service.MemberReadService;
 
@@ -36,7 +31,7 @@ public class AccountService {
 	private final MemberReadService memberReadService;
 	private final AccountWriteService accountWriteService;
 	private final AccountReadService accountReadService;
-	private final PublishedEventService publishedEventService;
+	private final DistributedLockManager distributedLockManager;
 
 	@Transactional
 	public void createAccount(AuthMember authMember, CreateAccountRequest createAccountRequest) {
@@ -61,42 +56,40 @@ public class AccountService {
 	@Transactional
 	public void depositToAccount(Long accountId, AccountDepositRequest accountDepositRequest) {
 		accountValidator.validateAmountPositive(accountDepositRequest.amount());
-		final Account receiver = accountReadService.findAccountById(accountId);
-		receiver.deposit(accountDepositRequest.amount());
-
-		final DepositEventMessage depositEventMessage = EventMapper.toDepositEventMessage(
-			receiver.getAccountNumber(), accountDepositRequest.amount());
-		recordPublishedEventForAccount(receiver, EventType.DEPOSIT_EVENT, depositEventMessage);
+		distributedLockManager.executeProtectedDistributedLock(accountId, () -> {
+			final Account receiver = accountReadService.findAccountById(accountId);
+			accountWriteService.processDeposit(receiver, accountDepositRequest);
+		});
 	}
 
 	@Transactional
 	public void withdrawToAccount(Long accountId, AccountWithdrawRequest accountWithdrawRequest) {
 		accountValidator.validateAmountPositive(accountWithdrawRequest.amount());
-		final Account sender = accountReadService.findAccountById(accountId);
-		accountValidator.validateAccountPasswordMatch(accountWithdrawRequest.password(), sender.getPassword());
-		sender.withdraw(accountWithdrawRequest.amount());
 
-		final WithdrawEventMessage withdrawEventMessage = EventMapper.toWithdrawEventMessage(
-			sender.getAccountNumber(), accountWithdrawRequest.amount());
-		recordPublishedEventForAccount(sender, EventType.WITHDRAWAL_EVENT, withdrawEventMessage);
+		distributedLockManager.executeProtectedDistributedLock(accountId, () -> {
+			final Account sender = accountReadService.findAccountById(accountId);
+			accountValidator.validateAccountPasswordMatch(accountWithdrawRequest.password(), sender.getPassword());
+			accountWriteService.processWithdraw(sender, accountWithdrawRequest);
+		});
 	}
 
 	@Transactional
 	public void transferToAccount(AuthMember authMember, Long accountId, TransferFormRequest transferFormRequest) {
-		final Account sender = findAndLockSenderAccount(accountId, transferFormRequest.receiverAccountNumber());
-		final Account receiver = findAndLockReceiverAccount(accountId, transferFormRequest.receiverAccountNumber());
+		final Long receiverId = accountReadService.findAccountIdByNumber(transferFormRequest.receiverAccountNumber());
+		final Long keyA = Math.min(accountId, receiverId);
+		final Long keyB = Math.max(accountId, receiverId);
 
-		accountValidator.validateAccountAccess(sender.getMember().getId(), authMember.id());
-		accountValidator.validateAccountPasswordMatch(transferFormRequest.password(), sender.getPassword());
-		accountValidator.validateSufficientBalance(sender.getBalance(), transferFormRequest.amount());
-		accountValidator.validateSelfTransfer(sender.getId(), receiver.getId());
+		distributedLockManager.executeProtectedMultiDistributedLock(keyA, keyB, () -> {
+			final Account sender = accountReadService.findAccountById(accountId);
+			final Account receiver = accountReadService.findAccountById(receiverId);
 
-		sender.withdraw(transferFormRequest.amount());
-		receiver.deposit(transferFormRequest.amount());
+			accountValidator.validateAccountAccess(sender.getMember().getId(), authMember.id());
+			accountValidator.validateAccountPasswordMatch(transferFormRequest.password(), sender.getPassword());
+			accountValidator.validateSufficientBalance(sender.getBalance(), transferFormRequest.amount());
+			accountValidator.validateSelfTransfer(sender.getId(), receiver.getId());
 
-		final TransferEventMessage transferEventMessage = EventMapper.toTransferEventMessage(
-			sender, receiver, transferFormRequest);
-		recordPublishedEventForAccount(sender, EventType.TRANSFER_EVENT, transferEventMessage);
+			accountWriteService.processTransfer(sender, receiver, transferFormRequest);
+		});
 	}
 
 	@Transactional
@@ -105,12 +98,6 @@ public class AccountService {
 		accountValidator.validateAccountAccess(account.getMember().getId(), authMember.id());
 		accountValidator.validateAccountPasswordMatch(updateStatusRequest.password(), account.getPassword());
 		accountWriteService.updateAccountStatus(account, updateStatusRequest);
-	}
-
-	private <T> void recordPublishedEventForAccount(Account account, EventType eventType, T eventMessage) {
-		final CreatePublishedEventRequest createPublishedEventRequest = EventMapper.toCreatePublishedEventRequest(
-			account, eventType, eventMessage);
-		publishedEventService.recordPublishedEvent(createPublishedEventRequest);
 	}
 
 	private Account findAndLockSenderAccount(Long senderAccountId, String receiverAccountNumber) {
