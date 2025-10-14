01de3f5497931390ee12fc16a7b2d7f8a71b5027
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-10-14 16:33:59 +0900

    refactor: 송금 서비스 분리

---
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountTransferService.java
@@ -0,0 +1,39 @@
+package com.remitro.account.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.remitro.account.application.dto.request.TransferFormRequest;
+import com.remitro.account.application.validator.AccountValidator;
+import com.remitro.account.domain.model.Account;
+import com.remitro.account.infrastructure.redis.DistributedLockManager;
+import com.remitro.common.auth.model.AuthMember;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccountTransferService {
+
+	private final AccountValidator accountValidator;
+	private final DistributedLockManager distributedLockManager;
+	private final AccountReadService accountReadService;
+	private final AccountWriteService accountWriteService;
+
+	public void transferToAccount(AuthMember authMember, Long accountId, TransferFormRequest transferFormRequest) {
+		final Long receiverId = accountReadService.findAccountIdByNumber(transferFormRequest.receiverAccountNumber());
+		final Long keyA = Math.min(accountId, receiverId);
+		final Long keyB = Math.max(accountId, receiverId);
+
+		distributedLockManager.executeProtectedMultiDistributedLock(keyA, keyB, () -> {
+			final Account sender = accountReadService.findAccountById(accountId);
+			final Account receiver = accountReadService.findAccountById(receiverId);
+
+			accountValidator.validateAccountAccess(sender.getMember().getId(), authMember.id());
+			accountValidator.validateAccountPasswordMatch(transferFormRequest.password(), sender.getPassword());
+			accountValidator.validateSufficientBalance(sender.getBalance(), transferFormRequest.amount());
+			accountValidator.validateSelfTransfer(sender.getId(), receiver.getId());
+
+			accountWriteService.processTransfer(sender, receiver, transferFormRequest);
+		});
+	}
+}
