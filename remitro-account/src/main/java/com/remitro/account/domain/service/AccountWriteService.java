76294d56730e811768d12f8c41d620c27b0c4adc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-10-14 16:33:41 +0900

    refactor: 입금, 출금 DTO명 변경

--- remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.remitro.account.application.dto.request.AccountDepositRequest;
-import com.remitro.account.application.dto.request.AccountWithdrawRequest;
 import com.remitro.account.application.dto.request.CreateAccountRequest;
 import com.remitro.account.application.dto.request.CreatePublishedEventRequest;
+import com.remitro.account.application.dto.request.DepositFormRequest;
 import com.remitro.account.application.dto.request.TransferFormRequest;
 import com.remitro.account.application.dto.request.UpdateStatusRequest;
+import com.remitro.account.application.dto.request.WithdrawFormRequest;
 import com.remitro.account.application.mapper.EventMapper;
 import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.enums.AccountStatus;
@@ -52,28 +52,31 @@ public class AccountWriteService {
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void processDeposit(Account receiver, AccountDepositRequest accountDepositRequest) {
-		receiver.deposit(accountDepositRequest.amount());
-		final DepositEventMessage depositEventMessage = EventMapper.toDepositEventMessage(
-			receiver.getAccountNumber(), accountDepositRequest.amount());
-		recordPublishedEventForAccount(receiver, EventType.DEPOSIT_EVENT, depositEventMessage);
+	public void processDeposit(Account receiver, DepositFormRequest depositFormRequest) {
+		receiver.deposit(depositFormRequest.amount());
+
+		final DepositEventMessage eventMessage = EventMapper.toDepositEventMessage(
+			receiver.getAccountNumber(), depositFormRequest.amount());
+		recordPublishedEventForAccount(receiver, EventType.DEPOSIT_EVENT, eventMessage);
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void processWithdraw(Account sender, AccountWithdrawRequest accountWithdrawRequest) {
-		sender.withdraw(accountWithdrawRequest.amount());
-		final WithdrawEventMessage withdrawEventMessage = EventMapper.toWithdrawEventMessage(
-			sender.getAccountNumber(), accountWithdrawRequest.amount());
-		recordPublishedEventForAccount(sender, EventType.WITHDRAWAL_EVENT, withdrawEventMessage);
+	public void processWithdraw(Account sender, WithdrawFormRequest withdrawFormRequest) {
+		sender.withdraw(withdrawFormRequest.amount());
+
+		final WithdrawEventMessage eventMessage = EventMapper.toWithdrawEventMessage(
+			sender.getAccountNumber(), withdrawFormRequest.amount());
+		recordPublishedEventForAccount(sender, EventType.WITHDRAWAL_EVENT, eventMessage);
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void processTransfer(Account sender, Account receiver, TransferFormRequest transferFormRequest) {
 		sender.withdraw(transferFormRequest.amount());
 		receiver.deposit(transferFormRequest.amount());
-		final TransferEventMessage transferEventMessage = EventMapper.toTransferEventMessage(
+
+		final TransferEventMessage eventMessage = EventMapper.toTransferEventMessage(
 			sender, receiver, transferFormRequest);
-		recordPublishedEventForAccount(sender, EventType.TRANSFER_EVENT, transferEventMessage);
+		recordPublishedEventForAccount(sender, EventType.TRANSFER_EVENT, eventMessage);
 	}
 
 	private String generateRandomAccountNumber() {
