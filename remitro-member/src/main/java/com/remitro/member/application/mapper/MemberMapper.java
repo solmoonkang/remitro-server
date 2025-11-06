144cd651ea80798d04245987e181014ce1ac01fb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-06 17:17:17 +0900

    feat: 사용자 정보 응답 DTO 핼퍼 메서드 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.remitro.member.application.mapper;
+
+import com.remitro.member.application.dto.response.MemberInfoResponse;
+import com.remitro.member.domain.model.Member;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberMapper {
+
+	public static MemberInfoResponse toMemberInfoResponse(Member member) {
+		return new MemberInfoResponse(member.getEmail(), member.getNickname(), member.getPhoneNumber());
+	}
+}
