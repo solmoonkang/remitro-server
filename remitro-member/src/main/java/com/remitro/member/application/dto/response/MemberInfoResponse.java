c59c8c75cc938674fe207aa154254a6bbcab8302
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-06 17:17:10 +0900

    feat: 사용자 정보 응답 DTO 정의

---
+++ remitro-member/src/main/java/com/remitro/member/application/dto/response/MemberInfoResponse.java
@@ -0,0 +1,18 @@
+package com.remitro.member.application.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(name = "사용자 정보 응답 DTO")
+public record MemberInfoResponse(
+	@Schema(name = "사용자 이메일", example = "member@example.com")
+	String email,
+
+	@Schema(name = "사용자 닉네임", example = "memberNickname")
+	String nickname,
+
+	@Schema(name = "사용자 전화번호", example = "memberPhoneNumber")
+	String phoneNumber
+) {
+}
