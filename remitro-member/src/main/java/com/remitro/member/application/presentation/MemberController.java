5974ab2ad83bc020ec01ea93011fcc9a5322b6fa
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-06 17:17:34 +0900

    feat: 사용자 정보 조회 엔드포인트 설정

--- remitro-member/src/main/java/com/remitro/member/application/presentation/MemberController.java
+++ remitro-member/src/main/java/com/remitro/member/application/presentation/MemberController.java
@@ -2,13 +2,17 @@ package com.remitro.member.application.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.remitro.common.auth.annotation.Auth;
+import com.remitro.common.auth.model.AuthMember;
 import com.remitro.member.application.dto.request.SignUpRequest;
+import com.remitro.member.application.dto.response.MemberInfoResponse;
 import com.remitro.member.domain.service.MemberService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,7 +25,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
-@Tag(name = "사용자 APIs", description = "회원가입 및 사용자 정보 수정 API")
+@Tag(name = "사용자 APIs", description = "회원가입 및 사용자 정보 조회 API")
 public class MemberController {
 
 	private final MemberService memberService;
@@ -39,5 +43,18 @@ public class MemberController {
 	public ResponseEntity<?> signUpMember(@Valid @RequestBody SignUpRequest signUpRequest) {
 		memberService.signUpMember(signUpRequest);
 		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 회원가입을 성공적으로 완료했습니다.");
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "사용자 정보 조회", description = "사용자 정보 조회를 진행합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "🎉 사용자 정보 조회 성공"),
+		@ApiResponse(responseCode = "401", description = "🔒 인증되지 않은 토큰"),
+		@ApiResponse(responseCode = "404", description = "🔍 존재하지 않는 사용자"),
+		@ApiResponse(responseCode = "500", description = "💥 서버 내부 오류")
+	})
+	public ResponseEntity<MemberInfoResponse> findMemberInfo(@Auth AuthMember authMember) {
+		return ResponseEntity.ok().body(memberService.findMemberInfo(authMember));
 	}
 }
