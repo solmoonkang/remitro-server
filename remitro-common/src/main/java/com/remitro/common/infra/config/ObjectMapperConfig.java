0adc48c8a9b15c5e836cb51897a1c395a4750259
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-10 18:20:47 +0900

    feat: ObjectMapper 설정 클래스 정의

---
+++ remitro-common/src/main/java/com/remitro/common/infra/config/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.remitro.common.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class ObjectMapperConfig {
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper()
+			.findAndRegisterModules()
+			.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+			.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
+}
