af730e1cb48b9348ebaff0d01581a69bac36c772
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-10 18:20:34 +0900

    feat: 계좌 개설 응답 DTO 정의

---
+++ remitro-account/src/main/java/com/remitro/account/application/dto/response/OpenAccontCreationResponse.java
@@ -0,0 +1,15 @@
+package com.remitro.account.application.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(name = "계좌 개설 응답 DTO")
+public record OpenAccontCreationResponse(
+	@Schema(description = "계좌 ID", example = "999L")
+	Long accountId,
+
+	@Schema(description = "계좌 번호", example = "110-431-402158")
+	String accountNumber
+) {
+}
