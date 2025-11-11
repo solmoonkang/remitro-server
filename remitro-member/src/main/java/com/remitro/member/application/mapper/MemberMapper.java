15be8da8315fc6539cd728f2d5acf0525379d79d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-11 16:59:34 +0900

    feat: 사용자 이벤트 변경 감지 DTO 헬퍼 메서드 추가

--- remitro-member/src/main/java/com/remitro/member/application/mapper/MemberMapper.java
+++ remitro-member/src/main/java/com/remitro/member/application/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package com.remitro.member.application.mapper;
 
+import com.remitro.common.contract.member.MemberStatusChangedEvent;
 import com.remitro.member.application.dto.response.MemberInfoResponse;
 import com.remitro.member.domain.model.Member;
 
@@ -11,5 +12,9 @@ public class MemberMapper {
 
 	public static MemberInfoResponse toMemberInfoResponse(Member member) {
 		return new MemberInfoResponse(member.getEmail(), member.getNickname(), member.getPhoneNumber());
+	}
+
+	public static MemberStatusChangedEvent toMemberStatusChangedEvent(Member member) {
+		return new MemberStatusChangedEvent(member.getId(), member.getActivityStatus());
 	}
 }
