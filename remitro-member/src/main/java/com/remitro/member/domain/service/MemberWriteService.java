a28ac17bf48def65372bbd75f4f491954fb56ae7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-11 17:40:32 +0900

    refactor: 회원가입 로직 메서드 분리

--- remitro-member/src/main/java/com/remitro/member/domain/service/MemberWriteService.java
+++ remitro-member/src/main/java/com/remitro/member/domain/service/MemberWriteService.java
@@ -3,9 +3,15 @@ package com.remitro.member.domain.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.remitro.common.domain.enums.AggregateType;
+import com.remitro.common.domain.enums.EventType;
+import com.remitro.common.infra.util.JsonUtil;
 import com.remitro.member.application.dto.request.SignUpRequest;
+import com.remitro.member.application.mapper.MemberMapper;
 import com.remitro.member.domain.model.Member;
+import com.remitro.member.domain.model.OutboxMessage;
 import com.remitro.member.domain.repository.MemberRepository;
+import com.remitro.member.domain.repository.OutboxMessageRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +21,24 @@ public class MemberWriteService {
 
 	private final PasswordEncoder passwordEncoder;
 	private final MemberRepository memberRepository;
+	private final OutboxMessageRepository outboxMessageRepository;
 
 	public void saveMember(SignUpRequest signUpRequest) {
+		final Member member = saveMemberMetadata(signUpRequest);
+		saveMemberActivityEvent(member);
+	}
+
+	private Member saveMemberMetadata(SignUpRequest signUpRequest) {
 		final String encodedPassword = passwordEncoder.encode(signUpRequest.password());
-		final Member member = Member.createMember(
-			signUpRequest.email(), encodedPassword, signUpRequest.nickname(), signUpRequest.phoneNumber()
-		);
-		memberRepository.save(member);
+		final Member member = Member.create(
+			signUpRequest.email(), encodedPassword, signUpRequest.nickname(), signUpRequest.phoneNumber());
+		return memberRepository.save(member);
+	}
+
+	private void saveMemberActivityEvent(Member member) {
+		final String eventMessage = JsonUtil.toJSON(MemberMapper.toMemberStatusChangedEvent(member));
+		final OutboxMessage outboxMessage = OutboxMessage.create(
+			member.getId(), AggregateType.MEMBER, EventType.MEMBER_ACTIVITY_UPSERTED_EVENT, eventMessage);
+		outboxMessageRepository.save(outboxMessage);
 	}
 }
