9de73b1a971336ff865575c448686baf13ed42ee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-11 17:20:41 +0900

    refactor: 메서드명 변경

--- remitro-member/src/main/java/com/remitro/member/domain/repository/OutboxMessageRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/repository/OutboxMessageRepository.java
@@ -2,16 +2,13 @@ package com.remitro.member.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import com.remitro.common.domain.enums.EventStatus;
 import com.remitro.member.domain.model.OutboxMessage;
 
 public interface OutboxMessageRepository extends JpaRepository<OutboxMessage, Long> {
 
-	@Query(
-		value = "SELECT * FROM MEMBER_OUTBOX_MESSAGES WHERE event_status = 'PENDING' ORDER BY outbox_message_id ASC LIMIT :limit",
-		nativeQuery = true)
-	List<OutboxMessage> findTopNPendingOrderByIdAsc(@Param("limit") int limit);
+	List<OutboxMessage> findByEventStatusOrderByIdAsc(EventStatus eventStatus, Pageable pageable);
 }
