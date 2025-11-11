f5663be6a644c9ad9d1b84d84f1b58d80bca4230
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-11 16:58:48 +0900

    feat: 사용자 아웃박스 엔티티 정의

---
+++ remitro-member/src/main/java/com/remitro/member/domain/model/OutboxMessage.java
@@ -0,0 +1,77 @@
+package com.remitro.member.domain.model;
+
+import java.util.UUID;
+
+import com.remitro.common.domain.BaseTimeEntity;
+import com.remitro.common.domain.enums.AggregateType;
+import com.remitro.common.domain.enums.EventStatus;
+import com.remitro.common.domain.enums.EventType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "MEMBER_OUTBOX_MESSAGES", indexes = {
+	@Index(name = "idx_outbox_id_status", columnList = "id, event_status")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OutboxMessage extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "outbox_message_id", nullable = false)
+	private Long id;
+
+	@Column(name = "event_id", unique = true, nullable = false, length = 36)
+	private String eventId;
+
+	@Column(name = "aggregate_id", nullable = false)
+	private Long aggregateId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "aggregate_type", nullable = false)
+	private AggregateType aggregateType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "event_type", nullable = false)
+	private EventType eventType;
+
+	@Column(name = "event_data", nullable = false, columnDefinition = "TEXT")
+	private String eventData;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "event_status", nullable = false)
+	private EventStatus eventStatus;
+
+	private OutboxMessage(String eventId, Long aggregateId, AggregateType aggregateType, EventType eventType,
+		String eventData) {
+
+		this.eventId = eventId;
+		this.aggregateId = aggregateId;
+		this.aggregateType = aggregateType;
+		this.eventType = eventType;
+		this.eventData = eventData;
+		this.eventStatus = EventStatus.PENDING;
+	}
+
+	public static OutboxMessage create(Long aggregateId, AggregateType aggregateType, EventType eventType,
+		String eventData) {
+
+		return new OutboxMessage(UUID.randomUUID().toString(), aggregateId, aggregateType, eventType, eventData);
+	}
+
+	public void markPublished() {
+		this.eventStatus = EventStatus.PUBLISHED;
+	}
+}
