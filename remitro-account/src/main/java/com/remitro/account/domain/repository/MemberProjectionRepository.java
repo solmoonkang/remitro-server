af78a209d0e7fdb8b801069a71ffc164f4df3e24
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-14 14:53:36 +0900

    feat: 사용자 캐시 데이터 저장소 정의

---
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/MemberProjectionRepository.java
@@ -0,0 +1,8 @@
+package com.remitro.account.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.remitro.account.domain.model.read.MemberProjection;
+
+public interface MemberProjectionRepository extends JpaRepository<MemberProjection, Long> {
+}
