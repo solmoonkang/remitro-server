9500184f2055ff74e4cf8dbf89078cb981cfbfe1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-14 14:51:35 +0900

    refactor: 불필요한 DTO 핼퍼 로직 제거

--- remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
+++ remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
@@ -1,8 +1,6 @@
 package com.remitro.account.application.mapper;
 
-import java.util.List;
-
-import com.remitro.account.application.dto.response.AccountDetailResponse;
+import com.remitro.account.application.dto.response.OpenAccountCreationResponse;
 import com.remitro.account.domain.model.Account;
 
 import lombok.AccessLevel;
@@ -11,15 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AccountMapper {
 
-	public static AccountDetailResponse toAccountDetailResponse(Account account) {
-		return new AccountDetailResponse(account.getMember().getNickname(), account.getAccountNumber(),
-			account.getAccountName(), account.getBalance(), account.isActivated(), account.getAccountType().name(),
-			account.getAccountStatus().name());
-	}
-
-	public static List<AccountDetailResponse> toAccountListResponse(List<Account> accounts) {
-		return accounts.stream()
-			.map(AccountMapper::toAccountDetailResponse)
-			.toList();
+	public static OpenAccountCreationResponse toOpenAccountCreationResponse(Account account) {
+		return new OpenAccountCreationResponse(account.getId(), account.getAccountNumber());
 	}
 }
