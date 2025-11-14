717b409e323ee6bf68f1851eedb20f3422b4c0d6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-14 14:52:55 +0900

    feat: 인증된 사용자 활성화 상태 여부 검증 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/validator/AccountValidator.java
+++ remitro-account/src/main/java/com/remitro/account/application/validator/AccountValidator.java
@@ -3,9 +3,13 @@ package com.remitro.account.application.validator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.remitro.common.error.exception.BadRequestException;
-import com.remitro.common.error.exception.ForbiddenException;
-import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.account.domain.model.read.MemberProjection;
+import com.remitro.account.domain.repository.AccountRepository;
+import com.remitro.common.contract.member.ActivityStatus;
+import com.remitro.common.infra.error.exception.BadRequestException;
+import com.remitro.common.infra.error.exception.ConflictException;
+import com.remitro.common.infra.error.exception.ForbiddenException;
+import com.remitro.common.infra.error.model.ErrorMessage;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +20,7 @@ public class AccountValidator {
 	public static final long MINIMUM_AMOUNT = 0L;
 
 	private final PasswordEncoder passwordEncoder;
+	private final AccountRepository accountRepository;
 
 	public void validateAccountAccess(Long accountOwnerId, Long loginMemberId) {
 		if (!accountOwnerId.equals(loginMemberId)) {
@@ -44,6 +49,12 @@ public class AccountValidator {
 	public void validateSelfTransfer(Long senderAccountId, Long receiverAccountId) {
 		if (senderAccountId.equals(receiverAccountId)) {
 			throw new BadRequestException(ErrorMessage.INVALID_TRANSFER);
+		}
+	}
+
+	public void validateMemberIsActive(MemberProjection memberProjection) {
+		if (memberProjection.getActivityStatus() != ActivityStatus.ACTIVE) {
+			throw new ConflictException(ErrorMessage.MEMBER_INACTIVE);
 		}
 	}
 }
