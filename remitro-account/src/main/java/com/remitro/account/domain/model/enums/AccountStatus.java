7478a67a37c535379f51072c5698e884e06a60ea
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-14 14:52:36 +0900

    refactor: 불필요한 주석 제거

--- remitro-account/src/main/java/com/remitro/account/domain/model/enums/AccountStatus.java
+++ remitro-account/src/main/java/com/remitro/account/domain/model/enums/AccountStatus.java
@@ -2,8 +2,5 @@ package com.remitro.account.domain.model.enums;
 
 public enum AccountStatus {
 
-	NORMAL,        // 정상
-	FROZEN,        // 동결
-	TERMINATED,    // 해지
-	DORMANT        // 휴면
+	NORMAL, FROZEN, TERMINATED, DORMANT
 }
