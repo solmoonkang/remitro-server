628b8bf2c9a5eaa449bcf5484419973a7202f6d7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-14 14:55:06 +0900

    feat: member 모듈 수신 로직 구현

---
+++ remitro-account/src/main/java/com/remitro/account/infrastructure/messaging/MemberEventSubscriber.java
@@ -0,0 +1,49 @@
+package com.remitro.account.infrastructure.messaging;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.account.domain.model.read.MemberProjection;
+import com.remitro.account.domain.repository.MemberProjectionRepository;
+import com.remitro.common.contract.member.MemberStatusChangedEvent;
+import com.remitro.common.infra.util.JsonUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MemberEventSubscriber {
+
+	private final MemberProjectionRepository memberProjectionRepository;
+
+	@KafkaListener(
+		topics = "${topics.member-status-changed}",
+		groupId = "account-service"
+	)
+	@Transactional
+	public void handleMemberStatusChangedEvent(String eventMessage) {
+		try {
+			final MemberStatusChangedEvent memberStatusChangedEvent = JsonUtil.fromJSON(
+				eventMessage,
+				MemberStatusChangedEvent.class
+			);
+
+			final MemberProjection member = MemberProjection.create(
+				memberStatusChangedEvent.memberId(),
+				memberStatusChangedEvent.activityStatus()
+			);
+
+			memberProjectionRepository.findById(memberStatusChangedEvent.memberId())
+				.ifPresentOrElse(
+					memberProjection -> memberProjection.update(memberStatusChangedEvent.activityStatus()),
+					() -> memberProjectionRepository.save(member)
+				);
+		} catch (Exception e) {
+			log.error("[✅ LOGGER] 사용자 상태 변경 이벤트를 처리하지 못했습니다: {}", e.getMessage());
+			throw e;
+		}
+	}
+}
