5939d0907709448dd7443e3f53fbf66a62568ff0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-15 20:44:10 +0900

    feat: 계좌 개설 관련 상수 정의

---
+++ remitro-account/src/main/java/com/remitro/account/domain/constant/AccountConstant.java
@@ -0,0 +1,14 @@
+package com.remitro.account.domain.constant;
+
+import java.time.format.DateTimeFormatter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccountConstant {
+
+	public static final int ACCOUNT_NUMBER_GENERATION_MAX_ATTEMPTS = 5;
+	public static final DateTimeFormatter ACCOUNT_NUMBER_GENERATION_FORMAT =
+		DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+}
