628318c4354bea95029d65e93a758a13986b4d7f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-15 14:14:34 +0900

    refactor: 불필요한 로직 제거

--- remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
+++ remitro-account/src/main/java/com/remitro/account/domain/service/AccountWriteService.java
@@ -39,14 +39,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AccountWriteService {
 
-	public static final int RANDOM_ACCOUNT_NUMBER_LENGTH = 10;
-	public static final int MAX_RETRY_ATTEMPTS = 3;
 	private static final int MAX_ACCOUNT_NUMBER_ATTEMPTS = 5;
 	private static final DateTimeFormatter ACCOUNT_NUMBER_FORMAT = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
 
 	private final PasswordEncoder passwordEncoder;
 	private final AccountRepository accountRepository;
-	private final OutboxMessageHandler outboxMessageHandler;
 	private final OutboxMessageRepository outboxMessageRepository;
 
 	public Account saveAccount(MemberProjection member, OpenAccountRequest openAccountRequest) {
@@ -92,73 +89,5 @@ public class AccountWriteService {
 		}
 
 		throw new InternalServerException(ErrorMessage.ACCOUNT_NUMBER_GENERATION_FAILED);
-	}
-
-	public void updateAccountStatus(Account account, UpdateStatusRequest updateStatusRequest) {
-		final AccountStatus newAccountStatus = AccountStatus.valueOf(updateStatusRequest.accountStatus().toUpperCase());
-		account.updateAccountStatus(newAccountStatus);
-	}
-
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void processDeposit(Account receiver, DepositFormRequest depositFormRequest) {
-		receiver.deposit(depositFormRequest.amount());
-
-		final DepositEventMessage eventMessage = EventMapper.toDepositEventMessage(
-			receiver.getAccountNumber(), depositFormRequest.amount());
-		recordPublishedEventForAccount(receiver, EventType.DEPOSIT_EVENT, eventMessage);
-	}
-
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void processWithdraw(Account sender, WithdrawFormRequest withdrawFormRequest) {
-		sender.withdraw(withdrawFormRequest.amount());
-
-		final WithdrawEventMessage eventMessage = EventMapper.toWithdrawEventMessage(
-			sender.getAccountNumber(), withdrawFormRequest.amount());
-		recordPublishedEventForAccount(sender, EventType.WITHDRAWAL_EVENT, eventMessage);
-	}
-
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	public void processTransfer(Account sender, Account receiver, TransferFormRequest transferFormRequest) {
-		sender.withdraw(transferFormRequest.amount());
-		receiver.deposit(transferFormRequest.amount());
-
-		final TransferEventMessage eventMessage = EventMapper.toTransferEventMessage(
-			sender, receiver, transferFormRequest);
-		recordPublishedEventForAccount(sender, EventType.TRANSFER_EVENT, eventMessage);
-	}
-
-	private String generateRandomAccountNumber() {
-		StringBuilder randomNumberBuilder = new StringBuilder();
-
-		for (int i = 0; i < RANDOM_ACCOUNT_NUMBER_LENGTH; i++) {
-			randomNumberBuilder.append(ThreadLocalRandom.current().nextInt(10));
-		}
-
-		return randomNumberBuilder.toString();
-	}
-
-	private void retryAccountCreation(Member member, OpenAccountRequest openAccountRequest, AccountType accountType,
-		String encodedPassword) {
-
-		for (int i = 0; true; i++) {
-			final String accountNumber = accountType.getCode() + generateRandomAccountNumber();
-
-			try {
-				final Account account = Account.createAccount(
-					member, accountNumber, openAccountRequest.accountName(), encodedPassword, accountType);
-				accountRepository.save(account);
-				return;
-			} catch (DataIntegrityViolationException e) {
-				if (i == MAX_RETRY_ATTEMPTS - 1) {
-					throw new ConflictException(ErrorMessage.ACCOUNT_NUMBER_COLLISION);
-				}
-			}
-		}
-	}
-
-	private <T> void recordPublishedEventForAccount(Account account, EventType eventType, T eventMessage) {
-		final OutboxMessageRequest outboxMessageRequest = EventMapper.toOutboxMessageRequest(
-			account, eventType, eventMessage);
-		outboxMessageHandler.recordOutboxMessage(outboxMessageRequest);
 	}
 }
