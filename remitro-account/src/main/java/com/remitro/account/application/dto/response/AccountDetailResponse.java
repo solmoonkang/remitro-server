5b3732affa82244dd6256303ee06afd19a241b42
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 02:35:40 +0900

    refactor: 데이터 타입 수정 및 불필요한 필드 제거

--- remitro-account/src/main/java/com/remitro/account/application/dto/response/AccountDetailResponse.java
+++ remitro-account/src/main/java/com/remitro/account/application/dto/response/AccountDetailResponse.java
@@ -1,13 +1,18 @@
 package com.remitro.account.application.dto.response;
 
+import java.time.LocalDateTime;
+
+import com.remitro.account.domain.model.enums.AccountStatus;
+import com.remitro.account.domain.model.enums.AccountType;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 @Schema(description = "계좌 정보 응답 DTO")
 public record AccountDetailResponse(
-	@Schema(description = "계좌 소유자 닉네임", example = "memberNickname")
-	String nickname,
+	@Schema(description = "계좌 고유 번호", example = "1")
+	Long accountId,
 
 	@Schema(description = "계좌 번호", example = "110-431-402158")
 	String accountNumber,
@@ -15,16 +20,16 @@ public record AccountDetailResponse(
 	@Schema(description = "계좌 이름", example = "accountName")
 	String accountName,
 
-	@Schema(description = "계좌 잔액", example = "10,000L")
+	@Schema(description = "계좌 잔액", example = "25000")
 	Long balance,
 
-	@Schema(description = "계좌 활성화 여부", example = "TRUE, FALSE")
-	boolean isActivated,
+	@Schema(description = "계좌 타입", example = "CHECKING", implementation = AccountType.class)
+	AccountType accountType,
 
-	@Schema(description = "계좌 타입", example = "CHECKING, SAVINGS, DEPOSIT")
-	String accountType,
+	@Schema(description = "계좌 상태", example = "NORMAL", implementation = AccountStatus.class)
+	AccountStatus accountStatus,
 
-	@Schema(description = "계좌 상태", example = "NORMAL, FROZEN, TERMINATED, DORMANT")
-	String accountStatus
+	@Schema(description = "계좌 생성 일시", example = "2025-03-01T10:15:30")
+	LocalDateTime createdAt
 ) {
 }
