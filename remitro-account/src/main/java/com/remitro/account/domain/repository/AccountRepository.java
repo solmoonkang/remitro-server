93eabdb7053b3b00e0e7152b97c4f66371dd8a11
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 02:35:14 +0900

    feat: 사용자 인증을 위한 계좌 객체 조회 로직 정의

--- remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
@@ -1,28 +1,12 @@
 package com.remitro.account.domain.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.remitro.account.domain.model.Account;
-import com.remitro.account.domain.model.enums.AccountType;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-	@Query("SELECT a FROM Account a WHERE a.id = :accountId")
-	Optional<Account> findByAccountId(@Param("accountId") Long accountId);
-
-	List<Account> findByMemberId(Long memberId);
-
-	Optional<Account> findByAccountNumber(String accountNumber);
-
-	@Query("SELECT a.id FROM Account a WHERE a.accountNumber = :accountNumber")
-	Optional<Long> findAccountIdByAccountNumber(@Param("accountNumber") String accountNumber);
-
-	boolean existsByAccountNumber(String accountNumber);
-
-	boolean existsByMemberIdAndAccountType(Long memberId, AccountType accountType);
+	Optional<Account> findByIdAndMemberId(Long accountId, Long memberId);
 }
