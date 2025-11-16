6daf6cfe0660992db5a99d4d1d1d810e74c6d313
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 02:36:26 +0900

    feat: 단일 계좌 정보 조회 엔드포인트 설정

--- remitro-account/src/main/java/com/remitro/account/presentation/AccountController.java
+++ remitro-account/src/main/java/com/remitro/account/presentation/AccountController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.remitro.account.application.dto.request.OpenAccountRequest;
+import com.remitro.account.application.dto.response.AccountDetailResponse;
 import com.remitro.account.application.dto.response.OpenAccountCreationResponse;
 import com.remitro.account.application.service.AccountService;
 import com.remitro.common.infra.auth.annotation.Auth;
@@ -27,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/accounts")
 @RequiredArgsConstructor
-@Tag(name = "계좌 APIs", description = "계좌 정보 저장, 송금, 입출금, 조회 관련 API")
+@Tag(name = "계좌 APIs", description = "계좌 개설 및 조회, 입/출금 관련 API")
 public class AccountController {
 
 	private final AccountService accountService;
@@ -54,5 +57,20 @@ public class AccountController {
 		return ResponseEntity
 			.created(URI.create("/api/accounts/" + openAccountCreationResponse.accountId()))
 			.body(openAccountCreationResponse);
+	}
+
+	@GetMapping("/{accountId}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "단일 계좌 상세 조회", description = "사용자 인증 후 단일 계좌 정보를 조회합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "🎉 단일 계좌 정보 조회 성공"),
+		@ApiResponse(responseCode = "404", description = "🔍 존재하지 않는 사용자"),
+		@ApiResponse(responseCode = "500", description = "💥 서버 내부 오류")
+	})
+	public ResponseEntity<AccountDetailResponse> findAccountDetail(
+		@Auth AuthMember authMember,
+		@PathVariable Long accountId) {
+
+		return ResponseEntity.ok().body(accountService.findAccountDetail(authMember.id(), accountId));
 	}
 }
