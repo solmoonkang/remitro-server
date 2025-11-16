78598817911929d1e9c2e8194bcd6c8ec1efca56
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 01:58:58 +0900

    refactor: 패키지 위치 이동

--- remitro-account/src/main/java/com/remitro/account/domain/repository/MemberProjectionRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/MemberProjectionRepository.java
@@ -2,7 +2,7 @@ package com.remitro.account.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.remitro.account.domain.model.read.MemberProjection;
+import com.remitro.account.domain.model.MemberProjection;
 
 public interface MemberProjectionRepository extends JpaRepository<MemberProjection, Long> {
 }
