ceecb94a358ce914c18d2be131783cd730778f2d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 02:36:11 +0900

    feat: 단일 계좌 정보 조회 읽기 작업

--- remitro-account/src/main/java/com/remitro/account/application/service/AccountReadService.java
+++ remitro-account/src/main/java/com/remitro/account/application/service/AccountReadService.java
@@ -2,7 +2,9 @@ package com.remitro.account.application.service;
 
 import org.springframework.stereotype.Service;
 
+import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.MemberProjection;
+import com.remitro.account.domain.repository.AccountRepository;
 import com.remitro.account.domain.repository.MemberProjectionRepository;
 import com.remitro.common.infra.error.exception.NotFoundException;
 import com.remitro.common.infra.error.model.ErrorMessage;
@@ -14,9 +16,15 @@ import lombok.RequiredArgsConstructor;
 public class AccountReadService {
 
 	private final MemberProjectionRepository memberProjectionRepository;
+	private final AccountRepository accountRepository;
 
 	public MemberProjection findMemberProjectionById(Long memberId) {
 		return memberProjectionRepository.findById(memberId)
 			.orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
+	}
+
+	public Account findAccountByIdAndMemberId(Long memberId, Long accountId) {
+		return accountRepository.findByIdAndMemberId(memberId, accountId)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.ACCOUNT_NOT_FOUND));
 	}
 }
