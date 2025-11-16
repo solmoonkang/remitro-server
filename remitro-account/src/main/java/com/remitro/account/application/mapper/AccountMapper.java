77f7b3e9dcaa48a7d8a93974fd3cc42f525491ef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 02:35:56 +0900

    feat: 단일 계좌 정보 조회 DTO 핼퍼 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
+++ remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
@@ -1,5 +1,6 @@
 package com.remitro.account.application.mapper;
 
+import com.remitro.account.application.dto.response.AccountDetailResponse;
 import com.remitro.account.application.dto.response.OpenAccountCreationResponse;
 import com.remitro.account.domain.model.Account;
 
@@ -11,5 +12,17 @@ public class AccountMapper {
 
 	public static OpenAccountCreationResponse toOpenAccountCreationResponse(Account account) {
 		return new OpenAccountCreationResponse(account.getId(), account.getAccountNumber());
+	}
+
+	public static AccountDetailResponse toAccountDetailResponse(Account account) {
+		return new AccountDetailResponse(
+			account.getId(),
+			account.getAccountNumber(),
+			account.getAccountName(),
+			account.getBalance(),
+			account.getAccountType(),
+			account.getAccountStatus(),
+			account.getCreatedAt()
+		);
 	}
 }
