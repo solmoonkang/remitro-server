30ae3d23b3611c0962cca65ffdb85bb829adab71
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 18:09:36 +0900

    feat: 회원 보유 계좌 목록 조회 로직 구현

--- remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.remitro.account.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,9 @@ import com.remitro.account.domain.model.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+	boolean existsByAccountNumber(String accountNumber);
+
 	Optional<Account> findByIdAndMemberId(Long accountId, Long memberId);
+
+	List<Account> findAccountsByMemberId(Long memberId);
 }
