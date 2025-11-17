13c11295e89a802b94794b0baa8aba6dd5744e27
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-17 18:09:46 +0900

    feat: 회원 보유 계좌 목록 DTO 핼퍼 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
+++ remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
@@ -1,8 +1,13 @@
 package com.remitro.account.application.mapper;
 
+import java.util.List;
+
 import com.remitro.account.application.dto.response.AccountDetailResponse;
+import com.remitro.account.application.dto.response.AccountSummaryResponse;
+import com.remitro.account.application.dto.response.AccountsSummaryResponse;
 import com.remitro.account.application.dto.response.OpenAccountCreationResponse;
 import com.remitro.account.domain.model.Account;
+import com.remitro.account.domain.model.MemberProjection;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -23,6 +28,27 @@ public class AccountMapper {
 			account.getAccountType(),
 			account.getAccountStatus(),
 			account.getCreatedAt()
+		);
+	}
+
+	public static AccountSummaryResponse toAccountSummaryResponse(Account account) {
+		return new AccountSummaryResponse(
+			account.getId(),
+			account.getAccountNumber(),
+			account.getAccountName(),
+			account.getBalance(),
+			account.getAccountType(),
+			account.getAccountStatus()
+		);
+	}
+
+	public static AccountsSummaryResponse toAccountsSummaryResponse(MemberProjection member, List<Account> accounts) {
+		return new AccountsSummaryResponse(
+			member.getMemberId(),
+			member.getNickname(),
+			accounts.stream()
+				.map(AccountMapper::toAccountSummaryResponse)
+				.toList()
 		);
 	}
 }
