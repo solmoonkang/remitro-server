5678be3027b42532554c81290838093529fe36bb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-19 02:11:32 +0900

    feat: 입금 시 검증 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/validator/AccountValidator.java
+++ remitro-account/src/main/java/com/remitro/account/application/validator/AccountValidator.java
@@ -1,10 +1,15 @@
 package com.remitro.account.application.validator;
 
+import static com.remitro.account.domain.constant.AccountConstant.*;
+
+import java.util.Objects;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.MemberProjection;
-import com.remitro.account.domain.repository.AccountRepository;
+import com.remitro.account.domain.model.enums.AccountStatus;
 import com.remitro.common.contract.member.ActivityStatus;
 import com.remitro.common.infra.error.exception.BadRequestException;
 import com.remitro.common.infra.error.exception.ConflictException;
@@ -17,44 +22,41 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AccountValidator {
 
-	public static final long MINIMUM_AMOUNT = 0L;
-
 	private final PasswordEncoder passwordEncoder;
-	private final AccountRepository accountRepository;
 
-	public void validateAccountAccess(Long accountOwnerId, Long loginMemberId) {
-		if (!accountOwnerId.equals(loginMemberId)) {
+	public void validateMemberIsActive(MemberProjection memberProjection) {
+		if (memberProjection.getActivityStatus() != ActivityStatus.ACTIVE) {
+			throw new ConflictException(ErrorMessage.MEMBER_INACTIVE);
+		}
+	}
+
+	public void validateAccountOwner(Long accountOwnerId, Long loginMemberId) {
+		if (!Objects.equals(accountOwnerId, loginMemberId)) {
 			throw new ForbiddenException(ErrorMessage.ACCOUNT_ACCESS_FORBIDDEN);
+		}
+	}
+
+	public void validateAccountStatusNormal(AccountStatus accountStatus) {
+		if (accountStatus != AccountStatus.NORMAL) {
+			throw new BadRequestException(ErrorMessage.INACTIVE_ACCOUNT_STATUS);
+		}
+	}
+
+	public void validateAmountPositive(Long amount) {
+		if (amount == null || amount <= MINIMUM_AMOUNT) {
+			throw new BadRequestException(ErrorMessage.INVALID_AMOUNT);
+		}
+	}
+
+	public void validateSufficientBalance(Account account, Long amount) {
+		if (account.getBalance() < amount) {
+			throw new BadRequestException(ErrorMessage.INSUFFICIENT_FUNDS);
 		}
 	}
 
 	public void validateAccountPasswordMatch(String rawPassword, String savedPassword) {
 		if (!passwordEncoder.matches(rawPassword, savedPassword)) {
 			throw new BadRequestException(ErrorMessage.INVALID_PASSWORD);
-		}
-	}
-
-	public void validateAmountPositive(Long amount) {
-		if (amount <= MINIMUM_AMOUNT) {
-			throw new BadRequestException(ErrorMessage.INVALID_AMOUNT);
-		}
-	}
-
-	public void validateSufficientBalance(Long balance, Long amount) {
-		if (balance < amount) {
-			throw new BadRequestException(ErrorMessage.INSUFFICIENT_FUNDS);
-		}
-	}
-
-	public void validateSelfTransfer(Long senderAccountId, Long receiverAccountId) {
-		if (senderAccountId.equals(receiverAccountId)) {
-			throw new BadRequestException(ErrorMessage.INVALID_TRANSFER);
-		}
-	}
-
-	public void validateMemberIsActive(MemberProjection memberProjection) {
-		if (memberProjection.getActivityStatus() != ActivityStatus.ACTIVE) {
-			throw new ConflictException(ErrorMessage.MEMBER_INACTIVE);
 		}
 	}
 }
