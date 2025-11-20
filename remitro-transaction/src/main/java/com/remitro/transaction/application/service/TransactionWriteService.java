c1c3f837609d7216a0948852ccaa4e3f6dfc9be1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-20 14:13:36 +0900

    feat: 거래 내역 및 원장 쓰기 작업

--- remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionWriteService.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionWriteService.java
@@ -2,7 +2,12 @@ package com.remitro.transaction.application.service;
 
 import org.springframework.stereotype.Service;
 
+import com.remitro.common.contract.account.AccountTransactionEvent;
+import com.remitro.transaction.domain.model.LedgerEntry;
 import com.remitro.transaction.domain.model.Transaction;
+import com.remitro.transaction.domain.model.enums.LedgerDirection;
+import com.remitro.transaction.domain.model.enums.TransactionType;
+import com.remitro.transaction.domain.repository.LedgerEntryRepository;
 import com.remitro.transaction.domain.repository.TransactionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,17 +17,36 @@ import lombok.RequiredArgsConstructor;
 public class TransactionWriteService {
 
 	private final TransactionRepository transactionRepository;
+	private final LedgerEntryRepository ledgerEntryRepository;
 
-	public void saveTransferTransaction(Transaction senderTransaction, Transaction receiverTransaction) {
-		transactionRepository.save(senderTransaction);
-		transactionRepository.save(receiverTransaction);
+	public Transaction saveTransaction(
+		String eventId,
+		TransactionType transactionType,
+		AccountTransactionEvent accountTransactionEvent
+	) {
+		final Transaction transaction = Transaction.create(
+			eventId,
+			transactionType,
+			accountTransactionEvent.amount(),
+			accountTransactionEvent.description(),
+			accountTransactionEvent.occurredAt()
+		);
+		return transactionRepository.save(transaction);
 	}
 
-	public void saveDepositTransaction(Transaction depositTransaction) {
-		transactionRepository.save(depositTransaction);
-	}
-
-	public void saveWithdrawalTransaction(Transaction drawalTransaction) {
-		transactionRepository.save(drawalTransaction);
+	public void saveLedgerEntry(
+		Transaction transaction,
+		LedgerDirection ledgerDirection,
+		Long balanceAfter,
+		AccountTransactionEvent accountTransactionEvent
+	) {
+		final LedgerEntry ledgerEntry = LedgerEntry.create(
+			transaction,
+			ledgerDirection,
+			accountTransactionEvent.amount(),
+			balanceAfter,
+			accountTransactionEvent.occurredAt()
+		);
+		ledgerEntryRepository.save(ledgerEntry);
 	}
 }
