857434e15592febea108a4fa4ddbde62e37a299d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-20 14:09:30 +0900

    feat: 입금 요청 DTO 정의

--- remitro-account/src/main/java/com/remitro/account/application/dto/request/deposit/DepositRequest.java
+++ remitro-account/src/main/java/com/remitro/account/application/dto/request/deposit/DepositRequest.java
@@ -9,6 +9,9 @@ public record DepositRequest(
 	@NotNull(message = "입금 금액을 입력해주세요.")
 	@Positive(message = "0원보다 큰 금액을 입력해주세요.")
 	@Schema(description = "입금 금액", example = "10000")
-	Long amount
+	Long amount,
+
+	@Schema(description = "입금 메모", example = "testDescription")
+	String description
 ) {
 }
