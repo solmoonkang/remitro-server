5dfb5b5175f1699938ecaffe3fee112047c9a552
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-20 14:11:21 +0900

    feat: 출금 이벤트 발생 DTO 정의

---
+++ remitro-common/src/main/java/com/remitro/common/contract/account/AccountWithdrawEvent.java
@@ -0,0 +1,18 @@
+package com.remitro.common.contract.account;
+
+import java.time.LocalDateTime;
+
+public record AccountWithdrawEvent(
+	Long accountId,
+
+	Long memberId,
+
+	Long amount,
+
+	Long balanceAfter,
+
+	String description,
+
+	LocalDateTime occurredAt
+) implements AccountTransactionEvent {
+}
