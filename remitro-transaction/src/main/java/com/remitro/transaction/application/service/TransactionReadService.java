6e4ee661fc24960467e728cf9381e2da384e6fb1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-20 14:13:23 +0900

    feat: 이벤트 ID 중복 여부 읽기 작업

--- remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionReadService.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionReadService.java
@@ -1,12 +1,7 @@
 package com.remitro.transaction.application.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
-import com.remitro.common.error.exception.NotFoundException;
-import com.remitro.common.error.model.ErrorMessage;
-import com.remitro.transaction.domain.model.Transaction;
 import com.remitro.transaction.domain.repository.TransactionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,12 +12,7 @@ public class TransactionReadService {
 
 	private final TransactionRepository transactionRepository;
 
-	public Transaction findTransactionById(Long transactionId) {
-		return transactionRepository.findById(transactionId)
-			.orElseThrow(() -> new NotFoundException(ErrorMessage.TRANSACTION_NOT_FOUND));
-	}
-
-	public List<Transaction> findTransactionsByAccountId(Long accountId) {
-		return transactionRepository.findAllByAccountIdOrderByDateDesc(accountId);
+	public boolean existsByEventId(String eventId) {
+		return transactionRepository.existsByEventId(eventId);
 	}
 }
