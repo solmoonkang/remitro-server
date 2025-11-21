4d5447505864cf5856367b7ff34cbdf14321ab3c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-22 01:49:14 +0900

    feat: RedisProperties 활성화 설정

--- remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
+++ remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
@@ -3,13 +3,17 @@ package com.remitro.account;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.remitro.account.infrastructure.config.RedisProperties;
 
 @SpringBootApplication(
 	exclude = {RedissonAutoConfiguration.class}
 )
 @EnableJpaAuditing
+@EnableConfigurationProperties(RedisProperties.class)
 @ComponentScan(basePackages = "com.remitro.common")
 public class AccountServiceApplication {
 
