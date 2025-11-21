8f78ea7e10b93c4915e30f7e017253e829e9f8db
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-21 14:31:42 +0900

    feat: 잔액 캐시 처리를 위한 도메인 레벨 로직 구현

---
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/AccountBalanceRedisRepository.java
@@ -0,0 +1,40 @@
+package com.remitro.account.domain.repository;
+
+import static com.remitro.common.infra.util.RedisConstant.*;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.remitro.account.infrastructure.redis.ValueRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class AccountBalanceRedisRepository {
+
+	private final ValueRedisRepository valueRedisRepository;
+
+	public void saveCachedBalance(Long accountId, Long balance) {
+		valueRedisRepository.setValue(generateBalanceCacheKey(accountId), String.valueOf(balance), BALANCE_CACHE_TTL);
+	}
+
+	public Optional<Long> findCachedBalance(Long accountId) {
+		final String balance = valueRedisRepository.getValue(generateBalanceCacheKey(accountId));
+
+		if (balance == null) {
+			return Optional.empty();
+		}
+
+		return Optional.of(Long.parseLong(balance));
+	}
+
+	public void evictCachedBalance(Long accountId) {
+		valueRedisRepository.deleteValue(generateBalanceCacheKey(accountId));
+	}
+
+	private String generateBalanceCacheKey(Long accountId) {
+		return BALANCE_CACHE_PREFIX + accountId;
+	}
+}
