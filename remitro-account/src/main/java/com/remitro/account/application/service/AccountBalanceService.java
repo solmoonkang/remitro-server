f00f923385a103f9714b72c5f79da778a7b21320
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-21 14:32:18 +0900

    feat: 잔액 조회 시 캐시 처리를 위한 서비스 구현

--- remitro-account/src/main/java/com/remitro/account/application/service/AccountBalanceService.java
+++ remitro-account/src/main/java/com/remitro/account/application/service/AccountBalanceService.java
@@ -4,21 +4,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.account.application.dto.request.deposit.DepositCommand;
+import com.remitro.account.application.dto.response.AccountBalanceResponse;
 import com.remitro.account.application.dto.response.DepositResponse;
 import com.remitro.account.application.mapper.AccountMapper;
 import com.remitro.account.application.validator.AccountValidator;
 import com.remitro.account.domain.model.Account;
+import com.remitro.account.domain.repository.AccountBalanceRedisRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AccountBalanceTransactionService {
+@Transactional(readOnly = true)
+public class AccountBalanceService {
 
 	private final AccountValidator accountValidator;
-	private AccountReadService accountReadService;
-	private AccountWriteService accountWriteService;
-	private AccountOutboxService accountOutboxService;
+	private final AccountReadService accountReadService;
+	private final AccountWriteService accountWriteService;
+	private final AccountOutboxService accountOutboxService;
+	private final AccountBalanceRedisRepository accountBalanceRedisRepository;
+
+	public AccountBalanceResponse findAccountBalance(Long memberId, Long accountId) {
+		final Account account = accountReadService.findAccountByIdAndMemberId(memberId, accountId);
+		final Long balance = accountBalanceRedisRepository.findCachedBalance(accountId)
+			.orElseGet(() -> cacheBalanceFromAccount(account));
+		return AccountMapper.toAccountBalanceResponse(balance);
+	}
+
+	private Long cacheBalanceFromAccount(Account account) {
+		final Long balance = account.getBalance();
+		accountBalanceRedisRepository.saveCachedBalance(account.getId(), balance);
+		return balance;
+	}
 
 	@Transactional
 	public DepositResponse runDepositTransaction(DepositCommand depositCommand) {
@@ -29,6 +46,7 @@ public class AccountBalanceTransactionService {
 		accountValidator.validateAmountPositive(depositCommand.amount());
 
 		accountWriteService.increaseBalance(account, depositCommand.amount());
+		accountBalanceRedisRepository.saveCachedBalance(account.getId(), account.getBalance());
 		accountOutboxService.appendDepositEvent(account, depositCommand.amount(), depositCommand.description());
 
 		return AccountMapper.toDepositResponse(account, depositCommand);
