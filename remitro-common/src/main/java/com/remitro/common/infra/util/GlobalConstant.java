1c53ea6ea047230f9929a62854d740d405ce434b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-21 14:25:54 +0900

    feat: 전역 상수 유틸 클래스 정의

---
+++ remitro-common/src/main/java/com/remitro/common/infra/util/GlobalConstant.java
@@ -0,0 +1,10 @@
+package com.remitro.common.infra.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GlobalConstant {
+
+	public static final String SEPARATOR = ":";
+}
