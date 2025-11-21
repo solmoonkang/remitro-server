977aa149b92ddc28a5eb067bf87c92955e64d03d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-21 14:26:02 +0900

    feat: Kafka 상수 유틸 클래스 정의

--- remitro-common/src/main/java/com/remitro/common/infra/util/KafkaConstant.java
+++ remitro-common/src/main/java/com/remitro/common/infra/util/KafkaConstant.java
@@ -6,6 +6,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KafkaConstant {
 
-	public static final String TOPIC_TRANSACTION_EVENTS = "remitro.events.transactions";
-	public static final String GROUP_TRANSACTION_CONSUMERS = "remitro.transaction.group";
+	public static final String EVENT_HEADER_ID = "eventId";
+	public static final String EVENT_HEADER_TYPE = "eventType";
+
+	// ACCOUNT
+	public static final String MEMBER_EVENTS_TOPIC_NAME = "${topics.member-events}";
+	public static final String ACCOUNT_CONSUMER_GROUP_ID = "account-service";
+
+	// TRANSACTION
+	public static final String ACCOUNT_EVENTS_TOPIC_NAME = "${topics.account-events}";
+	public static final String TRANSACTION_CONSUMER_GROUP_ID = "transaction-service";
 }
