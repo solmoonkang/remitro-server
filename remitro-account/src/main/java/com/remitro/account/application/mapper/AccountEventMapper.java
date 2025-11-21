9dcbc0097f21baf501d2cdac85418f763a48322a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-22 01:53:09 +0900

    feat: 계좌 상태 변경 이벤트 DTO 핼퍼 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/mapper/AccountEventMapper.java
+++ remitro-account/src/main/java/com/remitro/account/application/mapper/AccountEventMapper.java
@@ -3,13 +3,25 @@ package com.remitro.account.application.mapper;
 import java.time.LocalDateTime;
 
 import com.remitro.account.domain.model.Account;
+import com.remitro.account.domain.model.enums.AccountStatus;
 import com.remitro.common.contract.account.AccountDepositEvent;
+import com.remitro.common.contract.account.AccountStatusChangedEvent;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AccountEventMapper {
+
+	public static AccountStatusChangedEvent toAccountStatusChangedEvent(Account account, AccountStatus accountStatus) {
+		return new AccountStatusChangedEvent(
+			account.getId(),
+			account.getMemberId(),
+			accountStatus.name(),
+			account.getAccountStatus().name(),
+			LocalDateTime.now()
+		);
+	}
 
 	public static AccountDepositEvent toAccountDepositEvent(Account account, Long amount, String description) {
 		return new AccountDepositEvent(
