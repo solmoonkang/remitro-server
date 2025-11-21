e4edd9aecb7004f24ba8392732048f0da3f238e7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-22 01:49:30 +0900

    feat: 분산 락 관련 상수 정의

--- remitro-common/src/main/java/com/remitro/common/infra/util/RedisConstant.java
+++ remitro-common/src/main/java/com/remitro/common/infra/util/RedisConstant.java
@@ -10,18 +10,23 @@ public class RedisConstant {
 	public static final String IDEMPOTENCY_PREFIX = "IDEMPOTENCY:PREFIX:";
 	public static final String IDEMPOTENCY_MARKER_VALUE = "LOCK";
 
-	// IDEMPOTENCY SERVICE PREFIX
 	public static final String OPEN_ACCOUNT_PREFIX = "OPEN:ACCOUNT:";
 	public static final String DEPOSIT_PREFIX = "DEPOSIT:";
 
-	// BALANCE CACHE PREFIX
-	public static final String BALANCE_CACHE_PREFIX = "ACCOUNT:BALANCE:";
-
-	// IDEMPOTENCY TTL
 	public static final long OPEN_ACCOUNT_IDEMPOTENCY_TTL = 60L * 5;
 	public static final long BALANCE_CHANGE_IDEMPOTENCY_TTL = 60L;
 	public static final long TRANSFER_IDEMPOTENCY_TTL = 60L * 3;
 
-	// BALACE CACHE TTL
+	// BALANCE CACHE PREFIX
+	public static final String BALANCE_CACHE_PREFIX = "ACCOUNT:BALANCE:";
+
 	public static final long BALANCE_CACHE_TTL = 30L;
+
+	// DISTRIBUTED LOCK PREFIX
+	public static final String ACCOUNT_LOCK_PREFIX = "LOCK:ACCOUNT:";
+
+	public static final long LOCK_ACQUIRE_TIMEOUT_SECONDS = 3L;
+	public static final long LOCK_HOLD_DURATION_SECONDS = 15L;
+	public static final int LOCK_MAX_RETRY_ATTEMPTS = 5;
+	public static final long LOCK_BACKOFF_BASE_MILLISECONDS = 200L;
 }
