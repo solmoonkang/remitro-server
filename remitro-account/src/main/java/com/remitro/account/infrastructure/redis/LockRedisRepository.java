2ee0c26180cb1812a7d96a472b5c404c653be614
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-22 01:50:54 +0900

    refactor: Redis unlock 실패 시 로그 설정

--- remitro-account/src/main/java/com/remitro/account/infrastructure/redis/LockRedisRepository.java
+++ remitro-account/src/main/java/com/remitro/account/infrastructure/redis/LockRedisRepository.java
@@ -4,13 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisException;
 import org.springframework.stereotype.Repository;
 
 import com.remitro.common.infra.error.exception.LockAcquireException;
 import com.remitro.common.infra.error.model.ErrorMessage;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class LockRedisRepository {
@@ -31,8 +34,14 @@ public class LockRedisRepository {
 	}
 
 	public void unLock(RLock lock) {
-		if (lock.isHeldByCurrentThread()) {
-			lock.unlock();
+		try {
+			if (lock.isHeldByCurrentThread()) {
+				lock.unlock();
+			}
+		} catch (RedisException e) {
+			log.warn("[✅ LOGGER] UNLOCK 처리에 실패했습니다. "
+				+ "이미 LEASE TIME 만료로 자동 해제되었거나, REDIS 연결 상태가 불안정한 상황일 수 있습니다."
+			);
 		}
 	}
 }
