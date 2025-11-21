a3433de3f431cde8a6260dde47219f00b2e1729e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-22 01:48:34 +0900

    refactor: 가독성 향상을 위한 개행 처리

--- remitro-account/src/main/java/com/remitro/account/infrastructure/config/RedissonConfig.java
+++ remitro-account/src/main/java/com/remitro/account/infrastructure/config/RedissonConfig.java
@@ -3,7 +3,6 @@ package com.remitro.account.infrastructure.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,11 +17,13 @@ public class RedissonConfig {
 	@Bean
 	public RedissonClient redissonClient() {
 		Config config = new Config();
-		String redisAddress = String.format("redis://%s:%d",
-			redisProperties.getHost(),
-			redisProperties.getPort());
+		config.useSingleServer()
+			.setAddress(
+				String.format(
+					"redis://%s:%d", redisProperties.host(), redisProperties.port()
+				)
+			);
 
-		config.useSingleServer().setAddress(redisAddress);
 		return Redisson.create(config);
 	}
 }
