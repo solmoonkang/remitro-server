f30c1d49b94ec497931ab65bf0ed4e1a8929a151
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-23 03:50:23 +0900

    chore: remitro-messaging 모듈 생성

---
+++ remitro-messaging/src/test/java/com/remitro/messaging/RemitroMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitro.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitroMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
