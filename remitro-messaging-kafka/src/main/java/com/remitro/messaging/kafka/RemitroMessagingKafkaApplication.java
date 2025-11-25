ecce75d5e8fe74507dcfac7b09ba19fc96141cd1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-25 13:19:45 +0900

    chore: messaging-kafka 모듈 정의

---
+++ remitro-messaging-kafka/src/main/java/com/remitro/messaging/kafka/RemitroMessagingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.remitro.messaging.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitroMessagingKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitroMessagingKafkaApplication.class, args);
+	}
+
+}
