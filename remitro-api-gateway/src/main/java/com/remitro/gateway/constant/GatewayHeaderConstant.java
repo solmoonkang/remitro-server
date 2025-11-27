4f55346f43400cb196f7abc7f35e6504a1f0f42a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-28 02:05:03 +0900

    feat: 게이트웨이 헤더 관련 상수 정의

---
+++ remitro-api-gateway/src/main/java/com/remitro/gateway/constant/GatewayHeaderConstant.java
@@ -0,0 +1,12 @@
+package com.remitro.gateway.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GatewayHeaderConstant {
+
+	public static final String HEADER_MEMBER_ID = "X-Member-Id";
+	public static final String HEADER_MEMBER_EMAIL = "X-Member-Email";
+	public static final String HEADER_MEMBER_NICKNAME = "X-Member-Nickname";
+}
