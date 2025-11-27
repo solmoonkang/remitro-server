52015f444b9214302a5abd813f54a950beb70908
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-28 02:17:10 +0900

    feat: 라우팅 관련 상수 정의

---
+++ remitro-api-gateway/src/main/java/com/remitro/gateway/constant/RouteConstant.java
@@ -0,0 +1,19 @@
+package com.remitro.gateway.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RouteConstant {
+
+	public static final String MEMBER_SERVICE_URI = "lb://remitro-member";
+	public static final String AUTH_SERVICE_URI = "lb://remitro-auth";
+	public static final String ACCOUNT_SERVICE_URI = "lb://remitro-account";
+	public static final String TRANSACTION_SERVICE_URI = "lb://remitro-transaction";
+
+	public static final String MEMBER_PATH = "/members/**";
+	public static final String AUTH_PATH = "/auth/**";
+	public static final String ACCOUNT_PATH = "/accounts/**";
+	public static final String TRANSACTION_PATH = "/transactions/**";
+
+}
