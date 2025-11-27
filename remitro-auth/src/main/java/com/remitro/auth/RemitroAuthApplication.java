bf169eb25d453ddf8a02474a92ca3fd7ca868721
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-27 19:08:04 +0900

    feat: FeignClient 설정 추가

--- remitro-auth/src/main/java/com/remitro/auth/RemitroAuthApplication.java
+++ remitro-auth/src/main/java/com/remitro/auth/RemitroAuthApplication.java
@@ -3,9 +3,13 @@ package com.remitro.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.remitro.auth.infrastructure.config.TokenConfig;
 
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.remitro.auth")
 @EnableConfigurationProperties(TokenConfig.class)
 @SpringBootApplication
 public class RemitroAuthApplication {
