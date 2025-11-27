9fcaeceb2eeac1e02a19ac36429a144c46abf1c0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-28 02:00:07 +0900

    feat: 계좌 상태 변경 시 이벤트 ID 읽기 작업

--- remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionReadService.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionReadService.java
@@ -2,6 +2,7 @@ package com.remitro.transaction.application.service;
 
 import org.springframework.stereotype.Service;
 
+import com.remitro.transaction.domain.repository.StatusHistoryRepository;
 import com.remitro.transaction.domain.repository.TransactionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,13 @@ import lombok.RequiredArgsConstructor;
 public class TransactionReadService {
 
 	private final TransactionRepository transactionRepository;
+	private final StatusHistoryRepository statusHistoryRepository;
 
-	public boolean existsByEventId(String eventId) {
+	public boolean existsTransactionEvent(String eventId) {
 		return transactionRepository.existsByEventId(eventId);
+	}
+
+	public boolean existsStatusChangeEvent(String eventId) {
+		return statusHistoryRepository.existsByEventId(eventId);
 	}
 }
