545a838098d12b4447002f845338779715938832
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-27 19:10:46 +0900

    feat: String 자료구조 로우 레벨 저장소 정의

---
+++ remitro-auth/src/main/java/com/remitro/auth/infrastructure/redis/ValueRedisRepository.java
@@ -0,0 +1,27 @@
+package com.remitro.auth.infrastructure.redis;
+
+import java.time.Duration;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ValueRedisRepository {
+
+	private final StringRedisTemplate stringRedisTemplate;
+
+	public void set(String key, String value, Duration expirationTime) {
+		stringRedisTemplate.opsForValue().set(key, value, expirationTime);
+	}
+
+	public String get(String key) {
+		return stringRedisTemplate.opsForValue().get(key);
+	}
+
+	public void delete(String key) {
+		stringRedisTemplate.delete(key);
+	}
+}
