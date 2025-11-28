ae5a144281b9e792019fc4ea626afcae4cc97ed0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-29 02:40:14 +0900

    refactor: 가독성 향상을 위한 개행 처리

--- remitro-member/src/main/java/com/remitro/member/application/mapper/MemberMapper.java
+++ remitro-member/src/main/java/com/remitro/member/application/mapper/MemberMapper.java
@@ -1,6 +1,5 @@
 package com.remitro.member.application.mapper;
 
-import com.remitro.common.contract.member.MemberStatusChangedEvent;
 import com.remitro.member.application.dto.response.MemberInfoResponse;
 import com.remitro.member.domain.model.Member;
 
@@ -11,10 +10,10 @@ import lombok.NoArgsConstructor;
 public class MemberMapper {
 
 	public static MemberInfoResponse toMemberInfoResponse(Member member) {
-		return new MemberInfoResponse(member.getEmail(), member.getNickname(), member.getPhoneNumber());
-	}
-
-	public static MemberStatusChangedEvent toMemberStatusChangedEvent(Member member) {
-		return new MemberStatusChangedEvent(member.getId(), member.getActivityStatus());
+		return new MemberInfoResponse(
+			member.getEmail(),
+			member.getNickname(),
+			member.getPhoneNumber()
+		);
 	}
 }
