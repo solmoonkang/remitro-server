819a7d2a289ba9306deaf5222f75e67a00a11893
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-29 02:34:38 +0900

    feat: Kafka 이벤트 producer 구현

---
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/messaging/KafkaEventProducer.java
@@ -0,0 +1,36 @@
+package com.remitro.member.infrastructure.messaging;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.remitro.common.contract.event.EventEnvelope;
+import com.remitro.common.util.JsonMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaEventProducer {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+	public void send(String eventTopic, EventEnvelope eventEnvelope) {
+		try {
+			final String eventMessage = JsonMapper.toJSON(eventEnvelope);
+			kafkaTemplate.send(eventTopic, eventEnvelope.eventId(), eventMessage);
+
+			log.info("[✅ LOGGER] KAFKA 메시지 발행에 성공했습니다: EVENT_ID={}, EVENT_TYPE={}",
+				eventEnvelope.eventId(),
+				eventEnvelope.eventType()
+			);
+
+		} catch (Exception e) {
+			log.error("[✅ LOGGER] KAFKA 메시지 발행에 실패했습니다: EVENT_ID={}, REASON={}",
+				eventEnvelope.eventId(),
+				e.getMessage()
+			);
+		}
+	}
+}
