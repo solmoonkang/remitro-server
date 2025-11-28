18c0971fadaa0bb59cae2f7516917aaa58723a04
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-29 02:41:16 +0900

    refactor: 패키지명 수정

--- remitro-member/src/main/java/com/remitro/member/domain/repository/OutboxMessageRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/repository/OutboxMessageRepository.java
@@ -2,13 +2,12 @@ package com.remitro.member.domain.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.remitro.common.domain.enums.EventStatus;
+import com.remitro.member.domain.event.EventStatus;
 import com.remitro.member.domain.model.OutboxMessage;
 
 public interface OutboxMessageRepository extends JpaRepository<OutboxMessage, Long> {
 
-	List<OutboxMessage> findByEventStatusOrderByIdAsc(EventStatus eventStatus, Pageable pageable);
+	List<OutboxMessage> findByEventStatusOrderByIdAsc(EventStatus eventStatus);
 }
