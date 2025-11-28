a66c0e8f4c30dc34aab2d0e378c392ba31955329
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-11-29 02:38:56 +0900

    feat: 회원 상태 업데이트 이벤트 DTO 정의

---
+++ remitro-member/src/main/java/com/remitro/member/domain/event/MemberStatusUpdatedEvent.java
@@ -0,0 +1,16 @@
+package com.remitro.member.domain.event;
+
+import com.remitro.member.domain.enums.ActivityStatus;
+import com.remitro.member.domain.enums.KycStatus;
+
+public record MemberStatusUpdatedEvent(
+	Long memberId,
+
+	String nickname,
+
+	ActivityStatus activityStatus,
+
+	KycStatus kycStatus
+) {
+}
+
