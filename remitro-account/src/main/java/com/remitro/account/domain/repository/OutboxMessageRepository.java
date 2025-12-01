a6cb5a4cacaf407ef6681194d7eee8854cf64a9c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-02 00:54:47 +0900

    refactor: 패키지명 수정 및 패키지 이동

--- remitro-account/src/main/java/com/remitro/account/domain/repository/OutboxMessageRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/OutboxMessageRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import com.remitro.account.domain.event.EventStatus;
 import com.remitro.account.domain.model.OutboxMessage;
-import com.remitro.common.domain.enums.EventStatus;
 
 public interface OutboxMessageRepository extends JpaRepository<OutboxMessage, Long> {
 
