7cdc53b9944931ccbb3b67ab9c715b05d392c8c7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-02 00:55:27 +0900

    refactor: 상태 변경 로직명 수정

--- remitro-account/src/main/java/com/remitro/account/application/service/account/DormantAccountService.java
+++ remitro-account/src/main/java/com/remitro/account/application/service/account/DormantAccountService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.account.application.service.outbox.AccountOutboxService;
+import com.remitro.account.application.validator.AccountStatusTransactionValidator;
 import com.remitro.account.domain.enums.AccountStatus;
 import com.remitro.account.domain.model.Account;
 
@@ -18,6 +19,7 @@ public class DormantAccountService {
 
 	private final AccountReadService accountReadService;
 	private final AccountOutboxService accountOutboxService;
+	private final AccountStatusTransactionValidator accountStatusTransactionValidator;
 
 	@Transactional
 	public int markDormantAccounts() {
@@ -26,9 +28,10 @@ public class DormantAccountService {
 		List<Account> accounts = accountReadService.findInactiveAccounts(threshold);
 
 		for (Account account : accounts) {
-			AccountStatus previousStatus = account.getAccountStatus();
-			account.changeToDormant();
-			accountOutboxService.appendAccountStatusUpdatedEvent(account, previousStatus);
+			AccountStatus currentStatus = account.getAccountStatus();
+			accountStatusTransactionValidator.validateStatusTransition(currentStatus, AccountStatus.DORMANT);
+			account.applyAccountStatus(AccountStatus.DORMANT);
+			accountOutboxService.appendAccountStatusUpdatedEvent(account, currentStatus);
 		}
 
 		return accounts.size();
