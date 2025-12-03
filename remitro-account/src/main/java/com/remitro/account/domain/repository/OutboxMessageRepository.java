92aa3c5a9975206d5dbc364045626231f56608b0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-04 03:09:24 +0900

    refactor: 불필요한 매개변수 제거

--- remitro-account/src/main/java/com/remitro/account/domain/repository/OutboxMessageRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/repository/OutboxMessageRepository.java
@@ -2,7 +2,6 @@ package com.remitro.account.domain.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.remitro.account.domain.event.EventStatus;
@@ -10,5 +9,5 @@ import com.remitro.account.domain.model.OutboxMessage;
 
 public interface OutboxMessageRepository extends JpaRepository<OutboxMessage, Long> {
 
-	List<OutboxMessage> findByEventStatusOrderByCreatedAtAsc(EventStatus eventStatus, Pageable pageable);
+	List<OutboxMessage> findByEventStatusOrderByCreatedAtAsc(EventStatus eventStatus);
 }
