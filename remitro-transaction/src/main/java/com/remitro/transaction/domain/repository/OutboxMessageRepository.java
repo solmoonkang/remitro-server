4daa787534894a8900872be5ef7f10ddb2f8004f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-05 11:33:14 +0900

    remove: 불필요한 클래스 제거

--- remitro-transaction/src/main/java/com/remitro/transaction/domain/repository/OutboxMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.remitro.transaction.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.remitro.transaction.domain.model.OutboxMessage;
-
-public interface OutboxMessageRepository extends JpaRepository<OutboxMessage, Long> {
-}
