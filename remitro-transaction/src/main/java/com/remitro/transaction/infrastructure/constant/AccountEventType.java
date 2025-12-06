038cdd9cf2490c242062c2dfea251dff9d38e92e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-06 15:36:10 +0900

    feat: account 모듈 관련 이벤트 상수 정의

---
+++ remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/constant/AccountEventType.java
@@ -0,0 +1,13 @@
+package com.remitro.transaction.infrastructure.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccountEventType {
+
+	public static final String ACCOUNT_DEPOSIT = "ACCOUNT_DEPOSIT";
+	public static final String ACCOUNT_WITHDRAWAL = "ACCOUNT_WITHDRAWAL";
+	public static final String ACCOUNT_TRANSFER = "ACCOUNT_TRANSFER";
+	public static final String ACCOUNT_STATUS_UPDATED = "ACCOUNT_STATUS_UPDATED";
+}
