7ca65c199688ceb7f8e773267b369c77f3607736
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-09 16:40:32 +0900

    feat: 계좌 상태 업데이트 이벤트 핸들러 정의

---
+++ remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/messaging/handler/StatusUpdatedEventHandler.java
@@ -0,0 +1,22 @@
+package com.remitro.transaction.infrastructure.messaging.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.transaction.application.service.TransactionService;
+import com.remitro.transaction.domain.event.AccountStatusUpdatedEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StatusUpdatedEventHandler implements TransactionEventHandler<AccountStatusUpdatedEvent> {
+
+	private final TransactionService transactionService;
+
+	@Override
+	@Transactional
+	public void handle(String eventId, AccountStatusUpdatedEvent event) {
+		transactionService.recordStatusUpdated(eventId, event);
+	}
+}
