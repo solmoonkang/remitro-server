515f5e4d39d03bf2474d86a176e9dc28456dfd1c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-13 12:53:19 +0900

    refactor: 불필요한 로직 제거

--- remitro-account/src/main/java/com/remitro/account/application/service/account/AccountWriteService.java
+++ remitro-account/src/main/java/com/remitro/account/application/service/account/AccountWriteService.java
@@ -10,17 +10,11 @@ import org.springframework.stereotype.Service;
 
 import com.remitro.account.application.dto.request.OpenAccountRequest;
 import com.remitro.account.domain.enums.AccountType;
-import com.remitro.account.domain.enums.AggregateType;
-import com.remitro.account.domain.event.EventType;
 import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.MemberProjection;
-import com.remitro.account.domain.model.OutboxMessage;
 import com.remitro.account.domain.repository.AccountRepository;
-import com.remitro.account.domain.repository.OutboxMessageRepository;
-import com.remitro.common.contract.account.AccountOpenedEvent;
 import com.remitro.common.error.exception.InternalServerException;
 import com.remitro.common.error.model.ErrorMessage;
-import com.remitro.common.util.JsonMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,7 +24,6 @@ public class AccountWriteService {
 
 	private final PasswordEncoder passwordEncoder;
 	private final AccountRepository accountRepository;
-	private final OutboxMessageRepository outboxMessageRepository;
 
 	public Account saveAccount(MemberProjection member, OpenAccountRequest openAccountRequest) {
 		final String accountNumber = generateUniqueAccountNumber(openAccountRequest.accountType());
@@ -44,24 +37,6 @@ public class AccountWriteService {
 		);
 
 		return accountRepository.save(account);
-	}
-
-	public void appendAccountOpenedEventOutbox(Account account) {
-		final AccountOpenedEvent accountOpenedEvent = new AccountOpenedEvent(
-			account.getId(),
-			account.getMemberId(),
-			account.getAccountType().getCode()
-		);
-		final String eventMessage = JsonMapper.toJSON(accountOpenedEvent);
-
-		final OutboxMessage outboxMessage = OutboxMessage.create(
-			account.getId(),
-			AggregateType.ACCOUNT,
-			EventType.ACCOUNT_CREATED,
-			eventMessage
-		);
-
-		outboxMessageRepository.save(outboxMessage);
 	}
 
 	public void increaseBalance(Account account, Long amount) {
