98a2a2dd652dfd5c479a04ce7a34840f5a953d3f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-14 01:25:26 +0900

    feat: 계좌 개설 이벤트 추가

--- remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/messaging/dispatcher/TransactionEventDispatcher.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/messaging/dispatcher/TransactionEventDispatcher.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 
 import com.remitro.common.contract.event.EventEnvelope;
 import com.remitro.common.util.JsonMapper;
+import com.remitro.transaction.domain.event.AccountDepositEvent;
+import com.remitro.transaction.domain.event.AccountOpenedEvent;
+import com.remitro.transaction.domain.event.AccountStatusUpdatedEvent;
+import com.remitro.transaction.infrastructure.messaging.handler.AccountOpenedEventHandler;
 import com.remitro.transaction.infrastructure.messaging.handler.DepositEventHandler;
 import com.remitro.transaction.infrastructure.messaging.handler.StatusUpdatedEventHandler;
-import com.remitro.transaction.domain.event.AccountDepositEvent;
-import com.remitro.transaction.domain.event.AccountStatusUpdatedEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,19 +21,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class TransactionEventDispatcher {
 
-	private final DepositEventHandler depositEventHandler;
+	private final AccountOpenedEventHandler accountOpenedEventHandler;
 	private final StatusUpdatedEventHandler statusUpdatedEventHandler;
+	private final DepositEventHandler depositEventHandler;
 
 	public void dispatch(EventEnvelope eventEnvelope) {
 		switch (eventEnvelope.eventType()) {
-			case ACCOUNT_DEPOSIT -> depositEventHandler.handle(
+			case ACCOUNT_OPENED -> accountOpenedEventHandler.handle(
 				eventEnvelope.eventId(),
-				JsonMapper.fromJSON(eventEnvelope.eventPayload(), AccountDepositEvent.class)
+				JsonMapper.fromJSON(eventEnvelope.eventPayload(), AccountOpenedEvent.class)
 			);
 
 			case ACCOUNT_STATUS_UPDATED -> statusUpdatedEventHandler.handle(
 				eventEnvelope.eventId(),
 				JsonMapper.fromJSON(eventEnvelope.eventPayload(), AccountStatusUpdatedEvent.class)
+			);
+
+			case ACCOUNT_DEPOSIT -> depositEventHandler.handle(
+				eventEnvelope.eventId(),
+				JsonMapper.fromJSON(eventEnvelope.eventPayload(), AccountDepositEvent.class)
 			);
 
 			default -> log.warn("[✅ LOGGER] 알 수 없는 MEMBER 이벤트 타입을 수신했습니다: EVENT_TYPE={}",
