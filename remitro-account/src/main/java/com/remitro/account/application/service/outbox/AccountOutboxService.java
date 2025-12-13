a440b93db2af8ef36474e41aa2fb5c86e81c5c00
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-13 12:53:03 +0900

    feat: 계좌 개설 시 아웃박스 패턴 적용

--- remitro-account/src/main/java/com/remitro/account/application/service/outbox/AccountOutboxService.java
+++ remitro-account/src/main/java/com/remitro/account/application/service/outbox/AccountOutboxService.java
@@ -3,14 +3,17 @@ package com.remitro.account.application.service.outbox;
 import org.springframework.stereotype.Service;
 
 import com.remitro.account.application.mapper.AccountEventMapper;
+import com.remitro.account.domain.enums.AccountStatus;
+import com.remitro.account.domain.enums.AccountStatusUpdateReason;
+import com.remitro.account.domain.enums.ActorType;
 import com.remitro.account.domain.enums.AggregateType;
 import com.remitro.account.domain.event.EventType;
+import com.remitro.account.domain.event.account.AccountDepositEvent;
+import com.remitro.account.domain.event.account.AccountOpenedEvent;
+import com.remitro.account.domain.event.account.AccountStatusUpdatedEvent;
 import com.remitro.account.domain.model.Account;
 import com.remitro.account.domain.model.OutboxMessage;
-import com.remitro.account.domain.enums.AccountStatus;
 import com.remitro.account.domain.repository.OutboxMessageRepository;
-import com.remitro.common.contract.account.AccountDepositEvent;
-import com.remitro.common.contract.account.AccountStatusChangedEvent;
 import com.remitro.common.util.JsonMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -21,36 +24,60 @@ public class AccountOutboxService {
 
 	private final OutboxMessageRepository outboxMessageRepository;
 
-	public void appendAccountStatusUpdatedEvent(Account account, AccountStatus previousStatus) {
-		final AccountStatusChangedEvent accountStatusChangedEvent = AccountEventMapper.toAccountStatusChangedEvent(
+	public void appendAccountOpenedEventOutbox(Account account) {
+		final AccountOpenedEvent accountOpenedEvent = AccountEventMapper.toAccountOpenedEvent(
 			account,
-			previousStatus
+			ActorType.MEMBER,
+			AccountStatusUpdateReason.USER_REQUEST
 		);
 
-		final OutboxMessage accountOutbox = OutboxMessage.create(
+		final OutboxMessage outboxMessage = OutboxMessage.create(
+			account.getId(),
+			AggregateType.ACCOUNT,
+			EventType.ACCOUNT_OPENED,
+			JsonMapper.toJSON(accountOpenedEvent)
+		);
+
+		outboxMessageRepository.save(outboxMessage);
+	}
+
+	public void appendAccountStatusUpdatedEventOutbox(
+		Account account,
+		AccountStatus previousStatus,
+		ActorType actorType,
+		AccountStatusUpdateReason accountStatusUpdateReason
+	) {
+		final AccountStatusUpdatedEvent accountStatusUpdatedEvent = AccountEventMapper.toAccountStatusUpdatedEvent(
+			account,
+			previousStatus,
+			actorType,
+			accountStatusUpdateReason
+		);
+
+		final OutboxMessage outboxMessage = OutboxMessage.create(
 			account.getId(),
 			AggregateType.ACCOUNT,
 			EventType.ACCOUNT_STATUS_UPDATED,
-			JsonMapper.toJSON(accountStatusChangedEvent)
+			JsonMapper.toJSON(accountStatusUpdatedEvent)
 		);
 
-		outboxMessageRepository.save(accountOutbox);
+		outboxMessageRepository.save(outboxMessage);
 	}
 
-	public void appendDepositEvent(Account account, Long amount, String description) {
+	public void appendDepositEventOutbox(Account account, Long amount, String description) {
 		final AccountDepositEvent accountDepositEvent = AccountEventMapper.toAccountDepositEvent(
 			account,
 			amount,
 			description
 		);
 
-		final OutboxMessage accountOutbox = OutboxMessage.create(
+		final OutboxMessage outboxMessage = OutboxMessage.create(
 			account.getId(),
 			AggregateType.ACCOUNT,
-			EventType.ACCOUNT_BALANCE_UPDATED,
+			EventType.ACCOUNT_DEPOSITED,
 			JsonMapper.toJSON(accountDepositEvent)
 		);
 
-		outboxMessageRepository.save(accountOutbox);
+		outboxMessageRepository.save(outboxMessage);
 	}
 }
