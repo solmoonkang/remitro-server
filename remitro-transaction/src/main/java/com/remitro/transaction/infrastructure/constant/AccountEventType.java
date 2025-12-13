ba2eee8c0aa4b9882c99278f75eee97b213eaafc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-14 01:23:55 +0900

    feat: 계좌 개설 이벤트 타입 정의

--- remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/constant/AccountEventType.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/infrastructure/constant/AccountEventType.java
@@ -6,8 +6,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccountEventType {
 
+	public static final String ACCOUNT_OPENED = "ACCOUNT_OPENED";
+	public static final String ACCOUNT_STATUS_UPDATED = "ACCOUNT_STATUS_UPDATED";
 	public static final String ACCOUNT_DEPOSIT = "ACCOUNT_DEPOSIT";
 	public static final String ACCOUNT_WITHDRAWAL = "ACCOUNT_WITHDRAWAL";
 	public static final String ACCOUNT_TRANSFER = "ACCOUNT_TRANSFER";
-	public static final String ACCOUNT_STATUS_UPDATED = "ACCOUNT_STATUS_UPDATED";
 }
