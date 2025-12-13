bc600aad8591550e98fa7eaaa10f122583490823
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-14 01:26:02 +0900

    refactor: 역할 책임을 위한 메서드 분리

--- remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionWriteService.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/application/service/TransactionWriteService.java
@@ -2,15 +2,8 @@ package com.remitro.transaction.application.service;
 
 import org.springframework.stereotype.Service;
 
-import com.remitro.transaction.domain.enums.LedgerDirection;
-import com.remitro.transaction.domain.enums.TransactionType;
-import com.remitro.transaction.domain.event.AccountStatusUpdatedEvent;
 import com.remitro.transaction.domain.event.AccountTransactionEvent;
-import com.remitro.transaction.domain.model.AccountStatusHistory;
-import com.remitro.transaction.domain.model.LedgerEntry;
 import com.remitro.transaction.domain.model.Transaction;
-import com.remitro.transaction.domain.repository.LedgerEntryRepository;
-import com.remitro.transaction.domain.repository.StatusHistoryRepository;
 import com.remitro.transaction.domain.repository.TransactionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 public class TransactionWriteService {
 
 	private final TransactionRepository transactionRepository;
-	private final LedgerEntryRepository ledgerEntryRepository;
-	private final StatusHistoryRepository statusHistoryRepository;
 
 	public Transaction saveTransaction(String eventId, AccountTransactionEvent accountTransactionEvent) {
 		final Transaction transaction = Transaction.create(
@@ -34,41 +25,5 @@ public class TransactionWriteService {
 		);
 
 		return transactionRepository.save(transaction);
-	}
-
-	public void saveLedgerEntry(
-		Transaction transaction,
-		Long balanceAfter,
-		AccountTransactionEvent accountTransactionEvent
-	) {
-		final LedgerEntry ledgerEntry = LedgerEntry.create(
-			transaction,
-			resolveLedgerDirection(accountTransactionEvent.transactionType()),
-			accountTransactionEvent.amount(),
-			balanceAfter,
-			accountTransactionEvent.occurredAt()
-		);
-
-		ledgerEntryRepository.save(ledgerEntry);
-	}
-
-	public void saveAccountStatusHistory(String eventId, AccountStatusUpdatedEvent accountStatusUpdatedEvent) {
-		final AccountStatusHistory accountStatusHistory = AccountStatusHistory.create(
-			accountStatusUpdatedEvent.accountId(),
-			accountStatusUpdatedEvent.memberId(),
-			eventId,
-			accountStatusUpdatedEvent.previousStatus(),
-			accountStatusUpdatedEvent.newStatus(),
-			accountStatusUpdatedEvent.changedAt()
-		);
-
-		statusHistoryRepository.save(accountStatusHistory);
-	}
-
-	private LedgerDirection resolveLedgerDirection(TransactionType transactionType) {
-		return switch (transactionType) {
-			case DEPOSIT, TRANSFER_IN -> LedgerDirection.CREDIT;
-			case WITHDRAWAL, TRANSFER_OUT -> LedgerDirection.DEBIT;
-		};
 	}
 }
