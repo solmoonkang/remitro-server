3fa5c55391e81dbb182a29f2b0fe279a96d6a5c4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-14 23:56:45 +0900

    refactor: 불필요한 주석 제거

--- remitro-transaction/src/main/java/com/remitro/transaction/domain/enums/AmlRuleCode.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/domain/enums/AmlRuleCode.java
@@ -1,14 +1,8 @@
 package com.remitro.transaction.domain.enums;
 
-/**
- * AML 룰 엔진에서 "어떤 규칙에 걸렸는지"를 나타내는 코드 값
- * HIGH_AMOUNT_SINGLE -> 단일 고액 거래
- * RAPID_IN_OUT -> 단기간 입출금 반복
- * NEW_ACCOUNT_LARGE_TRANSFER -> 신규 계좌의 고액 송금
- */
 public enum AmlRuleCode {
 
-	HIGH_AMOUNT_SINGLE,
-	RAPID_IN_OUT,
-	NEW_ACCOUNT_LARGE_TRANSFER
+	HIGH_AMOUNT_SINGLE,                    // 1회 거래 금액이 비정상적으로 큼
+	RAPID_IN_OUT,                          // 짧은 시간 내 입금 -> 전액 출금
+	NEW_ACCOUNT_LARGE_TRANSFER             // 신규 계좌에서 대량 자금 이동
 }
