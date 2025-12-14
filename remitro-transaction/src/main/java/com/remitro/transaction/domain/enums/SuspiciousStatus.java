cc94e79100288a3568eef9e89c2a44d93329c398
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-14 23:58:07 +0900

    refactor: 의미 확인을 위한 주석 추가

--- remitro-transaction/src/main/java/com/remitro/transaction/domain/enums/SuspiciousStatus.java
+++ remitro-transaction/src/main/java/com/remitro/transaction/domain/enums/SuspiciousStatus.java
@@ -2,7 +2,7 @@ package com.remitro.transaction.domain.enums;
 
 public enum SuspiciousStatus {
 
-	OPEN,
-	REVIEWED,
-	CLOSED
+	OPEN,				// 의심 거래가 탐지됨 (조치 전)
+	REVIEWED,			// 시스템/운영자가 검토함
+	CLOSED				// 정상 또는 제재 확정으로 종료
 }
