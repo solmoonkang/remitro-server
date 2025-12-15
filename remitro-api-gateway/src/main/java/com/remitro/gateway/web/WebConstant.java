c71834ae35fb93ad94f986ec0d5699674a8d4893
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-15 16:41:43 +0900

    refactor: 가독성 향상을 위한 패키지 이동

---
+++ remitro-api-gateway/src/main/java/com/remitro/gateway/web/WebConstant.java
@@ -0,0 +1,10 @@
+package com.remitro.gateway.web;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class WebConstant {
+
+	public static final int FILTER_ORDER = -100;
+}
