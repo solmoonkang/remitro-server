4f24ee3a57f7dcd4e624898ca4d104a231c18090
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-15 16:41:22 +0900

    feat: 게이트웨이 Request Id 랜덤 생성 로직 구현

---
+++ remitro-api-gateway/src/main/java/com/remitro/gateway/web/request/RequestIdGenerator.java
@@ -0,0 +1,14 @@
+package com.remitro.gateway.web.request;
+
+import java.util.UUID;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RequestIdGenerator {
+
+	public static String generate() {
+		return UUID.randomUUID().toString();
+	}
+}
