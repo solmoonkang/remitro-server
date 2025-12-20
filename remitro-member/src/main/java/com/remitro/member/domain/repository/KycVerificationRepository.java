95b9d0a85fddc6bc3b6da18bf8654d99e66d0dbb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-21 02:14:40 +0900

    feat: KYC 검증 엔티티 여부 로직 구현

--- remitro-member/src/main/java/com/remitro/member/domain/repository/KycVerificationRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/repository/KycVerificationRepository.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.remitro.member.domain.enums.KycVerificationStatus;
 import com.remitro.member.domain.model.KycVerification;
 
 public interface KycVerificationRepository extends JpaRepository<KycVerification, Long> {
 
 	Optional<KycVerification> findTopByMemberIdOrderByRequestedAtDesc(Long memberId);
+
+	boolean existsByMemberIdAndKycVerificationStatus(Long memberId, KycVerificationStatus kycVerificationStatus);
 }
