6f4f9296364f981a53af90f11d807307fca95b04
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-21 02:18:49 +0900

    feat: 역할 열거형 클래스 정의

--- remitro-common/src/main/java/com/remitro/common/security/Role.java
+++ remitro-common/src/main/java/com/remitro/common/security/Role.java
@@ -2,7 +2,15 @@ package com.remitro.common.security;
 
 public enum Role {
 
-	MEMBER,
-	ADMIN,
-	OPERATOR
+	MEMBER,			// 일반 사용자
+	ADMIN,			// 운영자
+	SYSTEM;			// 내부 서비스 / 배치 / 마이그레이션
+
+	public boolean isAdmin() {
+		return this == ADMIN;
+	}
+
+	public boolean isSystem() {
+		return this == SYSTEM;
+	}
 }
