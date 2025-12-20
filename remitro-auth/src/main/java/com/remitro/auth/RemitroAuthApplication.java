88be5e1d20f6968cf2ddc959e2bb247c80a3c6fc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-20 21:07:06 +0900

    refactor: 가독성 향상을 위한 패키지 이동

--- remitro-auth/src/main/java/com/remitro/auth/RemitroAuthApplication.java
+++ remitro-auth/src/main/java/com/remitro/auth/RemitroAuthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import com.remitro.auth.infrastructure.config.TokenConfig;
+import com.remitro.auth.infrastructure.security.TokenConfig;
 
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.remitro.auth")
