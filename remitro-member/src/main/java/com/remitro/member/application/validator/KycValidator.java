34350cd1c0d303224b9ab7a1ab9ec1fa55c287ee
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-21 02:13:47 +0900

    feat: KYC pending 상태 여부 검증 로직 구현

--- remitro-member/src/main/java/com/remitro/member/application/validator/KycValidator.java
+++ remitro-member/src/main/java/com/remitro/member/application/validator/KycValidator.java
@@ -6,9 +6,27 @@ import com.remitro.common.error.exception.BadRequestException;
 import com.remitro.common.error.model.ErrorMessage;
 import com.remitro.member.application.dto.request.UpdateKycStatusRequest;
 import com.remitro.member.domain.enums.KycStatus;
+import com.remitro.member.domain.enums.KycVerificationStatus;
+import com.remitro.member.domain.repository.KycVerificationRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class KycValidator {
+
+	private final KycVerificationRepository kycVerificationRepository;
+
+	public void validateNoPendingKyc(Long memberId) {
+		boolean existsPending = kycVerificationRepository.existsByMemberIdAndKycVerificationStatus(
+			memberId,
+			KycVerificationStatus.PENDING
+		);
+
+		if (existsPending) {
+			throw new BadRequestException(ErrorMessage.KYC_ALREADY_IN_PROGRESS);
+		}
+	}
 
 	public void validateCompletionRequest(UpdateKycStatusRequest updateKycStatusRequest) {
 		if (updateKycStatusRequest.kycStatus() != KycStatus.VERIFIED) {
