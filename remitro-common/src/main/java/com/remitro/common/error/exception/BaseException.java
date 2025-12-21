6e9bcc7b8282d200dde59b068500a12610c37cf8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-22 03:08:14 +0900

    feat: 추상 커스텀 예외 클래스 정의

---
+++ remitro-common/src/main/java/com/remitro/common/error/exception/BaseException.java
@@ -0,0 +1,19 @@
+package com.remitro.common.error.exception;
+
+import com.remitro.common.error.code.ErrorCode;
+import com.remitro.common.error.message.ErrorMessage;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+	private final ErrorMessage errorMessage;
+
+	protected BaseException(ErrorCode errorCode, ErrorMessage errorMessage, Object... args) {
+		super(errorMessage.formattedMessage(args));
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+}
