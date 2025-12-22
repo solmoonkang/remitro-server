c14172498b7bab01a213099a6a926b1416f827b7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-22 20:57:11 +0900

    chore: 초기 세팅

---
+++ remitro-event/src/test/java/com/remitro/event/RemitroEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitro.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitroEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
