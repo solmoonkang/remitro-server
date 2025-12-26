f3258939a41af76e1198554027114b3b54cb8af4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 15:53:49 +0900

    refactor: 로그 레벨 수정 및 가독성 향상을 위한 개행 처리

--- remitro-member/src/main/java/com/remitro/member/presentation/handler/MemberExceptionHandler.java
+++ remitro-member/src/main/java/com/remitro/member/presentation/handler/MemberExceptionHandler.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.remitro.common.error.code.ErrorCode;
 import com.remitro.common.error.exception.BadRequestException;
 import com.remitro.common.error.exception.ConflictException;
 import com.remitro.common.error.exception.ForbiddenException;
-import com.remitro.common.error.exception.InternalServerException;
-import com.remitro.common.error.exception.LockAcquireException;
 import com.remitro.common.error.exception.NotFoundException;
 import com.remitro.common.error.exception.UnauthorizedException;
-import com.remitro.common.error.model.ErrorResponse;
+import com.remitro.common.error.message.ErrorMessage;
+import com.remitro.common.error.response.ErrorResponse;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -29,66 +29,68 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberExceptionHandler {
 
-	@ExceptionHandler(InternalServerException.class)
-	protected ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
-		log.error("[✅ LOGGER] BAD REQUEST ERROR: 잘못된 요청 에러, {}", e.getMessage());
-		return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), null));
+	@ExceptionHandler(BadRequestException.class)
+	protected ResponseEntity<ErrorResponse> handleBadRequest(BadRequestException e) {
+		log.warn("[✅ LOGGER] BAD REQUEST: CODE={}, MESSAGE={}", e.getErrorCode().getCode(), e.getMessage());
+
+		return ResponseEntity.badRequest().body(ErrorResponse.from(e));
 	}
 
 	@ExceptionHandler(UnauthorizedException.class)
-	protected ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException e) {
-		log.error("[✅ LOGGER] UNAUTHORIZED ERROR: 인증되지 않은 사용자 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage(), null));
+	protected ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e) {
+		log.warn("[✅ LOGGER] UNAUTHORIZED: CODE={}, MESSAGE={}", e.getErrorCode().getCode(), e.getMessage());
+
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
 	}
 
 	@ExceptionHandler(ForbiddenException.class)
-	protected ResponseEntity<ErrorResponse> handleForbiddenException(ForbiddenException e) {
-		log.error("[✅ LOGGER] FORBIDDEN ERROR: 허용되지 않은 권한 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(e.getMessage(), null));
+	protected ResponseEntity<ErrorResponse> handleForbidden(ForbiddenException e) {
+		log.warn("[✅ LOGGER] FORBIDDEN: CODE={}, MESSAGE={}", e.getErrorCode().getCode(), e.getMessage());
+
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ErrorResponse.from(e));
 	}
 
 	@ExceptionHandler(NotFoundException.class)
-	protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
-		log.error("[✅ LOGGER] NOT FOUND ERROR: 리소스를 찾을 수 없는 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage(), null));
+	protected ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
+		log.warn("[✅ LOGGER] NOT FOUND: CODE={}, MESSAGE={}", e.getErrorCode().getCode(), e.getMessage());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.from(e));
 	}
 
 	@ExceptionHandler(ConflictException.class)
-	protected ResponseEntity<ErrorResponse> handleConflictException(ConflictException e) {
-		log.error("[✅ LOGGER] CONFLICT ERROR: 충돌 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(e.getMessage(), null));
-	}
+	protected ResponseEntity<ErrorResponse> handleConflict(ConflictException e) {
+		log.warn("[✅ LOGGER] CONFLICT: CODE={}, MESSAGE={}", e.getErrorCode().getCode(), e.getMessage());
 
-	@ExceptionHandler(InternalServerException.class)
-	protected ResponseEntity<ErrorResponse> handleInternalServerException(InternalServerException e) {
-		log.error("[✅ LOGGER] SERVER ERROR: 서버 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(e.getMessage(), null));
-	}
-
-	@ExceptionHandler(LockAcquireException.class)
-	protected ResponseEntity<ErrorResponse> handleLockAcquireException(LockAcquireException e) {
-		log.error("[✅ LOGGER] LOCK ACQURE ERROR: 락 획득 에러, {}", e.getMessage());
-		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new ErrorResponse(e.getMessage(), null));
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponse.from(e));
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		log.error("[✅ LOGGER] METHOD ARGUMENT NOT VALID ERROR: 잘못된 요청 에러");
-
+	public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException e) {
 		Map<String, String> validationMap = e.getBindingResult()
 			.getFieldErrors()
 			.stream()
-			.collect(Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));
+			.collect(Collectors.toMap(
+				FieldError::getField,
+				DefaultMessageSourceResolvable::getDefaultMessage
+			));
 
-		return ResponseEntity.badRequest().body(new ErrorResponse(
-			"잘못된 요청입니다. 입력한 데이터가 유효하지 않습니다.", validationMap
-		));
+		return ResponseEntity.badRequest()
+			.body(new ErrorResponse(
+				"COMMON_400_VALIDATION",
+				"입력값이 유효하지 않습니다.",
+				validationMap
+			));
 	}
 
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-		log.error("[✅ LOGGER] UNKNOWN ERROR: 알 수 없는 에러", e);
+		log.error("[✅ LOGGER] UNKNOWN ERROR", e);
+
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(new ErrorResponse("서버에서 알 수 없는 에러가 발생했습니다.", null));
+			.body(new ErrorResponse(
+				ErrorCode.UNKNOWN_SERVER_ERROR.getCode(),
+				ErrorMessage.UNKNOWN_SERVER_ERROR.getMessage(),
+				null
+			));
 	}
 }
