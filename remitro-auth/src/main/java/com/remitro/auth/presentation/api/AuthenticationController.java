5f0f81be5be0a604dd48c2fc2b7defb9d35de972
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 21:17:32 +0900

    refactor: 패키지 이동

--- remitro-auth/src/main/java/com/remitro/auth/presentation/api/AuthenticationController.java
+++ remitro-auth/src/main/java/com/remitro/auth/presentation/api/AuthenticationController.java
@@ -1,7 +1,6 @@
-package com.remitro.auth.presentation;
+package com.remitro.auth.presentation.api;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
