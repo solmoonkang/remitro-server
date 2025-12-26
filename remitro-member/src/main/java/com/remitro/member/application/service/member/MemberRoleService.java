52554b10d7e1aaf395b92fc1d1153735053f41ff
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 15:56:47 +0900

    refactor: 가독성 향상을 위한 개행 처리 및 메서드 명 수정

--- remitro-member/src/main/java/com/remitro/member/application/service/member/MemberRoleService.java
+++ remitro-member/src/main/java/com/remitro/member/application/service/member/MemberRoleService.java
@@ -3,8 +3,9 @@ package com.remitro.member.application.service.member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.remitro.common.error.code.ErrorCode;
 import com.remitro.common.error.exception.ForbiddenException;
-import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.common.error.message.ErrorMessage;
 import com.remitro.common.security.Role;
 import com.remitro.member.domain.model.Member;
 
@@ -19,15 +20,18 @@ public class MemberRoleService {
 	private final MemberWriteService memberWriteService;
 
 	@Transactional
-	public void updateMemberRole(Long targetMemberId, Role nextRole, Long adminMemberId) {
+	public void changeMemberRole(Long targetMemberId, Role nextRole, Long adminMemberId) {
 		final Member adminMember = memberReadService.findMemberById(adminMemberId);
 
 		if (!adminMember.getRole().isAdmin()) {
-			throw new ForbiddenException(ErrorMessage.ADMIN_PERMISSION_REQUIRED);
+			throw new ForbiddenException(
+				ErrorCode.ADMIN_PERMISSION_REQUIRED,
+				ErrorMessage.ADMIN_PERMISSION_REQUIRED
+			);
 		}
 
 		final Member targetMember = memberReadService.findMemberById(targetMemberId);
 
-		memberWriteService.updateRole(targetMember, nextRole, adminMemberId);
+		memberWriteService.changeRole(targetMember, nextRole, adminMemberId);
 	}
 }
