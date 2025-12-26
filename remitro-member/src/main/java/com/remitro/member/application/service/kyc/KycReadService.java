e3ab4c6805dd8908fc54a84424fc5d38702f6f23
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 15:41:57 +0900

    feat: 최근 KycVerification 조회 읽기 작업 구현

--- remitro-member/src/main/java/com/remitro/member/application/service/kyc/KycReadService.java
+++ remitro-member/src/main/java/com/remitro/member/application/service/kyc/KycReadService.java
@@ -1,9 +1,14 @@
 package com.remitro.member.application.service.kyc;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.remitro.common.error.code.ErrorCode;
 import com.remitro.common.error.exception.NotFoundException;
-import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.common.error.message.ErrorMessage;
+import com.remitro.member.domain.enums.KycVerificationStatus;
 import com.remitro.member.domain.model.KycVerification;
 import com.remitro.member.domain.repository.KycVerificationRepository;
 
@@ -17,6 +22,19 @@ public class KycReadService {
 
 	public KycVerification findKycVerificationByMemberId(Long memberId) {
 		return kycVerificationRepository.findTopByMemberIdOrderByRequestedAtDesc(memberId)
-			.orElseThrow(() -> new NotFoundException(ErrorMessage.KYC_VERIFICATION_NOT_FOUND));
+			.orElseThrow(() -> new NotFoundException(
+				ErrorCode.KYC_VERIFICATION_NOT_FOUND,
+				ErrorMessage.KYC_VERIFICATION_NOT_FOUND)
+			);
+	}
+
+	public Optional<KycVerification> findLatestKycVerification(Long memberId) {
+		return kycVerificationRepository.findTopByMemberIdOrderByRequestedAtDesc(memberId);
+	}
+
+	public List<KycVerification> findAllPending() {
+		return kycVerificationRepository.findAllByKycVerificationStatusOrderByRequestedAtAsc(
+			KycVerificationStatus.PENDING
+		);
 	}
 }
