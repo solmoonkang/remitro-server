66e9900e3e744a27ca923cbe23dc8c15a13283ef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 18:14:29 +0900

    feat: 계정 잠금 주체 필드 추가

--- remitro-event/src/main/java/com/remitro/event/member/event/MemberLockedEvent.java
+++ remitro-event/src/main/java/com/remitro/event/member/event/MemberLockedEvent.java
@@ -2,12 +2,15 @@ package com.remitro.event.member.event;
 
 import java.time.LocalDateTime;
 
+import com.remitro.event.member.enums.LockActorType;
 import com.remitro.event.member.enums.MemberLockReason;
 
 public record MemberLockedEvent(
 	Long memberId,
 
 	Long adminMemberId,
+
+	LockActorType lockActorType,
 
 	MemberLockReason lockReason,
 
