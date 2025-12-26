cda7a67f90c523c416a1bccaec21d0c575e2d3af
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 15:57:03 +0900

    refactor: 회원 정보 검증 메서드명 수정 및 불필요한 로직 제거

--- remitro-member/src/main/java/com/remitro/member/application/service/member/MemberService.java
+++ remitro-member/src/main/java/com/remitro/member/application/service/member/MemberService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.member.application.dto.request.SignUpRequest;
-import com.remitro.member.application.dto.request.UpdateActivityStatusRequest;
 import com.remitro.member.application.dto.response.MemberInfoResponse;
 import com.remitro.member.application.mapper.MemberMapper;
 import com.remitro.member.application.validator.MemberValidator;
@@ -22,22 +21,16 @@ public class MemberService {
 	private final MemberReadService memberReadService;
 
 	@Transactional
-	public void signUpMember(SignUpRequest signUpRequest) {
-		memberValidator.validateEmailNotDuplicated(signUpRequest.email());
-		memberValidator.validateNicknameNotDuplicated(signUpRequest.nickname());
-		memberValidator.validatePhoneNumberNotDuplicated(signUpRequest.phoneNumber());
-		memberValidator.validatePasswordMatche(signUpRequest.password(), signUpRequest.checkPassword());
-		memberWriteService.saveMember(signUpRequest);
+	public void signUp(SignUpRequest signUpRequest) {
+		memberValidator.validateUniqueEmail(signUpRequest.email());
+		memberValidator.validateUniqueNickname(signUpRequest.nickname());
+		memberValidator.validateUniquePhoneNumber(signUpRequest.phoneNumber());
+		memberValidator.validatePasswordMatches(signUpRequest.password(), signUpRequest.checkPassword());
+		memberWriteService.register(signUpRequest);
 	}
 
-	public MemberInfoResponse findMemberInfo(Long memberId) {
+	public MemberInfoResponse getMemberInfo(Long memberId) {
 		final Member member = memberReadService.findMemberById(memberId);
 		return MemberMapper.toMemberInfoResponse(member);
-	}
-
-	@Transactional
-	public void updateActivityStatus(Long memberId, UpdateActivityStatusRequest updateActivityStatusRequest) {
-		final Member member = memberReadService.findMemberById(memberId);
-		memberWriteService.updateActivityStatus(member, updateActivityStatusRequest.activityStatus());
 	}
 }
