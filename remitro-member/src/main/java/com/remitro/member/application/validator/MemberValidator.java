4581967b7aed7c573d23e4e1575ff193568d6af9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-26 15:57:29 +0900

    refactor: 가독성 향상을 위한 메서드명 수정

--- remitro-member/src/main/java/com/remitro/member/application/validator/MemberValidator.java
+++ remitro-member/src/main/java/com/remitro/member/application/validator/MemberValidator.java
@@ -2,9 +2,10 @@ package com.remitro.member.application.validator;
 
 import org.springframework.stereotype.Component;
 
+import com.remitro.common.error.code.ErrorCode;
 import com.remitro.common.error.exception.BadRequestException;
 import com.remitro.common.error.exception.ConflictException;
-import com.remitro.common.error.model.ErrorMessage;
+import com.remitro.common.error.message.ErrorMessage;
 import com.remitro.member.domain.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,27 +16,39 @@ public class MemberValidator {
 
 	private final MemberRepository memberRepository;
 
-	public void validateEmailNotDuplicated(String email) {
+	public void validateUniqueEmail(String email) {
 		if (memberRepository.existsByEmail(email)) {
-			throw new ConflictException(ErrorMessage.EMAIL_DUPLICATED);
+			throw new ConflictException(
+				ErrorCode.EMAIL_ALREADY_EXISTS,
+				ErrorMessage.EMAIL_ALREADY_EXISTS
+			);
 		}
 	}
 
-	public void validateNicknameNotDuplicated(String nickname) {
+	public void validateUniqueNickname(String nickname) {
 		if (memberRepository.existsByNickname(nickname)) {
-			throw new ConflictException(ErrorMessage.NICKNAME_DUPLICATED);
+			throw new ConflictException(
+				ErrorCode.NICKNAME_ALREADY_EXISTS,
+				ErrorMessage.NICKNAME_ALREADY_EXISTS
+			);
 		}
 	}
 
-	public void validatePhoneNumberNotDuplicated(String phoneNumber) {
+	public void validateUniquePhoneNumber(String phoneNumber) {
 		if (memberRepository.existsByPhoneNumber(phoneNumber)) {
-			throw new ConflictException(ErrorMessage.PHONE_NUMBER_DUPLICATED);
+			throw new ConflictException(
+				ErrorCode.PHONE_NUMBER_ALREADY_EXISTS,
+				ErrorMessage.PHONE_NUMBER_ALREADY_EXISTS
+			);
 		}
 	}
 
-	public void validatePasswordMatche(String password, String checkPassword) {
+	public void validatePasswordMatches(String password, String checkPassword) {
 		if (!password.equals(checkPassword)) {
-			throw new BadRequestException(ErrorMessage.INVALID_PASSWORD);
+			throw new BadRequestException(
+				ErrorCode.PASSWORD_INVALID,
+				ErrorMessage.PASSWORD_INVALID
+			);
 		}
 	}
 }
