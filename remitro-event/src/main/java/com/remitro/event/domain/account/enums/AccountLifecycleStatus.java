7404aef6f33e2715b0aa6a74769aae14ea608e8c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-31 04:55:32 +0900

    feat: 계좌 라이프사이클 상태 열거형 정의

---
+++ remitro-event/src/main/java/com/remitro/event/domain/account/enums/AccountLifecycleStatus.java
@@ -0,0 +1,12 @@
+package com.remitro.event.domain.account.enums;
+
+public enum AccountLifecycleStatus {
+
+	NORMAL,
+	FROZEN,
+	SUSPENDED,
+	DORMANT,
+	INACTIVE,
+	TERMINATED,
+	PENDING_TERMINATION
+}
