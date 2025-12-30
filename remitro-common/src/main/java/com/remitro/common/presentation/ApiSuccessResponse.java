76d4625637550fcecef13bd948205181953a3c47
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-31 05:01:04 +0900

    refactor: 스웨거 설정 제거

--- remitro-common/src/main/java/com/remitro/common/presentation/ApiSuccessResponse.java
+++ remitro-common/src/main/java/com/remitro/common/presentation/ApiSuccessResponse.java
@@ -1,10 +1,6 @@
 package com.remitro.common.presentation;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "API 성공 응답")
 public record ApiSuccessResponse(
-	@Schema(description = "성공 메시지", example = "요청이 성공적으로 처리되었습니다.")
 	String message
 ) {
 
