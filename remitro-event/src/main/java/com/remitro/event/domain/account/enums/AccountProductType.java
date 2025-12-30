7e9394b321d9e3bb6f5aaffa9df259943ee58f7d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-12-31 04:57:15 +0900

    feat: 계좌 상품 타입 열거형 정의

---
+++ remitro-event/src/main/java/com/remitro/event/domain/account/enums/AccountProductType.java
@@ -0,0 +1,15 @@
+package com.remitro.event.domain.account.enums;
+
+public enum AccountProductType {
+
+	CHECKING,
+	SAVINGS,
+	FIXED_DEPOSIT,
+	INSTALLMENT_SAVINGS,
+	CMA,
+	LOAN,
+	CREDIT_LIMIT,
+	FOREIGN_CURRENCY,
+	HIGH_INTEREST_SAVING,
+	VIRTUAL
+}
