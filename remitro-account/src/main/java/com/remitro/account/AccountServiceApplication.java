395ab1ca164b742c8d7b0ddb3affaa0ba0ae4113
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-02 03:52:59 +0900

    feat: 스캔 범위 지정

--- remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
+++ remitro-account/src/main/java/com/remitro/account/AccountServiceApplication.java
@@ -4,17 +4,22 @@ import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.remitro.account.infrastructure.config.RedisProperties;
 
 @SpringBootApplication(
-	exclude = {RedissonAutoConfiguration.class}
+	exclude = {
+		RedissonAutoConfiguration.class
+	},
+	scanBasePackages = {
+		"com.remitro.account",
+		"com.remitro.common",
+		"com.remitro.event"
+	}
 )
 @EnableJpaAuditing
 @EnableConfigurationProperties(RedisProperties.class)
-@ComponentScan(basePackages = "com.remitro.common")
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
