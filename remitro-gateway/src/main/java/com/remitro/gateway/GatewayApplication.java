cb6ffdbb7105f2551951a2d47f1ae95808ff3967
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-03 04:20:20 +0900

    refactor: 토큰 설정 클래스명 수정

--- remitro-gateway/src/main/java/com/remitro/gateway/GatewayApplication.java
+++ remitro-gateway/src/main/java/com/remitro/gateway/GatewayApplication.java
@@ -5,15 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import com.remitro.gateway.config.TokenConfig;
+import com.remitro.gateway.config.JwtConfig;
 
-@EnableDiscoveryClient
-@EnableConfigurationProperties(TokenConfig.class)
 @SpringBootApplication
-public class ApiGatewayApplication {
-
+@EnableDiscoveryClient
+@EnableConfigurationProperties(JwtConfig.class)
+public class GatewayApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
