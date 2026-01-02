f1af151ea95c424de8cf607ea988423bf6540612
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-03 03:44:57 +0900

    refactor: 클래스명 수정

--- remitro-discovery/src/main/java/com/remitro/eureka/DiscoveryApplication.java
+++ remitro-discovery/src/main/java/com/remitro/eureka/DiscoveryApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
-
+@EnableEurekaServer
+public class DiscoveryApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(DiscoveryApplication.class, args);
 	}
-
 }
