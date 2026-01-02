49c5c2f738c2b98912e35e5435d6acd4964a94c6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-03 03:13:28 +0900

    chore: 초기 셋팅

---
+++ remitro-audit/src/main/java/com/remitro/audit/RemitroAuditApplication.java
@@ -0,0 +1,13 @@
+package com.remitro.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitroAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitroAuditApplication.class, args);
+	}
+
+}
---
+++ remitro-audit/src/test/java/com/remitro/audit/RemitroAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitro.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitroAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
