f559ac6e306354854ec87cd1acaa7bbda88c4511
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-03 03:11:27 +0900

    chore: 초기 셋팅

---
+++ remitro-notification/src/main/java/com/remitro/notification/RemitroNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.remitro.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitroNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitroNotificationApplication.class, args);
+	}
+
+}
---
+++ remitro-notification/src/test/java/com/remitro/notification/RemitroNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remitro.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitroNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
