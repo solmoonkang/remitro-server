62da057a2347795ae65dafe58b32ce05f0cd12a0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-06 17:00:11 +0900

    refactor: 빈 등록을 위한 어노테이션 추가

--- remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberSignUpPolicy.java
+++ remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberSignUpPolicy.java
@@ -1,27 +1,30 @@
 package com.remitro.member.domain.member.policy;
 
+import org.springframework.stereotype.Component;
+
 import com.remitro.common.error.code.ErrorCode;
 import com.remitro.common.error.exception.ConflictException;
 import com.remitro.common.error.message.ErrorMessage;
 
+@Component
 public class MemberSignUpPolicy {
 
 	public void validateDuplicated(boolean emailExists, boolean nicknameExists, boolean phoneExists) {
 		if (emailExists) {
 			throw new ConflictException(
-				ErrorCode.EMAIL_ALREADY_EXISTS, ErrorMessage.EMAIL_ALREADY_EXISTS
+				ErrorCode.MEMBER_DUPLICATED_RESOURCE, ErrorMessage.DUPLICATE_EMAIL
 			);
 		}
 
 		if (nicknameExists) {
 			throw new ConflictException(
-				ErrorCode.NICKNAME_ALREADY_EXISTS, ErrorMessage.NICKNAME_ALREADY_EXISTS
+				ErrorCode.MEMBER_DUPLICATED_RESOURCE, ErrorMessage.DUPLICATE_NICKNAME
 			);
 		}
 
 		if (phoneExists) {
 			throw new ConflictException(
-				ErrorCode.PHONE_NUMBER_ALREADY_EXISTS, ErrorMessage.PHONE_NUMBER_ALREADY_EXISTS
+				ErrorCode.MEMBER_DUPLICATED_RESOURCE, ErrorMessage.DUPLICATE_PHONE_NUMBER
 			);
 		}
 	}
