192b1ed95f29e6dc778ca09a8c16e7eecb030ef0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-06 16:51:09 +0900

    feat: 회원 ID를 통한 조회 로직 구현

--- remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/kyc/JpaKycVerificationQueryRepository.java
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/kyc/JpaKycVerificationQueryRepository.java
@@ -1,7 +1,10 @@
 package com.remitro.member.infrastructure.persistence.kyc;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
+import com.remitro.member.domain.kyc.model.KycVerification;
 import com.remitro.member.domain.kyc.repository.KycVerificationQueryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class JpaKycVerificationQueryRepository implements KycVerificationQueryRepository {
 
 	private final SpringDataKycVerificationRepository springDataKycVerificationRepository;
+
+	@Override
+	public Optional<KycVerification> findByMemberId(Long memberId) {
+		return springDataKycVerificationRepository.findByMemberId(memberId);
+	}
 }
