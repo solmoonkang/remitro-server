478ab7eb917324f922fe59617a33929223b4cf9e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-06 16:51:45 +0900

    feat: KYC 이벤트 매핑 로직 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/event/KycEventFactory.java
@@ -0,0 +1,35 @@
+package com.remitro.member.application.event;
+
+import com.remitro.event.domain.kyc.KycApprovedEvent;
+import com.remitro.event.domain.kyc.KycRejectedEvent;
+import com.remitro.event.domain.kyc.KycRequestedEvent;
+import com.remitro.member.domain.kyc.model.KycVerification;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class KycEventFactory {
+
+	public static KycRequestedEvent createRequestedEvent(KycVerification kycVerification) {
+		return new KycRequestedEvent(
+			kycVerification.getMemberId(),
+			kycVerification.getId()
+		);
+	}
+
+	public static KycApprovedEvent createApprovedEvent(KycVerification kycVerification) {
+		return new KycApprovedEvent(
+			kycVerification.getMemberId(),
+			kycVerification.getId()
+		);
+	}
+
+	public static KycRejectedEvent createRejectedEvent(KycVerification kycVerification) {
+		return new KycRejectedEvent(
+			kycVerification.getMemberId(),
+			kycVerification.getId(),
+			kycVerification.getReason()
+		);
+	}
+}
