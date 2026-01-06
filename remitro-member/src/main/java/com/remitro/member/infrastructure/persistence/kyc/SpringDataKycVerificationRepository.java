2a6caf8d738bfbb3d12487da6110f5d9485cdfbf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-06 17:00:35 +0900

    feat: KycVerification JPA 저장소 정의

--- remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/kyc/SpringDataKycVerificationRepository.java
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/kyc/SpringDataKycVerificationRepository.java
@@ -1,9 +1,12 @@
 package com.remitro.member.infrastructure.persistence.kyc;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.remitro.member.domain.kyc.model.KycVerification;
 
 public interface SpringDataKycVerificationRepository extends JpaRepository<KycVerification, Long> {
 
+	Optional<KycVerification> findByMemberId(Long memberId);
 }
