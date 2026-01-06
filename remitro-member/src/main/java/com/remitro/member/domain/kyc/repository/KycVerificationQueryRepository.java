211d2c9b41739f0822bdb7847448688c2022bd63
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-06 16:57:22 +0900

    feat: KycVerification 조회 저장소 구현

--- remitro-member/src/main/java/com/remitro/member/domain/kyc/repository/KycVerificationQueryRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/kyc/repository/KycVerificationQueryRepository.java
@@ -1,4 +1,10 @@
 package com.remitro.member.domain.kyc.repository;
 
+import java.util.Optional;
+
+import com.remitro.member.domain.kyc.model.KycVerification;
+
 public interface KycVerificationQueryRepository {
+
+	Optional<KycVerification> findByMemberId(Long memberId);
 }
