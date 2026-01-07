be8ebc08860534e63390fe374c027dc2350dde4a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-07 17:53:04 +0900

    feat: 비활성화 회원 목록 조회 로직 구현

--- remitro-member/src/main/java/com/remitro/member/domain/member/repository/MemberCommandRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/member/repository/MemberCommandRepository.java
@@ -1,8 +1,14 @@
 package com.remitro.member.domain.member.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.remitro.member.domain.member.model.ActivityStatus;
 import com.remitro.member.domain.member.model.Member;
 
 public interface MemberCommandRepository {
 
 	Member save(Member member);
+
+	List<Member> findActiveMembersInactiveSince(ActivityStatus activityStatus, LocalDateTime threshold);
 }
