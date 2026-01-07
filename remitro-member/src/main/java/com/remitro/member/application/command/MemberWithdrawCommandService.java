e096497a7c13514119d5eedb5f84ea8ad5fc29d9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-07 17:55:34 +0900

    feat: 회원 탈퇴 서비스 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/command/MemberWithdrawCommandService.java
@@ -0,0 +1,28 @@
+package com.remitro.member.application.command;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.member.application.event.MemberEventPublisher;
+import com.remitro.member.application.support.MemberFinder;
+import com.remitro.member.domain.member.model.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberWithdrawCommandService {
+
+	private final MemberFinder memberFinder;
+
+	private final MemberEventPublisher memberEventPublisher;
+
+	public void withdraw(Long memberId) {
+		final Member member = memberFinder.getById(memberId);
+
+		member.withdrawn();
+
+		memberEventPublisher.publishMemberWithdrawn(member);
+	}
+}
