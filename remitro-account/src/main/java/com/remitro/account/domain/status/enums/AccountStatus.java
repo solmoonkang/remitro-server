8d064d35d1ced731c54e7e3e96bb71e124b09e14
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-11 00:50:50 +0900

    feat: 계좌 상태 열거형 정의

---
+++ remitro-account/src/main/java/com/remitro/account/domain/status/enums/AccountStatus.java
@@ -0,0 +1,15 @@
+package com.remitro.account.domain.status.enums;
+
+public enum AccountStatus {
+
+	NORMAL,
+
+	FROZEN,
+	SUSPENDED,
+
+	DORMANT,
+	INACTIVE,
+
+	PENDING_TERMINATION,
+	TERMINATED
+}
