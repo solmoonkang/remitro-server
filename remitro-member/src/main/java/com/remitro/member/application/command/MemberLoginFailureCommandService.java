5c1733fa65c3df54c4759d475c9faebb9a5a8b0f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-11 01:12:45 +0900

    refactor: 패키지 위치 이동

--- remitro-member/src/main/java/com/remitro/member/application/command/MemberLoginFailureCommandService.java
+++ remitro-member/src/main/java/com/remitro/member/application/command/MemberLoginFailureCommandService.java
@@ -8,8 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.member.application.event.MemberEventPublisher;
 import com.remitro.member.application.support.MemberFinder;
-import com.remitro.member.domain.member.model.ActivityStatus;
+import com.remitro.member.domain.member.enums.ActivityStatus;
 import com.remitro.member.domain.member.model.Member;
+import com.remitro.member.domain.status.enums.StatusChangeReason;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,15 +20,25 @@ import lombok.RequiredArgsConstructor;
 public class MemberLoginFailureCommandService {
 
 	private final MemberFinder memberFinder;
+	private final MemberStatusHistoryRecorder memberStatusHistoryRecorder;
 
 	private final MemberEventPublisher memberEventPublisher;
 	private final Clock clock;
 
 	public void handleLoginFailure(Long memberId) {
 		final Member member = memberFinder.getById(memberId);
+
+		final ActivityStatus previousStatus = member.getActivityStatus();
 		member.increaseLoginFailure(LocalDateTime.now(clock));
 
-		if (member.getActivityStatus() == ActivityStatus.LOCKED) {
+		if (previousStatus != ActivityStatus.LOCKED && member.getActivityStatus() == ActivityStatus.LOCKED) {
+			memberStatusHistoryRecorder.record(
+				member.getId(),
+				previousStatus,
+				ActivityStatus.LOCKED,
+				StatusChangeReason.LOGIN_FAILURE_LOCK
+			);
+
 			memberEventPublisher.publishMemberLocked(member);
 		}
 	}
