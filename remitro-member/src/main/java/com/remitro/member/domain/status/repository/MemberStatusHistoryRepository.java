9e7bdd760f32acf321def05ef87813bc0b9ed8c1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-11 01:14:31 +0900

    feat: 회원 상태 기록 저장소 정의

---
+++ remitro-member/src/main/java/com/remitro/member/domain/status/repository/MemberStatusHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.remitro.member.domain.status.repository;
+
+import com.remitro.member.domain.status.model.MemberStatusHistory;
+
+public interface MemberStatusHistoryRepository {
+
+	void save(MemberStatusHistory memberStatusHistory);
+}
