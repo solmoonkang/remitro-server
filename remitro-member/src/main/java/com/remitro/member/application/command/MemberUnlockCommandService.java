adb62524f73ab2475cc2387a5205edb90a8510ca
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-11 01:14:38 +0900

    refactor: 불필요한 로직 제거 및 메서드명 수정

--- remitro-member/src/main/java/com/remitro/member/application/command/MemberUnlockCommandService.java
+++ remitro-member/src/main/java/com/remitro/member/application/command/MemberUnlockCommandService.java
@@ -5,7 +5,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.member.application.event.MemberEventPublisher;
 import com.remitro.member.application.support.MemberFinder;
+import com.remitro.member.domain.member.enums.ActivityStatus;
 import com.remitro.member.domain.member.model.Member;
+import com.remitro.member.domain.status.enums.StatusChangeReason;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,13 +17,22 @@ import lombok.RequiredArgsConstructor;
 public class MemberUnlockCommandService {
 
 	private final MemberFinder memberFinder;
+	private final MemberStatusHistoryRecorder memberStatusHistoryRecorder;
 
 	private final MemberEventPublisher memberEventPublisher;
 
 	public void unlock(Long memberId) {
 		final Member member = memberFinder.getById(memberId);
 
+		final ActivityStatus previousStatus = member.getActivityStatus();
 		member.unlock();
+
+		memberStatusHistoryRecorder.record(
+			member.getId(),
+			previousStatus,
+			member.getActivityStatus(),
+			StatusChangeReason.ADMIN_UNLOCK
+		);
 
 		memberEventPublisher.publishMemberUnlocked(member);
 	}
