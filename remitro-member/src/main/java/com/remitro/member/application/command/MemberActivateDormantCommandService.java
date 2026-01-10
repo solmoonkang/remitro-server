962c34ff3c9f8dd7558a11186aaf61e717e423c5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-11 01:08:19 +0900

    refactor: 패키지 위치 이동

--- remitro-member/src/main/java/com/remitro/member/application/command/MemberActivateDormantCommandService.java
+++ remitro-member/src/main/java/com/remitro/member/application/command/MemberActivateDormantCommandService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.remitro.member.application.support.MemberFinder;
+import com.remitro.member.domain.member.enums.ActivityStatus;
 import com.remitro.member.domain.member.model.Member;
+import com.remitro.member.domain.status.enums.StatusChangeReason;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,12 +16,22 @@ import lombok.RequiredArgsConstructor;
 public class MemberActivateDormantCommandService {
 
 	private final MemberFinder memberFinder;
+	private final MemberStatusHistoryRecorder memberStatusHistoryRecorder;
 
 	public void activate(Long memberId) {
 		final Member member = memberFinder.getById(memberId);
-
-		if (member.isDormant()) {
-			member.unlock();
+		if (!member.isDormant()) {
+			return;
 		}
+
+		final ActivityStatus previousStatus = member.getActivityStatus();
+		member.unlock();
+
+		memberStatusHistoryRecorder.record(
+			member.getId(),
+			previousStatus,
+			member.getActivityStatus(),
+			StatusChangeReason.DORMANT_ACTIVATED_BY_LOGIN
+		);
 	}
 }
