748211b19c6bcb39a5222dd9792f44d84cb8a0e1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-19 01:44:02 +0900

    feat(#21): 로그인 서비스 로직 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/command/LoginCommandService.java
@@ -0,0 +1,35 @@
+package com.remitro.member.application.command;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.member.application.command.dto.request.LoginRequest;
+import com.remitro.member.application.command.dto.response.LoginResponse;
+import com.remitro.member.application.mapper.TokenMapper;
+import com.remitro.member.application.support.MemberFinder;
+import com.remitro.member.domain.member.model.Member;
+import com.remitro.member.domain.member.policy.MemberPasswordPolicy;
+import com.remitro.member.infrastructure.security.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LoginCommandService {
+
+	private final MemberFinder memberFinder;
+	private final MemberPasswordPolicy memberPasswordPolicy;
+	private final JwtTokenProvider jwtTokenProvider;
+
+	public LoginResponse login(LoginRequest loginRequest) {
+		final Member member = memberFinder.getMemberByEmail(loginRequest.email());
+
+		memberPasswordPolicy.validatePasswordMatch(loginRequest.password(), member.getPassword());
+
+		final String accessToken = jwtTokenProvider.issueAccessToken(member.getId());
+		final String refreshToken = jwtTokenProvider.issueRefreshToken(member.getId());
+
+		return TokenMapper.toLoginResponse(accessToken, refreshToken);
+	}
+}
