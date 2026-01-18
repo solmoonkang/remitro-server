0f7fe3f88d8454336e35c91d34d6a4b424069a6b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-19 02:25:20 +0900

    refactor(#21): 메서드명 오타 수정

--- remitro-member/src/main/java/com/remitro/member/domain/token/repository/RefreshTokenRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/token/repository/RefreshTokenRepository.java
@@ -1,14 +1,10 @@
 package com.remitro.member.domain.token.repository;
 
-import java.util.Optional;
-
 import com.remitro.member.domain.token.model.RefreshToken;
 
 public interface RefreshTokenRepository {
 
-	Optional<RefreshToken> findByToken(String token);
-
 	void save(RefreshToken refreshToken);
 
-	void delteByMemberId(Long memberId);
+	void deleteByMemberId(Long memberId);
 }
