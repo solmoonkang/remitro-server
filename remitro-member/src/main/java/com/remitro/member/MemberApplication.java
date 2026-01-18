b86423ad55f8343cd8b2e635354fe4fc37b10738
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-18 22:19:38 +0900

    feat(#21): JwtProperties 설정 도입

--- remitro-member/src/main/java/com/remitro/member/MemberApplication.java
+++ remitro-member/src/main/java/com/remitro/member/MemberApplication.java
@@ -2,10 +2,14 @@ package com.remitro.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.remitro.member.infrastructure.security.JwtProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(JwtProperties.class)
 public class MemberApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemberApplication.class, args);
