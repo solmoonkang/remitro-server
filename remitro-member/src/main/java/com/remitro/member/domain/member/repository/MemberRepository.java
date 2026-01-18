b61cf8d998de469f9b80fa13f1e29ace8267c9bc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-19 01:43:39 +0900

    feat(#21): Member 조회 로직 구현

--- remitro-member/src/main/java/com/remitro/member/domain/member/repository/MemberRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/member/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.remitro.member.domain.member.repository;
 
+import java.util.Optional;
+
 import com.remitro.member.domain.member.model.Member;
 
 public interface MemberRepository {
+
+	Optional<Member> findById(Long memberId);
+
+	Optional<Member> findByEmail(String email);
 
 	boolean existsByEmail(String email);
 
