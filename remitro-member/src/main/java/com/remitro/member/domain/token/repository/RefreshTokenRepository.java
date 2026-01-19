aafbe7f413c3c3aa3b8ac243019c39b77f16dda3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-19 14:58:51 +0900

    feat(#25): 리프레시 토큰 조회 로직 구현

--- remitro-member/src/main/java/com/remitro/member/domain/token/repository/RefreshTokenRepository.java
+++ remitro-member/src/main/java/com/remitro/member/domain/token/repository/RefreshTokenRepository.java
@@ -1,8 +1,12 @@
 package com.remitro.member.domain.token.repository;
 
+import java.util.Optional;
+
 import com.remitro.member.domain.token.model.RefreshToken;
 
 public interface RefreshTokenRepository {
+
+	Optional<RefreshToken> findByMemberId(Long memberId);
 
 	void save(RefreshToken refreshToken);
 
