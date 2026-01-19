4f2f46a212f3dd5ed841c5e85285a339b117acd8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-19 17:00:01 +0900

    feat(#25): 비밀번호 변경 검증 정책 추가

--- remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberPasswordPolicy.java
+++ remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberPasswordPolicy.java
@@ -3,6 +3,7 @@ package com.remitro.member.domain.member.policy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.remitro.common.error.ErrorCode;
+import com.remitro.common.exception.BadRequestException;
 import com.remitro.common.exception.UnauthorizedException;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,25 @@ public class MemberPasswordPolicy {
 	public void validatePasswordMatch(String rawPassword, String encodedPassword) {
 		if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
 			throw new UnauthorizedException(ErrorCode.INVALID_PASSWORD);
+		}
+	}
+
+	public void validatePasswordChange(
+		String encodedPassword,
+		String currentPassword,
+		String newPassword,
+		String confirmPassword
+	) {
+		if (!passwordEncoder.matches(currentPassword, encodedPassword)) {
+			throw new BadRequestException(ErrorCode.WRONG_PASSWORD);
+		}
+
+		if (currentPassword.equals(newPassword)) {
+			throw new BadRequestException(ErrorCode.PASSWORD_REUSE_DENIED);
+		}
+
+		if (!newPassword.equals(confirmPassword)) {
+			throw new BadRequestException(ErrorCode.PASSWORD_CONFIRM_MISMATCH);
 		}
 	}
 }
