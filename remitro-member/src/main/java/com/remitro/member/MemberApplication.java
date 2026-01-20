014c06438f3efc708c1ca6c204d6e5893e9d4be5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-21 02:28:05 +0900

    refactor(#29): 자동 스캔 활성화

--- remitro-member/src/main/java/com/remitro/member/MemberApplication.java
+++ remitro-member/src/main/java/com/remitro/member/MemberApplication.java
@@ -2,16 +2,14 @@ package com.remitro.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.remitro.member.infrastructure.security.JwtProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-@EnableConfigurationProperties(JwtProperties.class)
+@ConfigurationPropertiesScan
 public class MemberApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemberApplication.class, args);
