2736a289f184d91993a7a8a65d9b758d3cd1aea3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-20 18:21:41 +0900

    feat(#29): 스케줄러 활성화

--- remitro-member/src/main/java/com/remitro/member/MemberApplication.java
+++ remitro-member/src/main/java/com/remitro/member/MemberApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.remitro.member.infrastructure.security.JwtProperties;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 public class MemberApplication {
 	public static void main(String[] args) {
