6010db0ff546622ff7a2b7ee630d4c25c21633c0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-21 03:31:23 +0900

    chore(#29): data.sql 파일 위치 수정

--- remitro-member/src/main/resources/resources/data.sql
+++
@@ -1,6 +0,0 @@
--- 1. 테스트용 데이터 삽입
-INSERT INTO member (email, nickname, phone_number, member_status, last_login_at, created_at, updated_at)
-VALUES ('testMember@example.com', 'testNickname', '01012345678', 'ACTIVE', '2023-01-01T00:00:00', now(), now());
-
--- 2. 특정 데이터를 휴면 대상(1년 전)으로 변경
-UPDATE member SET last_login_at = DATEADD('YEAR', -1, CURRENT_TIMESTAMP) WHERE email = 'testMember@example.com';
