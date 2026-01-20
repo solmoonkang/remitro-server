6d269cddb8bf6bb22c741a56b4c49093bc6e8373
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-21 02:27:37 +0900

    chore(#29): 역할 명확함을 위해 패키지 이동

--- remitro-member/src/main/java/com/remitro/member/application/batch/InactivityBatchScheduler.java
+++ remitro-member/src/main/java/com/remitro/member/application/batch/InactivityBatchScheduler.java
@@ -1,9 +1,7 @@
-package com.remitro.member.application.scheduler;
+package com.remitro.member.application.batch;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import com.remitro.member.application.command.DormancyCommandService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
