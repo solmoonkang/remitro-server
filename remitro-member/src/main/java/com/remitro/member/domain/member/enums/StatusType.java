eef9ba2cb8aee0e0549bf8ac44e9b90ad99f81af
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-22 04:14:01 +0900

    feat(#30): 보안 및 운영 측면 상태 정의

---
+++ remitro-member/src/main/java/com/remitro/member/domain/member/enums/StatusType.java
@@ -0,0 +1,14 @@
+package com.remitro.member.domain.member.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum StatusType {
+
+	MEMBER_STATUS("회원 운영 상태"),
+	LOGIN_SECURITY_STATUS("로그인 보안 상태");
+
+	private final String description;
+}
