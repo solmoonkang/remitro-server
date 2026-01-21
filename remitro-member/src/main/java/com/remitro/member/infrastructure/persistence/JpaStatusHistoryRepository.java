0f16f1b1768fdbf9f01baad0180794a92e98e78a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-22 02:38:47 +0900

    feat(#30): 회원 상태 이력 JPA 저장소 정의

---
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/JpaStatusHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.remitro.member.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.remitro.member.domain.member.model.StatusHistory;
+import com.remitro.member.domain.member.repository.StatusHistoryRepository;
+
+public interface JpaStatusHistoryRepository extends
+	JpaRepository<StatusHistory, Long>,
+	StatusHistoryRepository {
+}
