51b03fd1376779bf08bee64b639839909aadb406
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-22 04:14:54 +0900

    refactor(#30): 반환 타입 수정

--- remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberLoginPolicy.java
+++ remitro-member/src/main/java/com/remitro/member/domain/member/policy/MemberLoginPolicy.java
@@ -4,16 +4,19 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
-import com.remitro.member.domain.member.enums.MemberStatus;
+import com.remitro.member.domain.member.enums.LoginSecurityStatus;
 import com.remitro.member.domain.member.model.Member;
 
 @Component
 public class MemberLoginPolicy {
 
-	public void validateLoginable(Member member, LocalDateTime now) {
-		if (member.getMemberStatus() == MemberStatus.LOCKED && member.isUnlockable(now)) {
+	public boolean validateLoginable(Member member, LocalDateTime now) {
+		if (member.getLoginSecurityStatus() == LoginSecurityStatus.LOCKED && member.isUnlockable(now)) {
 			member.unlock();
+			return true;
 		}
+
+		return false;
 	}
 
 	public void validateFailure(Member member, LocalDateTime now) {
