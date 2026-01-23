ec3922ce538f69ed581f3ba945c481c78ee4c5fd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-23 20:45:25 +0900

    feat(#31): 탈퇴 계정 관련 에러 코드 추가

--- remitro-common-support/src/main/java/com/remitro/common/error/ErrorCode.java
+++ remitro-common-support/src/main/java/com/remitro/common/error/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
 	METHOD_NOT_ALLOWED(405, "C405001", "허용되지 않은 HTTP 메소드입니다."),
 
 	INTERNAL_SERVER_ERROR(500, "C500001", "서버 내부 오류가 발생했습니다."),
+	ALGORITHM_CRYPTO_ERROR(500, "C500002", "암호화 알고리즘 실행 중 오류가 발생했습니다."),
 
 	// --- A (Auth): 인증 및 인가 --- //
 	UNAUTHORIZED_ACCESS(401, "A401001", "인증이 필요합니다."),
@@ -22,12 +23,15 @@ public enum ErrorCode {
 
 	// --- M (Member): 회원 계정 및 상태 --- //
 	ALREADY_SUSPENDED(400, "M400001", "이미 정지 처리된 계정입니다."),
-	NOT_SUSPENDED(400, "M400002", "정지 상태가 아닌 계정은 해제할 수 없습니다."),
-	INVALID_SUSPEND_UNTIL(400, "M400003", "정지 해제 예정 시각은 현재 시각보다 이후여야 합니다."),
+	ALREADY_WITHDRAWN(400, "M400002", "이미 탈퇴 처리된 계정입니다."),
+	NOT_SUSPENDED(400, "M400003", "정지 상태가 아닌 계정은 해제할 수 없습니다."),
+	INVALID_SUSPEND_UNTIL(400, "M400004", "정지 해제 예정 시각은 현재 시각보다 이후여야 합니다."),
+	REJOIN_RESTRICTED_PERIOD(400, "M400005", "탈퇴 후 30일 이내에는 재가입이 불가능합니다."),
 
 	MEMBER_LOCKED(403, "M403001", "로그인 5회 실패로 인해 잠긴 계정입니다."),
 	MEMBER_DORMANT(403, "M403002", "장기 미접속으로 인한 휴면 계정입니다."),
 	MEMBER_SUSPENDED(403, "M403003", "관리자에 의해 정지된 계정입니다."),
+	MEMBER_WITHDRAWN(403, "M403004", "존재하지 않는 계정입니다."),
 
 	MEMBER_NOT_FOUND(404, "M404001", "존재하지 않는 회원입니다."),
 
