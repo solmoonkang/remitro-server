8c54ebaba1111fb2a6d251117a93ddb535686e13
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-23 20:49:20 +0900

    feat(#31): 토큰 관련 검증 로직 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/validator/TokenValidator.java
@@ -0,0 +1,23 @@
+package com.remitro.member.application.validator;
+
+import org.springframework.stereotype.Component;
+
+import com.remitro.common.error.ErrorCode;
+import com.remitro.common.exception.UnauthorizedException;
+import com.remitro.member.domain.token.model.RefreshToken;
+import com.remitro.member.domain.token.policy.RefreshTokenPolicy;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TokenValidator {
+
+	private final RefreshTokenPolicy refreshTokenPolicy;
+
+	public void validateTokenMatch(RefreshToken storedToken, String requestToken) {
+		if (!refreshTokenPolicy.matches(storedToken, requestToken)) {
+			throw new UnauthorizedException(ErrorCode.INVALID_TOKEN, "인증 정보");
+		}
+	}
+}
