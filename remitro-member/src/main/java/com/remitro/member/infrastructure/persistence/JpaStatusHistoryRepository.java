bb404e2128b745b7400e019afb1f0b8f165859ef
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-24 19:59:27 +0900

    refactor(#35): audit 패키지로 이동

--- remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/JpaStatusHistoryRepository.java
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/JpaStatusHistoryRepository.java
@@ -2,8 +2,8 @@ package com.remitro.member.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.remitro.member.domain.member.model.StatusHistory;
-import com.remitro.member.domain.member.repository.StatusHistoryRepository;
+import com.remitro.member.domain.history.model.StatusHistory;
+import com.remitro.member.domain.history.repository.StatusHistoryRepository;
 
 public interface JpaStatusHistoryRepository extends
 	JpaRepository<StatusHistory, Long>, StatusHistoryRepository {
