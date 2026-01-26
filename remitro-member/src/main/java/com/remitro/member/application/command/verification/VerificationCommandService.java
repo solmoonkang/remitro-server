88b799ca10ff7bd0b39b9b5e79e7a36937c9d5d2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-26 20:56:22 +0900

    feat(#35): 인증 코드 발급 및 코드 검증 후 토큰 발급 로직 구현

---
+++ remitro-member/src/main/java/com/remitro/member/application/command/verification/VerificationCommandService.java
@@ -0,0 +1,56 @@
+package com.remitro.member.application.command.verification;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.remitro.member.application.command.dto.request.CodeSendRequest;
+import com.remitro.member.application.command.dto.request.CodeVerifyRequest;
+import com.remitro.member.application.command.dto.response.CodeConfirmResponse;
+import com.remitro.member.application.command.dto.response.CodeIssueResponse;
+import com.remitro.member.application.mapper.VerificationMapper;
+import com.remitro.member.application.read.verification.VerificationFinder;
+import com.remitro.member.application.support.VerificationCodeGenerator;
+import com.remitro.member.domain.verification.model.Verification;
+import com.remitro.member.domain.verification.repository.VerificationRepository;
+import com.remitro.member.infrastructure.messaging.MessageSendSupport;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class VerificationCommandService {
+
+	private final VerificationFinder verificationFinder;
+	private final VerificationRepository verificationRepository;
+	private final VerificationCodeGenerator verificationCodeGenerator;
+	private final VerificationValidator verificationValidator;
+
+	private final MessageSendSupport messageSendSupport;
+	private final Clock clock;
+
+	public CodeIssueResponse issueCode(CodeSendRequest codeSendRequest) {
+		final String verificationCode = verificationCodeGenerator.generate();
+		final Verification verification = Verification.issue(codeSendRequest.email(), verificationCode);
+		verificationRepository.save(verification);
+
+		messageSendSupport.send(codeSendRequest.email(), verificationCode, LocalDateTime.now(clock));
+
+		return VerificationMapper.toCodeIssueResponse(verificationCode);
+	}
+
+	public CodeConfirmResponse verifyCode(CodeVerifyRequest codeVerifyRequest) {
+		final Verification verification = verificationFinder.getLatestPendingCode(codeVerifyRequest.email());
+
+		verificationValidator.validateAlreadyConfirmed(verification);
+		verificationValidator.validateCodeValidity(verification, codeVerifyRequest.verificationCode());
+
+		verification.confirm();
+		verificationRepository.save(verification);
+
+		return VerificationMapper.toCodeConfirmResponse(verification.getVerificationToken());
+	}
+}
