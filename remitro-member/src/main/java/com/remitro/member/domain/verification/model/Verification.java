16ad1b73ab40029c28576ed47d9a706a84f75a96
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-26 20:54:29 +0900

    feat(#35): Redis 검증 도메일 모델 생성

---
+++ remitro-member/src/main/java/com/remitro/member/domain/verification/model/Verification.java
@@ -0,0 +1,42 @@
+package com.remitro.member.domain.verification.model;
+
+import java.util.UUID;
+
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@RedisHash(value = "VERIFICATION_TOKEN", timeToLive = 300)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Verification {
+
+	@Id
+	private String email;
+
+	private String verificationCode;
+
+	@Indexed
+	private String verificationToken;
+
+	private boolean isVerified;
+
+	private Verification(String email, String verificationCode) {
+		this.email = email;
+		this.verificationCode = verificationCode;
+		this.verificationToken = UUID.randomUUID().toString();
+		this.isVerified = false;
+	}
+
+	public static Verification issue(String email, String verificationCode) {
+		return new Verification(email, verificationCode);
+	}
+
+	public void confirm() {
+		this.isVerified = true;
+	}
+}
