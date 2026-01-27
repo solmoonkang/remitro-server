8998112f97ff8e54239dec50f1b70f106c7e0fc9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-27 17:20:13 +0900

    chore(#35): 마지막 비밀번호 변경일 추가

--- remitro-member/src/main/resources/data.sql
+++ remitro-member/src/main/resources/data.sql
@@ -4,6 +4,7 @@ FROM member;
 
 -- 시스템 관리자 (ADMIN)
 INSERT INTO member (email, password_hash, nickname, phone_number, phone_number_hash, member_status,
-                    login_security_status, role, failed_count, created_at, updated_at, version)
+                    login_security_status, role, failed_count, password_last_changed_at, created_at, updated_at,
+                    version)
 VALUES ('adminMember@example.com', 'adminP@ss0rd!', 'adminNickname', '01012345678', 'ADMIN_PHONE_HASH_VALUE',
-        'ACTIVE', 'NORMAL', 'ADMIN', 0, now(), now(), 0);
+        'ACTIVE', 'NORMAL', 'ADMIN', 0, now(), now(), now(), 0);
