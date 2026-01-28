e87721c8841a3d566976bcabba1c4f99c1d47769
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-29 01:57:12 +0900

    refactor(#37): 계좌 타입별 개설 여부 판단 로직 구현

--- remitro-account/src/main/java/com/remitro/account/domain/account/policy/OpenPolicy.java
+++ remitro-account/src/main/java/com/remitro/account/domain/account/policy/OpenPolicy.java
@@ -2,13 +2,17 @@ package com.remitro.account.domain.account.policy;
 
 import org.springframework.stereotype.Component;
 
-import com.remitro.account.domain.projection.enums.MemberProjectionStatus;
+import com.remitro.account.domain.account.enums.AccountType;
 import com.remitro.account.domain.projection.model.MemberProjection;
 
 @Component
 public class OpenPolicy {
 
 	public boolean isRestricted(MemberProjection member) {
-		return member.getMemberStatus() != MemberProjectionStatus.ACTIVE;
+		return !member.isActive();
+	}
+
+	public boolean isNotOpenable(AccountType accountType) {
+		return !accountType.isOpenable();
 	}
 }
