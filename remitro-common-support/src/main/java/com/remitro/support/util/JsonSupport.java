33d19df39ceed6ffa6fed1c934099f88380cbd0d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-28 19:19:37 +0900

    feat(#37): Redis 기반 직렬화/역직렬화 유틸 클래스 정의

---
+++ remitro-common-support/src/main/java/com/remitro/support/util/JsonSupport.java
@@ -0,0 +1,29 @@
+package com.remitro.support.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JsonSupport {
+
+	public static String toJSON(ObjectMapper objectMapper, Object data) {
+		try {
+			return objectMapper.writeValueAsString(data);
+
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("[❎ ERROR] REDIS 직렬화에 실패했습니다.", e);
+		}
+	}
+
+	public static <T> T fromJSON(ObjectMapper objectMapper, String jsonData, Class<T> clazz) {
+		try {
+			return objectMapper.readValue(jsonData, clazz);
+
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("[❎ ERROR] REDIS 역직렬화에 실패했습니다.", e);
+		}
+	}
+}
