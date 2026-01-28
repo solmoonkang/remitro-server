0e796665ee31391b3d6e91d280db8eb3917db540
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-29 01:56:57 +0900

    refactor(#37): 불필요한 상수 제거 및 계좌 타입별 생성 개수 제한 로직 추가

--- remitro-account/src/main/java/com/remitro/account/domain/account/policy/LimitPolicy.java
+++ remitro-account/src/main/java/com/remitro/account/domain/account/policy/LimitPolicy.java
@@ -2,6 +2,7 @@ package com.remitro.account.domain.account.policy;
 
 import org.springframework.stereotype.Component;
 
+import com.remitro.account.domain.account.enums.AccountType;
 import com.remitro.account.domain.account.repository.AccountRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LimitPolicy {
 
-	private static final int MAX_ACCOUNT_COUNT = 5;
-
 	private final AccountRepository accountRepository;
 
-	public boolean isExceeded(Long memberId) {
-		return accountRepository.countByMemberId(memberId) >= MAX_ACCOUNT_COUNT;
+	public boolean isExceeded(Long memberId, AccountType accountType) {
+		int currentCount = accountRepository.countByMemberIdAndAccountType(memberId, accountType);
+		return currentCount >= accountType.getMaxAccountsPerMember();
 	}
 }
