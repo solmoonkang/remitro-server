cf58b5adc6260e8f0c54c4d1bb6f907cfca8b489
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-28 19:17:47 +0900

    refactor(#37): VerificationRepository 구현체 구현

--- remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/RedisVerificationRepository.java
+++ remitro-member/src/main/java/com/remitro/member/infrastructure/persistence/RedisVerificationRepository.java
@@ -1,9 +1,53 @@
 package com.remitro.member.infrastructure.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import java.time.Duration;
+import java.util.Optional;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.remitro.member.domain.verification.model.Verification;
 import com.remitro.member.domain.verification.repository.VerificationRepository;
+import com.remitro.support.util.JsonSupport;
 
-public interface RedisVerificationRepository extends CrudRepository<Verification, String>, VerificationRepository {
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisVerificationRepository implements VerificationRepository {
+
+	private static final String VERIFICATION_PREFIX = "VERIFICATION:";
+	private static final Duration VERIFICATION_EXPIRATION_TIME = Duration.ofMinutes(5);
+
+	private final StringRedisTemplate stringRedisTemplate;
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public Optional<Verification> findByEmail(String email) {
+		return Optional.ofNullable(stringRedisTemplate.opsForValue().get(
+				generateKey(email))
+			)
+			.map(jsonData -> JsonSupport.fromJSON(objectMapper, jsonData, Verification.class));
+	}
+
+	@Override
+	public void save(Verification verification) {
+		stringRedisTemplate.opsForValue().set(
+			generateKey(verification.getEmail()),
+			JsonSupport.toJSON(objectMapper, verification),
+			VERIFICATION_EXPIRATION_TIME
+		);
+	}
+
+	@Override
+	public void deleteByEmail(String email) {
+		stringRedisTemplate.delete(
+			generateKey(email)
+		);
+	}
+
+	private String generateKey(String email) {
+		return VERIFICATION_PREFIX + email;
+	}
 }
