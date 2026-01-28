3fe02e84fed12a2af477cbcbbcf61a0574a383a5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-01-29 01:55:08 +0900

    refactor(#37): 1인당 계좌 타입별 생성 개수 조회 로직으로 수정

--- remitro-account/src/main/java/com/remitro/account/domain/account/repository/AccountRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/account/repository/AccountRepository.java
@@ -1,10 +1,11 @@
 package com.remitro.account.domain.account.repository;
 
+import com.remitro.account.domain.account.enums.AccountType;
 import com.remitro.account.domain.account.model.Account;
 
 public interface AccountRepository {
 
 	Account save(Account account);
 
-	int countByMemberId(Long memberId);
+	int countByMemberIdAndAccountType(Long memberId, AccountType accountType);
 }
