d95d1d0afaf41658d7826fec4566aaa133d1a98d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-09 00:29:24 +0900

    feat(#39): Kafka 이벤트를 통한 회원 프로젝션 정보 저장

--- remitro-account/src/main/java/com/remitro/account/application/projection/MemberEventService.java
+++ remitro-account/src/main/java/com/remitro/account/application/projection/MemberEventService.java
@@ -1,6 +1,5 @@
 package com.remitro.account.application.projection;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
