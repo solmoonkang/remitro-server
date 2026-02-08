034672294859f68bb10be55b52497009bfb5baa2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-08 16:18:04 +0900

    refactor(#39): 확장성 및 유지보수성 향상을 위한 메서드 분리

--- remitro-account/src/main/java/com/remitro/account/infrastructure/messaging/MemberEventConsumer.java
+++ remitro-account/src/main/java/com/remitro/account/infrastructure/messaging/MemberEventConsumer.java
@@ -1,9 +1,12 @@
 package com.remitro.account.infrastructure.messaging;
 
+import java.util.function.Consumer;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.remitro.account.application.projection.MemberEventService;
 import com.remitro.event.common.EventEnvelope;
@@ -19,6 +22,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class MemberEventConsumer {
 
+	private static final TypeReference<EventEnvelope<JsonNode>> COMMON_ENVELOPE_TYPE =
+		new TypeReference<>() {
+		};
 	private static final TypeReference<EventEnvelope<MemberRegisteredEvent>> MEMBER_REGISTERED_TYPE =
 		new TypeReference<>() {
 		};
@@ -35,21 +41,30 @@ public class MemberEventConsumer {
 		try {
 			log.debug("[✅ LOGGER] KAFKA 메시지를 수신했습니다. (PAYLOAD = {})", payloadJson);
 
-			final EventEnvelope<MemberRegisteredEvent> eventEnvelope = JsonSupport.fromJSON(
+			final EventEnvelope<JsonNode> commonEnvelope = JsonSupport.fromJSON(
 				objectMapper,
 				payloadJson,
-				MEMBER_REGISTERED_TYPE
+				COMMON_ENVELOPE_TYPE
 			);
 
-			if (eventEnvelope.eventType() != EventType.MEMBER_REGISTERED) {
-				log.warn("[✅ LOGGER] 지원하지 않는 이벤트 타입입니다. (EVENT_TYPE = {})", eventEnvelope.eventType());
-				return;
-			}
-
-			memberEventService.handleMemberRegistered(eventEnvelope.payload());
+			this.dispatch(commonEnvelope, payloadJson);
 
 		} catch (Exception e) {
 			log.error("[✅ LOGGER] KAFKA CONSUMER 메시지 처리 중 오류가 발생했습니다. (PAYLOAD = {})", payloadJson, e);
 		}
+	}
+
+	private void dispatch(EventEnvelope<JsonNode> commonEventEnvelope, String payloadJson) {
+		final EventType eventType = commonEventEnvelope.eventType();
+
+		switch (eventType) {
+			case MEMBER_REGISTERED -> process(payloadJson, MEMBER_REGISTERED_TYPE, memberEventService::handleSignUp);
+			default -> log.warn("[✅ LOGGER] 지원하지 않는 이벤트 타입입니다. (EVENT_TYPE = {})", eventType);
+		}
+	}
+
+	private <T> void process(String payloadJson, TypeReference<EventEnvelope<T>> typeReference, Consumer<T> handler) {
+		final EventEnvelope<T> eventEnvelope = JsonSupport.fromJSON(objectMapper, payloadJson, typeReference);
+		handler.accept(eventEnvelope.payload());
 	}
 }
