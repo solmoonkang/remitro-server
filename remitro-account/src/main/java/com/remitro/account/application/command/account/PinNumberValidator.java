60774a645b0f2beb073a74fb00b73c6b15e99c58
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-09 23:45:32 +0900

    feat(#41): 핀 번호 검증 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/command/account/PinNumberValidator.java
+++ remitro-account/src/main/java/com/remitro/account/application/command/account/PinNumberValidator.java
@@ -1,10 +1,11 @@
-package com.remitro.account.application.command.deposit;
+package com.remitro.account.application.command.account;
 
 import org.springframework.stereotype.Component;
 
 import com.remitro.account.domain.account.policy.PinNumberPolicy;
 import com.remitro.support.error.ErrorCode;
 import com.remitro.support.exception.BadRequestException;
+import com.remitro.support.exception.UnauthorizedException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +15,15 @@ public class PinNumberValidator {
 
 	private final PinNumberPolicy pinNumberPolicy;
 
-	public void validateConfirm(String rawPinNumber, String confirmPinNumber) {
+	public void validatePinConfirm(String rawPinNumber, String confirmPinNumber) {
 		if (!pinNumberPolicy.isSamePinNumber(rawPinNumber, confirmPinNumber)) {
 			throw new BadRequestException(ErrorCode.PASSWORD_CONFIRM_MISMATCH);
+		}
+	}
+
+	public void validatePinMatch(String rawPassword, String encodedPassword) {
+		if (pinNumberPolicy.isWrongPinNumber(rawPassword, encodedPassword)) {
+			throw new UnauthorizedException(ErrorCode.INVALID_PASSWORD);
 		}
 	}
 }
