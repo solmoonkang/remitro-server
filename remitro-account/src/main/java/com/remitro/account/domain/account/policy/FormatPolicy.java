a4ee45951f3c6c7eb470f5be88aca7bd73f5b291
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-09 23:43:41 +0900

    feat(#41): 금액 응답 시 000 단위마다 콤마로 분리하는 로직 구현

--- remitro-account/src/main/java/com/remitro/account/domain/account/policy/FormatPolicy.java
+++ remitro-account/src/main/java/com/remitro/account/domain/account/policy/FormatPolicy.java
@@ -1,19 +1,26 @@
 package com.remitro.account.domain.account.policy;
 
+import java.text.DecimalFormat;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class FormatPolicy {
 
 	private static final String HYPHEN = "-";
+	private static final String DEFAULT_AMOUNT_TEXT = "0";
+	private static final DecimalFormat AMOUNT_FORMAT = new DecimalFormat("#,###");
 
-	public String format(String accountNumber) {
-		return new StringBuilder(14)
-			.append(accountNumber, 0, 3)
-			.append(HYPHEN)
-			.append(accountNumber, 3, 5)
-			.append(HYPHEN)
-			.append(accountNumber, 5, 12)
-			.toString();
+	public String formatAccountNumber(String accountNumber) {
+		return accountNumber.substring(0, 3) + HYPHEN +
+			   accountNumber.substring(3, 5) + HYPHEN +
+			   accountNumber.substring(5, 12);
+	}
+
+	public String formatAmount(Long amount) {
+		if (amount == null)
+			return DEFAULT_AMOUNT_TEXT;
+
+		return AMOUNT_FORMAT.format(amount);
 	}
 }
