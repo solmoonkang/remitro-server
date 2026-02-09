71ebcb97cedbb9918018e6197c4ccaf8fd2654e3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-09 20:51:57 +0900

    feat(#41): 입출금 응답 DTO 핼퍼 로직 구현

--- remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
+++ remitro-account/src/main/java/com/remitro/account/application/mapper/AccountMapper.java
@@ -2,23 +2,56 @@ package com.remitro.account.application.mapper;
 
 import java.time.LocalDateTime;
 
-import com.remitro.account.application.command.dto.response.DepositOpenResponse;
+import com.remitro.account.application.command.dto.response.AccountOpenResponse;
+import com.remitro.account.application.command.dto.response.DepositResponse;
+import com.remitro.account.application.command.dto.response.WithdrawResponse;
+import com.remitro.account.domain.ledger.model.AccountLedger;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AccountMapper {
+public final class AccountMapper {
 
-	public static DepositOpenResponse toDepositOpenResponse(
+	public static AccountOpenResponse toAccountOpenResponse(
 		String accountNumber,
 		String accountAlias,
 		LocalDateTime createdAt
 	) {
-		return new DepositOpenResponse(
+		return new AccountOpenResponse(
 			accountNumber,
 			accountAlias,
 			createdAt
+		);
+	}
+
+	public static DepositResponse toDepositResponse(
+		AccountLedger accountLedger,
+		String accountNumber,
+		String amount,
+		String currentBalance
+	) {
+		return new DepositResponse(
+			accountLedger.getId(),
+			accountNumber,
+			amount,
+			currentBalance,
+			accountLedger.getCreatedAt()
+		);
+	}
+
+	public static WithdrawResponse toWithdrawResponse(
+		AccountLedger accountLedger,
+		String accountNumber,
+		String amount,
+		String currentBalance
+	) {
+		return new WithdrawResponse(
+			accountLedger.getId(),
+			accountNumber,
+			amount,
+			currentBalance,
+			accountLedger.getCreatedAt()
 		);
 	}
 }
