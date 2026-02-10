b6ae87b4cce2954408e67e5d14ab8e68aead1ae7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-02-10 18:08:21 +0900

    feat(#41): 계좌 원장 내역 모두 저장 로직 구현

--- remitro-account/src/main/java/com/remitro/account/domain/ledger/repository/AccountLedgerRepository.java
+++ remitro-account/src/main/java/com/remitro/account/domain/ledger/repository/AccountLedgerRepository.java
@@ -1,8 +1,12 @@
 package com.remitro.account.domain.ledger.repository;
 
+import java.util.List;
+
 import com.remitro.account.domain.ledger.model.AccountLedger;
 
 public interface AccountLedgerRepository {
 
 	AccountLedger save(AccountLedger accountLedger);
+
+	List<AccountLedger> saveAll(List<AccountLedger> accountLedgers);
 }
